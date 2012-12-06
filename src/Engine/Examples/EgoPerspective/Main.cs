987abc069fb3b6d375512878f10d3c0327083ff8
Caspar Jacob <caspar.jacob@web.de>
2012-12-06 16:02:33 +0100

    added MoreShaders

--- src/Engine/Examples/EgoPerspective/Main.cs
+++ src/Engine/Examples/EgoPerspective/Main.cs
@@ -10,21 +10,33 @@ namespace Examples.EgoPerspective
     {
         private static float _angleHorz = 0.0f, _angleVert = 0.0f, _angleVelHorz = 0, _angleVelVert = 0, _rotationSpeed = 10.0f, _damping = 0.95f, _moveX = 0.0f, _moveY = 0.0f, _moveZ = 0.0f;
         private World _world;
-        protected ShaderProgram Sp3;
-        protected IShaderParam VColorParam;
-        protected IShaderParam VLightDir;
+        protected ShaderProgram sp;
+        protected IShaderParam darkColor;
+        protected IShaderParam brightColor;
+        protected IShaderParam chessSize;
+        protected IShaderParam smoothFactor;
 
         public override void Init()
         {
             _world = new World(RC, In);
-            Geometry geo = MeshReader.ReadWavefrontObj(new StreamReader(@"Assets/Teapot.obj.model"));
+            Geometry geo = MeshReader.ReadWavefrontObj(new StreamReader(@"Assets/ground.obj.model"));
             _world.addObject(geo, 0, 0, 1000);
 
-            Sp3 = Shaders.GetShader("multiLight", RC);
-            RC.SetShader(Sp3);
-            ShaderMaterial m = RC.CreateMaterial(Sp3);          
+            sp = MoreShaders.GetShader("multiLight", RC);
+            RC.SetShader(sp);
+            darkColor = sp.GetShaderParam("darkColor");
+            brightColor = sp.GetShaderParam("brightColor");
+            chessSize = sp.GetShaderParam("chessSize");
+            smoothFactor = sp.GetShaderParam("smoothFactor");
+            RC.SetShaderParam(darkColor, new float3(0, 0, 0));
+            RC.SetShaderParam(brightColor, new float3(1, 1, 1));
+            RC.SetShaderParam(chessSize, 100);
+            RC.SetShaderParam(smoothFactor, 0.5f);
+
+            ShaderMaterial m = RC.CreateMaterial(sp);
             m.SetShininess(8);
             RC.SetMaterial(m);
+
             RC.SetLightAmbient(1, new float4(0, 1, 0, 1));
             RC.SetLightSpecular(1, new float4(0, 1, 0, 1));
             RC.SetLightDiffuse(1, new float4(0, 1, 0, 1));
