edde0b8e4e63c08e7ead10177e5557faf9f2f4e7
Helge Meiering <helge.meiering@gmail.com>
2013-02-25 20:15:50 +0100

    Example comments

--- src/Engine/FuseeExampleApp1/Main.cs
+++ src/Engine/FuseeExampleApp1/Main.cs
@@ -1,11 +1,12 @@
-﻿using Fusee.Engine;
+﻿using System.IO;
+using Fusee.Engine;
 using Fusee.Math;
 
-namespace Examples.FuseeExampleApp1
+namespace Examples.Simple
 {
-    public class FuseeExampleApp1 : RenderCanvas
+    public class Simple : RenderCanvas
     {
-
+        //Pixel and Vertex Shader
         protected string Vs = @"
              #version 120
 
@@ -13,24 +14,18 @@ namespace Examples.FuseeExampleApp1
              * Applies the transformation matrix to vertex position.
              */
 
-            attribute vec2 fuUV;
-
             attribute vec4 fuColor;
             attribute vec3 fuVertex;
             attribute vec3 fuNormal;
-            
-            
-
+        
             varying vec4 vColor;
             varying vec3 vNormal;
-            varying vec2 vUV;
         
             uniform mat4 FUSEE_MVP;
             uniform mat4 FUSEE_ITMV;
 
             void main()
             {
-                vUV = fuUV;
                 gl_Position = FUSEE_MVP * vec4(fuVertex, 1.0);
                 // vColor = vec4(fuNormal * 0.5 + 0.5, 1.0);
                 // vec4 norm4 = FUSEE_MVP * vec4(fuNormal, 0.0);
@@ -49,40 +44,99 @@ namespace Examples.FuseeExampleApp1
             uniform vec4 vColor;
             varying vec3 vNormal;
 
-            //Variablen für Texturen im Pixelshader
-            uniform sampler2D texture_planet;
-            varying vec2 vUV;
-                        
-
-
             void main()
             {
-                
-
-                gl_FragColor = texture2D(texture_planet, vUV) ; //vColor * dot(vNormal, vec3(0, 0, 1));
+                gl_FragColor = vColor * dot(vNormal, vec3(0, 0, 1));
             }";
+        //angle variable
+        private static float _angleHorz = 0.0f, _angleVert = 0.0f, _angleVelHorz = 0, _angleVelVert = 0, _rotationSpeed = 10.0f, _damping = 0.95f;
+        //modell variable
+        protected Mesh Mesh, MeshFace;
+        //variable for color
+        protected IShaderParam VColorParam;
+
         public override void Init()
         {
-            // is called on startup
+            //initialize the variable
+            Geometry geo = MeshReader.ReadWavefrontObj(new StreamReader(@"Assets/Teapot.obj.model"));
+            Mesh = geo.ToMesh();
+
+            Geometry geo2 = MeshReader.ReadWavefrontObj(new StreamReader(@"Assets/Face.obj.model"));
+            MeshFace = geo2.ToMesh();
+
+            _angleHorz = 0;
+            _rotationSpeed = 10.0f;
+            ShaderProgram sp = RC.CreateShader(Vs, Ps);
+            RC.SetShader(sp);
+            VColorParam = sp.GetShaderParam("vColor");
+
+
+            RC.ClearColor = new float4(1, 1, 1, 1);
         }
 
         public override void RenderAFrame()
         {
-            // is called once a frame
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+            //move per mouse
+            if (Input.Instance.IsButtonDown(MouseButtons.Left))
+            {
+                _angleVelHorz = _rotationSpeed * Input.Instance.GetAxis(InputAxis.MouseX) * (float)Time.Instance.DeltaTime;
+                _angleVelVert = _rotationSpeed * Input.Instance.GetAxis(InputAxis.MouseY) * (float)Time.Instance.DeltaTime;
+            }
+            else
+            {
+                _angleVelHorz *= _damping;
+                _angleVelVert *= _damping;
+            }
+            _angleHorz += _angleVelHorz;
+            _angleVert += _angleVelVert;
+
+            //move per keyboard
+            if (Input.Instance.IsKeyDown(KeyCodes.Left))
+            {
+                _angleHorz -= _rotationSpeed * (float)Time.Instance.DeltaTime;
+            }
+            if (Input.Instance.IsKeyDown(KeyCodes.Right))
+            {
+                _angleHorz += _rotationSpeed * (float)Time.Instance.DeltaTime;
+            }
+            if (Input.Instance.IsKeyDown(KeyCodes.Up))
+            {
+                _angleVert -= _rotationSpeed * (float)Time.Instance.DeltaTime;
+            }
+            if (Input.Instance.IsKeyDown(KeyCodes.Down))
+            {
+                _angleVert += _rotationSpeed * (float)Time.Instance.DeltaTime;
+            }
+
+            float4x4 mtxRot = float4x4.CreateRotationY(_angleHorz) * float4x4.CreateRotationX(_angleVert);
+            float4x4 mtxCam = float4x4.LookAt(0, 200, 400, 0, 50, 0, 0, 1, 0);
+
+            RC.ModelView = mtxRot * float4x4.CreateTranslation(-100, 0, 0) * mtxCam;
+            //colordecloration
+            RC.SetShaderParam(VColorParam, new float4(0.5f, 0.8f, 0, 1));
+            RC.Render(Mesh);
+
+            RC.ModelView = mtxRot * float4x4.CreateTranslation(100, 0, 0) * mtxCam;
+            //colordecloration
+            RC.SetShaderParam(VColorParam, new float4(0.8f, 0.5f, 0, 1));
+            RC.Render(MeshFace);
+            Present();
         }
 
         public override void Resize()
         {
-            // is called when the window is resized
             RC.Viewport(0, 0, Width, Height);
 
-            var aspectRatio = Width / (float)Height;
-            RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 10000);
+            float aspectRatio = Width / (float)Height;
+            RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 5000);
         }
 
         public static void Main()
         {
-            var app = new FuseeExampleApp1();
+            
+            Simple app = new Simple();
             app.Run();
         }
 
