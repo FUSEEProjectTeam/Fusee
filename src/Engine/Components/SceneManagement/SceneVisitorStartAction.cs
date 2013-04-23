0bf98b1240811448531069eaf60832314735cbc9
Denis Dik <denis.dik@hs-furtwangen.de>
2013-04-23 16:28:51 +0200

    Changed Sphere.obj.model
    Signed-off-by: Denis Dik <denis.dik@hs-furtwangen.de>

---
+++ src/Engine/Components/SceneManagement/SceneVisitorStartAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fusee.Engine;
+
+namespace Fusee.SceneManagement
+{
+    class SceneVisitorStartAction : SceneVisitor
+    {
+        public override void Visit(SceneEntity cEntity)
+        {
+            cEntity.TraverseForRendering(this);
+        }
+
+        public override void Visit(ActionCode actionCode)
+        {
+            actionCode.Start();
+        }
+    }
+}
