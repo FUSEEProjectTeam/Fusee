d7cbf7b537745ed01f50137afb54ada430144c3f
Fabian Gärtner <fabian.gaertner@hs-furtwangen.de>
2013-05-09 12:04:25 +0200

    Linux (pt1)

--- src/Engine/Common/IRenderCanvasImp.cs
+++ src/Engine/Common/IRenderCanvasImp.cs
@@ -1,6 +1,4 @@
-﻿#pragma warning disable 1591 //disables the warning about missing XML-comments
-
-using System;
+﻿using System;
 
 namespace Fusee.Engine
 {
@@ -23,5 +21,3 @@ namespace Fusee.Engine
         event EventHandler<ResizeEventArgs> Resize;
     }
 }
-
-#pragma warning restore 1591
