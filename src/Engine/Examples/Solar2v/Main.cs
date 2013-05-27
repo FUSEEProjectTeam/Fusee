ed6bbc83e5e61ae4ea33864454c13594a9262356
CoffeeParser <denis.dik@hs-furtwangen.de>
2013-05-27 15:12:32 +0200

    Added new Project Solar2v.csproj
    Signed-off-by: CoffeeParser <denis.dik@hs-furtwangen.de>

---
+++ src/Engine/Examples/Solar2v/Main.cs
@@ -0,0 +1,35 @@
+using Fusee.Engine;
+using Fusee.SceneManagement;
+using Fusee.Math;
+
+namespace Examples.Solar2v
+{
+    public class Solar2v : RenderCanvas
+    {
+        public override void Init()
+        {
+            // is called on startup
+        }
+
+        public override void RenderAFrame()
+        {
+            // is called once a frame
+        }
+
+        public override void Resize()
+        {
+            // is called when the window is resized
+            RC.Viewport(0, 0, Width, Height);
+
+            var aspectRatio = Width / (float)Height;
+            RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 10000);
+        }
+
+        public static void Main()
+        {
+            var app = new Solar2v();
+            app.Run();
+        }
+
+    }
+}
