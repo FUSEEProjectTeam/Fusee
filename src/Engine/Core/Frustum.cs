c30f31bad530de1806cb4181da5c69e52b52721c
CoffeeParser <denis.dik@hs-furtwangen.de>
2013-05-27 14:25:29 +0200

    changed Frustum.cs to final version
    Signed-off-by: CoffeeParser <denis.dik@hs-furtwangen.de>

--- src/Engine/Core/Frustum.cs
+++ src/Engine/Core/Frustum.cs
@@ -10,167 +10,96 @@ namespace Fusee.Engine
     public class Frustum
     {
         private float[,] frustum = new float[6, 4];
-
+        private Plane[] pl = new Plane[6];
         public Frustum(float4x4 mvp)
         {
-            UpdateFrustum(mvp);
+            setFrustum(mvp);
         }
 
-
-
-        public void UpdateFrustum(float4x4 mvp)
+        private enum PlaneSide
         {
-            float t; // Temporary normalized value
-            /* Extract the numbers for the RIGHT plane */
-            frustum[0, 0] = mvp.M14 - mvp.M11;
-            frustum[0, 1] = mvp.M24 - mvp.M21;
-            frustum[0, 2] = mvp.M34 - mvp.M31;
-            frustum[0, 3] = mvp.M44 - mvp.M41;
-
-            /* Normalize the result */
-            t = (float)System.Math.Sqrt(frustum[0, 0] * frustum[0, 0] + frustum[0, 1] * frustum[0, 1] + frustum[0, 2] * frustum[0, 2]);
-            frustum[0, 0] /= t;
-            frustum[0, 1] /= t;
-            frustum[0, 2] /= t;
-            frustum[0, 3] /= t;
-
-            /* Extract the numbers for the LEFT plane */
-            frustum[1, 0] = mvp.M14 + mvp.M11;
-            frustum[1, 1] = mvp.M24 + mvp.M21;
-            frustum[1, 2] = mvp.M34 + mvp.M31;
-            frustum[1, 3] = mvp.M44 + mvp.M41;
-
-            /* Normalize the result */
-            t = (float)System.Math.Sqrt(frustum[1, 0] * frustum[1, 0] + frustum[1, 1] * frustum[1, 1] + frustum[1, 2] * frustum[1, 2]);
-            frustum[1, 0] /= t;
-            frustum[1, 1] /= t;
-            frustum[1, 2] /= t;
-            frustum[1, 3] /= t;
-
-            /* Extract the BOTTOM plane */
-            frustum[2, 0] = mvp.M14 + mvp.M12;
-            frustum[2, 1] = mvp.M24 + mvp.M22;
-            frustum[2, 2] = mvp.M34 + mvp.M32;
-            frustum[2, 3] = mvp.M44 + mvp.M42;
-
-            /* Normalize the result */
-            t = (float)System.Math.Sqrt(frustum[2, 0] * frustum[2, 0] + frustum[2, 1] * frustum[2, 1] + frustum[2, 2] * frustum[2, 2]);
-            frustum[2, 0] /= t;
-            frustum[2, 1] /= t;
-            frustum[2, 2] /= t;
-            frustum[2, 3] /= t;
-
-            /* Extract the TOP plane */
-            frustum[3, 0] = mvp.M14 - mvp.M12;
-            frustum[3, 1] = mvp.M24 - mvp.M22;
-            frustum[3, 2] = mvp.M34 - mvp.M32;
-            frustum[3, 3] = mvp.M44 - mvp.M42;
-
-            /* Normalize the result */
-            t = (float)System.Math.Sqrt(frustum[3, 0] * frustum[3, 0] + frustum[3, 1] * frustum[3, 1] + frustum[3, 2] * frustum[3, 2]);
-            frustum[3, 0] /= t;
-            frustum[3, 1] /= t;
-            frustum[3, 2] /= t;
-            frustum[3, 3] /= t;
-
-            /* Extract the FAR plane */
-            frustum[4, 0] = mvp.M14 - mvp.M13;
-            frustum[4, 1] = mvp.M24 - mvp.M23;
-            frustum[4, 2] = mvp.M34 - mvp.M33;
-            frustum[4, 3] = mvp.M44 - mvp.M43;
-
-            /* Normalize the result */
-            t = (float)System.Math.Sqrt(frustum[4, 0] * frustum[4, 0] + frustum[4, 1] * frustum[4, 1] + frustum[4, 2] * frustum[4, 2]);
-            frustum[4, 0] /= t;
-            frustum[4, 1] /= t;
-            frustum[4, 2] /= t;
-            frustum[4, 3] /= t;
-
-            /* Extract the NEAR plane */
-            frustum[5, 0] = mvp.M14 + mvp.M13;
-            frustum[5, 1] = mvp.M24 + mvp.M23;
-            frustum[5, 2] = mvp.M34 + mvp.M33;
-            frustum[5, 3] = mvp.M44 + mvp.M43;
-
-            /* Normalize the result */
-            t = (float)System.Math.Sqrt(frustum[5, 0] * frustum[5, 0] + frustum[5, 1] * frustum[5, 1] + frustum[5, 2] * frustum[5, 2]);
-            frustum[5, 0] /= t;
-            frustum[5, 1] /= t;
-            frustum[5, 2] /= t;
-            frustum[5, 3] /= t;
-            //Debug.WriteLine(mvp);
+            NEARP, FARP, BOTTOM, TOP, LEFT, RIGHT,
         }
+
+        private struct Plane
+        {
+            public float3 normal;
+            public float d;
+            public void setCoefficients(float a, float b, float c, float d)
+            {
+                // set the normal vector
+                normal = new float3(a, b, c);
+                //compute the lenght of the vector
+                float l = normal.Length;
+                // normalize the vector
+                normal = new float3(a / l, b / l, c / l);
+                // and divide d by th length as well
+                this.d = d / l;
+            }
+
+            public float distance(float3 p)
+            {
+                return (d + float3.Dot(normal, p));
+            }
+
+
+        }
+
+        public void setFrustum(float4x4 imvp)
+        {
+            pl[(int)PlaneSide.NEARP].setCoefficients(imvp.M31 + imvp.M41, imvp.M32 + imvp.M42, imvp.M33 + imvp.M43, imvp.M34 + imvp.M44);
+
+
+            pl[(int)PlaneSide.FARP].setCoefficients(-imvp.M31 + imvp.M41, -imvp.M32 + imvp.M42, -imvp.M33 + imvp.M43, -imvp.M34 + imvp.M44);
+
+
+            pl[(int)PlaneSide.BOTTOM].setCoefficients(imvp.M21 + imvp.M41, imvp.M22 + imvp.M42, imvp.M23 + imvp.M43, imvp.M24 + imvp.M44);
+
+
+            pl[(int)PlaneSide.TOP].setCoefficients(-imvp.M21 + imvp.M41, -imvp.M22 + imvp.M42, -imvp.M23 + imvp.M43, -imvp.M24 + imvp.M44);
+
+
+            pl[(int)PlaneSide.LEFT].setCoefficients(imvp.M11 + imvp.M41, imvp.M12 + imvp.M42, imvp.M13 + imvp.M43, imvp.M14 + imvp.M44);
+
+
+            pl[(int)PlaneSide.RIGHT].setCoefficients(-imvp.M11 + imvp.M41, -imvp.M12 + imvp.M42, -imvp.M13 + imvp.M43, -imvp.M14 + imvp.M44);
+
+        }
+
+        
 
         public bool PointInFrustum(float3 point)
         {
+            bool result = true;
+            for (int i = 0; i < 6; i++)
+            {
 
-            for (int p = 0; p < 6; p++)
-                if (frustum[p, 0] * point.x + frustum[p, 1] * point.y + frustum[p, 2] * point.z + frustum[p, 3] < 0)
+                if (pl[i].distance(point) < 0)
                 {
-                    Debug.WriteLine(point + " is not in frustum. " + frustum[p, 0] + "|" + frustum[p, 1] + "|" + frustum[p, 2] + "|" + frustum[p, 3]);
                     return false;
                 }
-            return true;
+            }
+            return result;
         }
 
 
         public bool SphereInFrustum(float3 point, float radius)
         {
+
             for (int p = 0; p < 6; p++)
-                if (frustum[p,0] * point.x + frustum[p,1] * point.y + frustum[p,2] * point.z + frustum[p,3] <= -radius)
+            {
+                if (pl[p].distance(point) <= -radius)
                 {
-                    //float sum = frustum[p, 0]*point.x + frustum[p, 1]*point.y + frustum[p, 2]*point.z + frustum[p, 3];
-                    //Debug.WriteLine(point +" | "+sum);
+
                     return false;
                 }
-                    
-            return true;
-        }
-
-        // Contains also the distance as result, may be used for levelofDetail
-        float SphereInFrustumDistance(float3 point, float radius)
-        {
-            int p;
-            float d = 0;
-
-            for (p = 0; p < 6; p++)
-            {
-                d = frustum[p,0] * point.x + frustum[p,1] * point.y + frustum[p,2] * point.z + frustum[p,3];
-                if (d <= -radius)
-                    return 0;
-            }
-            return d + radius;
-        }
-
-        public bool CubeInFrustum(float3 point, float size)
-        {
-            for (int p = 0; p < 6; p++)
-            {
-                if (frustum[p,0] * (point.x - size) + frustum[p,1] * (point.y - size) + frustum[p,2] * (point.z - size) + frustum[p,3] > 0)
-                    continue;
-                if (frustum[p,0] * (point.x + size) + frustum[p,1] * (point.y - size) + frustum[p,2] * (point.z - size) + frustum[p,3] > 0)
-                    continue;
-                if (frustum[p,0] * (point.x - size) + frustum[p,1] * (point.y + size) + frustum[p,2] * (point.z - size) + frustum[p,3] > 0)
-                    continue;
-                if (frustum[p,0] * (point.x + size) + frustum[p,1] * (point.y + size) + frustum[p,2] * (point.z - size) + frustum[p,3] > 0)
-                    continue;
-                if (frustum[p,0] * (point.x - size) + frustum[p,1] * (point.y - size) + frustum[p,2] * (point.z + size) + frustum[p,3] > 0)
-                    continue;
-                if (frustum[p,0] * (point.x + size) + frustum[p,1] * (point.y - size) + frustum[p,2] * (point.z + size) + frustum[p,3] > 0)
-                    continue;
-                if (frustum[p,0] * (point.x - size) + frustum[p,1] * (point.y + size) + frustum[p,2] * (point.z + size) + frustum[p,3] > 0)
-                    continue;
-                if (frustum[p,0] * (point.x + size) + frustum[p,1] * (point.y + size) + frustum[p,2] * (point.z + size) + frustum[p,3] > 0)
-                    continue;
-                return false;
             }
             return true;
         }
 
-        public bool SphereOrCubeInFrustum()
-        {
-            return true;
-        }
+       
+
+    
 
     }
 }
