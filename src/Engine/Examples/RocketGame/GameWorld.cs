2a4809facef7d4a2bb4f7eb5da390434f336e0b7
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2014-01-10 16:23:46 +0100

    All the Toon shaders

--- src/Engine/Examples/RocketGame/GameWorld.cs
+++ src/Engine/Examples/RocketGame/GameWorld.cs
@@ -37,12 +37,12 @@ namespace Examples.RocketGame
 
             _player = new Player("Assets/rocket2.obj.model", rc);
             _player.SetScale(0.5f);
-            _player.SetShader("Assets/rocket2.jpg");
+            _player.SetShader("Assets/rocket2.jpg", "Assets/toon_generic_4_tex.png", new float4(0, 0, 0, 1), new float2(5, 5));
             _player.SetCorrectionMatrix(float4x4.CreateRotationX((float) -Math.PI/2) * float4x4.CreateTranslation(-30,0,0));
 
             _room = new GameEntity("Assets/spacebox.obj.model", rc);
             //_room.SetShader(new float4(1,0,0,1));
-            _room.SetShader(new float4(1, 0, 0, 1), "Assets/toon_generic_5_tex.png", new float4(0, 0, 0, 1), new float2(5, 5));
+            _room.SetShader(new float4(1, 0, 0, 1), "Assets/toon_generic_7_tex.png", new float4(0, 0, 0, 1), new float2(5, 5));
             _room.SetScale(8);
 
             _gui = new GUI(rc, this);
@@ -51,7 +51,7 @@ namespace Examples.RocketGame
             _goals[0].SetScale(0.5f);
 
             _furniture.Add(new GameEntity("Assets/rocket2.obj.model", rc, 0, 250));
-            _furniture[0].SetShader("Assets/rocket2.jpg", "Assets/toon_generic_5_tex.png", new float4(0, 0, 0, 1), new float2(5, 5));
+            _furniture[0].SetShader("Assets/rocket2.jpg", "Assets/toon_generic_4_tex.png", new float4(0, 0, 0, 1), new float2(5, 5));
         }
 
         public void Render()
