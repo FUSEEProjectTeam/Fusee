eb6053b986a92c15479d6b22338a204dc19c8e4e
Fabian Gaertner <fabian.gaertner@hs-furtwangen.de>
2014-02-03 17:40:51 +0100

    It works in the web, but something is wrong

--- src/Engine/Core/ParticleEmitter.cs
+++ src/Engine/Core/ParticleEmitter.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using Fusee.Math;
-
 
 namespace Fusee.Engine
 {
-    struct ParticleData
+    internal struct ParticleData
     {
         public float3 Position;
         public float3 Velocity; // Contains the direction of movement (the vector's length ist the speed)
@@ -19,10 +15,9 @@ namespace Fusee.Engine
         public IShaderParam ParticleTextureParam;
         public ITexture ParticleTex;
     }
-    
+
     public class ParticleEmitter
     {
-
         public Mesh _particleMesh = new Mesh();
 
         public Mesh ParticleMesh
@@ -37,9 +32,7 @@ namespace Fusee.Engine
         // Initial rotation of particles
         // rotation damping over time
 
-        
-        
-        private List<ParticleData> _particleList = new List<ParticleData>();
+        private readonly List<ParticleData> _particleList = new List<ParticleData>();
         private ParticleData _particle;
         private static Random _rnd;
         private static double _randVelX;
@@ -49,108 +42,103 @@ namespace Fusee.Engine
         private static double _randPosY;
         private static double _randPosZ;
 
-        private int customCount;
-        private int customLifeMin;
-        private int customLifeMax;
-        private float customSize;
-        private double customRandPosX;
-        private double customRandPosY;
-        private double customRandPosZ;
-        private double customRandVelX;
-        private double customRandVelY;
-        private double customRandVelZ;
-        private  float customGravityX;
-        private  float customGravityY;
-        private  float customGravityZ;
+        private readonly int customCount;
+        private readonly int customLifeMin;
+        private readonly int customLifeMax;
+        private readonly float customSize;
+        private readonly double customRandPosX;
+        private readonly double customRandPosY;
+        private readonly double customRandPosZ;
+        private readonly double customRandVelX;
+        private readonly double customRandVelY;
+        private readonly double customRandVelZ;
+        private readonly float customGravityX;
+        private readonly float customGravityY;
+        private readonly float customGravityZ;
         public float Al = 0.2f;
-        
-        
-        
 
         //particelcount,Size,Life,randVelX,randVelY,randVelZ,gravityX, gravityY, gravityZ,
-        public ParticleEmitter(int myCount, int myLifeMin, int myLifeMax, float mySize, float myRandPosX, float myRandPosY, float myRandPosZ, double myRandVelX, double myRandVelY, double myRandVelZ, float myGravityX, float myGravityY, float myGravityZ)
+        public ParticleEmitter(int myCount, int myLifeMin, int myLifeMax, float mySize, float myRandPosX,
+            float myRandPosY, float myRandPosZ, double myRandVelX, double myRandVelY, double myRandVelZ,
+            float myGravityX, float myGravityY, float myGravityZ)
         {
-            _rnd            = new Random();
-            customCount     = myCount;
-            customLifeMin   = myLifeMin;
-            customLifeMax   = myLifeMax;
-            customSize      = mySize;
-            customRandPosX  = myRandPosX;
-            customRandPosY  = myRandPosY;
-            customRandPosZ  = myRandPosZ;
-            customRandVelX  = myRandVelX;
-            customRandVelY  = myRandVelY;
-            customRandVelZ  = myRandVelZ;
-            customGravityX  = myGravityX;
-            customGravityY  = myGravityY;
-            customGravityZ  = myGravityZ;
-
+            _rnd = new Random();
+            customCount = myCount;
+            customLifeMin = myLifeMin;
+            customLifeMax = myLifeMax;
+            customSize = mySize;
+            customRandPosX = myRandPosX;
+            customRandPosY = myRandPosY;
+            customRandPosZ = myRandPosZ;
+            customRandVelX = myRandVelX;
+            customRandVelY = myRandVelY;
+            customRandVelZ = myRandVelZ;
+            customGravityX = myGravityX;
+            customGravityY = myGravityY;
+            customGravityZ = myGravityZ;
         }
 
         public void Tick(double deltaTime)
         {
-            float3[] vertices = new float3[customCount * 4];
-            short[] triangles = new short[customCount * 6];
-            float3[] normals = new float3[customCount * 4];
-            float2[] uVs = new float2[customCount * 4];
+            var vertices = new float3[customCount*4];
+            var triangles = new short[customCount*6];
+            var normals = new float3[customCount*4];
+            var uVs = new float2[customCount*4];
 
             //var mtxNew = float4x4.CreateRotationY(5) * float4x4.CreateRotationX(5);
             //var myRot = float4x4.CreateRotationZ(1.0f);
             //Partikel-Daten initialisieren
             while (_particleList.Count < customCount)
             {
-                    _randVelX = GenRand(-customRandVelX, customRandVelX);
-                    _randVelY = GenRand(-customRandVelY, customRandVelY);
-                    _randVelZ = GenRand(-customRandVelZ, customRandVelZ);
-                    _randPosX = GenRand(-customRandPosX, customRandPosX);
-                    _randPosY = GenRand(-customRandPosY, customRandPosY);
-                    _randPosZ = GenRand(-customRandPosZ, customRandPosZ);
-                    _particle = new ParticleData();
-                    _particle.Position = new float3((float)_randPosX, (float)_randPosY, (float)_randPosZ);
-                    _particle.Velocity = new float3((float)_randVelX, (float)_randVelY, (float)_randVelZ);
-                    _particle.Gravity = new float3(customGravityX, customGravityY, customGravityZ);
-                    _particle.Life = GenIntRand(customLifeMin, customLifeMax);
-                    _particle.Size = customSize;
-                    _particleList.Add(_particle);
+                _randVelX = GenRand(-customRandVelX, customRandVelX);
+                _randVelY = GenRand(-customRandVelY, customRandVelY);
+                _randVelZ = GenRand(-customRandVelZ, customRandVelZ);
+                _randPosX = GenRand(-customRandPosX, customRandPosX);
+                _randPosY = GenRand(-customRandPosY, customRandPosY);
+                _randPosZ = GenRand(-customRandPosZ, customRandPosZ);
+                _particle = new ParticleData();
+                _particle.Position = new float3((float) _randPosX, (float) _randPosY, (float) _randPosZ);
+                _particle.Velocity = new float3((float) _randVelX, (float) _randVelY, (float) _randVelZ);
+                _particle.Gravity = new float3(customGravityX, customGravityY, customGravityZ);
+                _particle.Life = GenIntRand(customLifeMin, customLifeMax);
+                _particle.Size = customSize;
+                _particleList.Add(_particle);
             }
-
 
             // Partikel erstellen und Wert zuweisen
             for (int k = 0; k < _particleList.Count; k++)
             {
-
                 ParticleData t = _particleList[k];
                 float3 currentPos = t.Position;
                 float currentSize = t.Size;
 
-                
+
                 if (t.Life > 0)
                 {
-                    vertices[k * 4 + 0] = currentPos; 
-                    vertices[k * 4 + 1] = currentPos;
-                    vertices[k * 4 + 2] = currentPos;
-                    vertices[k * 4 + 3] = currentPos;
+                    vertices[k*4 + 0] = currentPos;
+                    vertices[k*4 + 1] = currentPos;
+                    vertices[k*4 + 2] = currentPos;
+                    vertices[k*4 + 3] = currentPos;
 
-                    uVs[k * 4 + 0] = new float2(currentSize, 0);
-                    uVs[k * 4 + 1] = new float2(currentSize, currentSize);
-                    uVs[k * 4 + 2] = new float2(0, currentSize);
-                    uVs[k * 4 + 3] = new float2(0, 0);
+                    uVs[k*4 + 0] = new float2(currentSize, 0);
+                    uVs[k*4 + 1] = new float2(currentSize, currentSize);
+                    uVs[k*4 + 2] = new float2(0, currentSize);
+                    uVs[k*4 + 3] = new float2(0, 0);
 
-                    normals[k * 4 + 0] = new float3(0, 0, 1);
-                    normals[k * 4 + 1] = new float3(0, 0, 1);
-                    normals[k * 4 + 2] = new float3(0, 0, 1);
-                    normals[k * 4 + 3] = new float3(0, 0, 1);
+                    normals[k*4 + 0] = new float3(0, 0, 1);
+                    normals[k*4 + 1] = new float3(0, 0, 1);
+                    normals[k*4 + 2] = new float3(0, 0, 1);
+                    normals[k*4 + 3] = new float3(0, 0, 1);
 
-                    triangles[k * 6 + 0] = (short)(k * 4 + 0);
-                    triangles[k * 6 + 1] = (short)(k * 4 + 1);
-                    triangles[k * 6 + 2] = (short)(k * 4 + 2);
-                    triangles[k * 6 + 3] = (short)(k * 4 + 0);
-                    triangles[k * 6 + 4] = (short)(k * 4 + 2);
-                    triangles[k * 6 + 5] = (short)(k * 4 + 3);
+                    triangles[k*6 + 0] = (short) (k*4 + 0);
+                    triangles[k*6 + 1] = (short) (k*4 + 1);
+                    triangles[k*6 + 2] = (short) (k*4 + 2);
+                    triangles[k*6 + 3] = (short) (k*4 + 0);
+                    triangles[k*6 + 4] = (short) (k*4 + 2);
+                    triangles[k*6 + 5] = (short) (k*4 + 3);
                 }
-                
- 
             }
+
             _particleMesh.Vertices = vertices;
             _particleMesh.Triangles = triangles;
             _particleMesh.Normals = normals;
@@ -160,11 +148,11 @@ namespace Fusee.Engine
             for (int i = 0; i < _particleList.Count; i++)
             {
                 ParticleData _changeParticle = _particleList[i];
-                _changeParticle.Position += (float)deltaTime * _changeParticle.Velocity;
+                _changeParticle.Position += (float) deltaTime*_changeParticle.Velocity;
                 _changeParticle.Velocity -= _changeParticle.Gravity;
-               // _changeParticle.Position *= float4x4.CreateRotationZ(0.01f);
+                // _changeParticle.Position *= float4x4.CreateRotationZ(0.01f);
                 //_changeParticle.Velocity *= float4x4.CreateRotationZ(360.0f);
-                
+
                 //Skalierung
                 /*if (_changeParticle.Size >= 0.0f)
                 {
@@ -176,33 +164,27 @@ namespace Fusee.Engine
 
                 if (_changeParticle.Life != 0)
                 {
-                    _changeParticle.Life -= 1;                    
+                    _changeParticle.Life -= 1;
                 }
                 _particleList[i] = _changeParticle;
                 if (_changeParticle.Life == 0)
                 {
                     _particleList.Remove(_changeParticle);
                 }
-
             }
-            
-
-
         }
 
 
-        static double GenRand(double one, double two)
+        private static double GenRand(double one, double two)
         {
             Random rand = _rnd;
-            return one + rand.NextDouble() * (two - one);
+            return one + rand.NextDouble()*(two - one);
         }
-        static int GenIntRand(int one, int two)
+
+        private static int GenIntRand(int one, int two)
         {
             Random rand = _rnd;
             return rand.Next(one, two);
         }
-
-
-
     }
 }
--- src/Engine/Examples/ParticleSystem/ParticleSystem.cs
+++ src/Engine/Examples/ParticleSystem/ParticleSystem.cs
@@ -2,7 +2,6 @@ using System;
 using Fusee.Engine;
 using Fusee.Math;
 
-
 namespace Examples.ParticleSystem
 {
     [FuseeApplication(Name = "Particle System", Description = "A very simple example.")]
@@ -37,15 +36,14 @@ namespace Examples.ParticleSystem
                 float rad_angle = angle*PI/180.0;
 
                 vec4 vPos = FUSEE_MV * vec4(fuVertex, 1.0);
-                vPos = vPos + 100*vec4(fuUV, 0, 0);                
+                vPos = vPos + 100.0*vec4(fuUV, 0, 0);                
                 gl_Position = FUSEE_P * vPos;
                 vNormal = mat3(FUSEE_ITMV[0].xyz, FUSEE_ITMV[1].xyz, FUSEE_ITMV[2].xyz) * fuNormal;
-                vUV.x = (fuUV.x == 0) ? 0 : 1;
-                vUV.y = (fuUV.y == 0) ? 0 : 1;
+                vUV.x = (fuUV.x != 0.0) ? 1.0 : 0.0;
+                vUV.y = (fuUV.y != 0.0) ? 1.0 : 0.0;
 
                 //vPos.x  = vPos.x*cos(rad_angle) - vPos.y*sin(rad_angle);
                // vPos.y = vPos.y*cos(rad_angle) + vPos.x*sin(rad_angle);
-          
             }";
 
         public string PsSimpleTexture = @"
@@ -74,8 +72,6 @@ namespace Examples.ParticleSystem
 
         #endregion
 
- 
-        
         // angle variables
         private static float _angleHorz, _angleVert, _angleVelHorz, _angleVelVert;
 
@@ -105,7 +101,8 @@ namespace Examples.ParticleSystem
             RC.ClearColor = new float4(0.7f, 0.7f, 1, 1);
 
             //_particleEmitter = new ParticleEmitter(500, 200, 700, 1.0f, 5.0f, 5.0f, 5.0f, 10.0, 10.0, 10.0, 0.0f, -8.5f, 0.0f);
-            _particleEmitter = new ParticleEmitter(705, 800, 1200, 1.0f, 4.0f, 4.0f, 4.0f, 0.0f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f);
+            _particleEmitter = new ParticleEmitter(705, 800, 1200, 1.0f, 4.0f, 4.0f, 4.0f, 0.0f, 0.0f, 5.0f, 0.0f, 0.0f,
+                0.0f);
             //_particleEmitter2 = new ParticleEmitter(1200, 100000, 100000, 1.0f, 30.0f, 1.0f, 30.0f, 0.1, 0.0, 0.1, 0.0f, 0.0f, 0.0f);
             // initialize the variables
             //_meshTea = new ParticleEmitter();//MeshReader.LoadMesh(@"Assets/Teapot.obj.model");
@@ -135,24 +132,21 @@ namespace Examples.ParticleSystem
                 SourceBlend = Blend.SourceAlpha,
                 DestinationBlend = Blend.InverseSourceAlpha
             });
-
-
         }
 
         // is called once a frame
         public override void RenderAFrame()
         {
-
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
             // move per mouse
             if (Input.Instance.IsButton(MouseButtons.Left))
             {
-                _angleVelHorz = RotationSpeed * Input.Instance.GetAxis(InputAxis.MouseX);
-                _angleVelVert = RotationSpeed * Input.Instance.GetAxis(InputAxis.MouseY);
+                _angleVelHorz = RotationSpeed*Input.Instance.GetAxis(InputAxis.MouseX);
+                _angleVelVert = RotationSpeed*Input.Instance.GetAxis(InputAxis.MouseY);
             }
             else
             {
-                var curDamp = (float)Math.Exp(-Damping * Time.Instance.DeltaTime);
+                var curDamp = (float) Math.Exp(-Damping*Time.Instance.DeltaTime);
                 _angleVelHorz *= curDamp;
                 _angleVelVert *= curDamp;
             }
@@ -162,40 +156,42 @@ namespace Examples.ParticleSystem
 
             // move per keyboard
             if (Input.Instance.IsKey(KeyCodes.Left))
-                _angleHorz -= RotationSpeed * (float)Time.Instance.DeltaTime;
+                _angleHorz -= RotationSpeed*(float) Time.Instance.DeltaTime;
 
             if (Input.Instance.IsKey(KeyCodes.Right))
-                _angleHorz += RotationSpeed * (float)Time.Instance.DeltaTime;
+                _angleHorz += RotationSpeed*(float) Time.Instance.DeltaTime;
 
             if (Input.Instance.IsKey(KeyCodes.Up))
-                _angleVert -= RotationSpeed * (float)Time.Instance.DeltaTime;
+                _angleVert -= RotationSpeed*(float) Time.Instance.DeltaTime;
 
             if (Input.Instance.IsKey(KeyCodes.Down))
-                _angleVert += RotationSpeed * (float)Time.Instance.DeltaTime;
+                _angleVert += RotationSpeed*(float) Time.Instance.DeltaTime;
 
-            var mtxRot = float4x4.CreateRotationY(_angleHorz) * float4x4.CreateRotationX(_angleVert);
+            var mtxRot = float4x4.CreateRotationY(_angleHorz)*float4x4.CreateRotationX(_angleVert);
             var mtxCam = float4x4.LookAt(0, 200, 500, 0, 0, 0, 0, 1, 0);
 
             // first mesh
             RC.ModelView = float4x4.CreateTranslation(0, -50, 0)*mtxRot*float4x4.CreateTranslation(-150, 0, 0)*mtxCam;
-            RC.ModelView = new float4x4(15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 1) * mtxRot * float4x4.CreateTranslation(-150, 0, 0) * mtxCam;
+            RC.ModelView = new float4x4(15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 1)*mtxRot*
+                           float4x4.CreateTranslation(-150, 0, 0)*mtxCam;
 
             RC.SetShader(_spTexture);
-       RC.SetShaderParamTexture(_textureParam, _iTex);
+            RC.SetShaderParamTexture(_textureParam, _iTex);
 
             _particleEmitter.Tick(Time.Instance.DeltaTime);
-           // _particleEmitter2.Tick(Time.Instance.DeltaTime);
+            // _particleEmitter2.Tick(Time.Instance.DeltaTime);
             RC.Render(_particleEmitter.ParticleMesh);
-           // RC.Render(_particleEmitter2.ParticleMesh);
+            // RC.Render(_particleEmitter2.ParticleMesh);
 
 
-          // second mesh
-           RC.ModelView = mtxRot * float4x4.CreateTranslation(150, 0, 0) * mtxCam;
-           RC.ModelView = new float4x4(15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 1) * mtxRot * float4x4.CreateTranslation(150, 0, 0) * mtxCam;
-           RC.SetShader(_spColor);
-              //RC.SetShaderParamTexture(_textureParam, _iTex);
-           RC.SetShaderParam(_colorParam, new float4(1, 1, 1, 1));
-           RC.Render(_meshFace);
+            // second mesh
+            RC.ModelView = mtxRot*float4x4.CreateTranslation(150, 0, 0)*mtxCam;
+            RC.ModelView = new float4x4(15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 0, 1)*mtxRot*
+                           float4x4.CreateTranslation(150, 0, 0)*mtxCam;
+            RC.SetShader(_spColor);
+            //RC.SetShaderParamTexture(_textureParam, _iTex);
+            RC.SetShaderParam(_colorParam, new float4(1, 1, 1, 1));
+            RC.Render(_meshFace);
 
             // swap buffers
             Present();
@@ -206,7 +202,7 @@ namespace Examples.ParticleSystem
         {
             RC.Viewport(0, 0, Width, Height);
 
-            var aspectRatio = Width / (float)Height;
+            var aspectRatio = Width/(float) Height;
             RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 5000);
         }
 
