8ca95fa5c9edb579af3de390b2649b36de7d06bb
Denis Dik <denis.dik@hs-furtwangen.de>
2014-02-09 21:48:49 +0100

    Merge cleanup.
    Signed-off-by: Denis Dik <denis.dik@hs-furtwangen.de>

--- src/Engine/Examples/MageExample/Main.cs
+++ src/Engine/Examples/MageExample/Main.cs
@@ -1,6 +1,5 @@
 ﻿using Fusee.Engine;
 using Fusee.Math;
-using System.IO;
 
 namespace Examples.MageExample
 {
@@ -11,118 +10,112 @@ namespace Examples.MageExample
 
         private const string VsBump = @"
 
-            attribute vec4 fuColor;
-            attribute vec3 fuVertex;
-            attribute vec3 fuNormal;
-            attribute vec2 fuUV;
-       
-            uniform mat4 FUSEE_MVP;
-            uniform mat4 FUSEE_ITMV;
+attribute vec4 fuColor;
+attribute vec3 fuVertex;
+attribute vec3 fuNormal;
+attribute vec2 fuUV;
+uniform mat4 FUSEE_MVP;
+uniform mat4 FUSEE_ITMV;
 
-            uniform vec4 FUSEE_L0_AMBIENT;
-            uniform vec4 FUSEE_L1_AMBIENT;
+uniform vec4 FUSEE_L0_AMBIENT;
+uniform vec4 FUSEE_L1_AMBIENT;
 
-            uniform float FUSEE_L0_ACTIVE;
-            uniform float FUSEE_L1_ACTIVE;
+uniform float FUSEE_L0_ACTIVE;
+uniform float FUSEE_L1_ACTIVE;
 
-            varying vec2 vUV;
-            varying vec3 lightDir[8];
-            varying vec3 vNormal;
-            varying vec4 endAmbient;
-            varying vec3 eyeVector;
+varying vec2 vUV;
+varying vec3 lightDir[8];
+varying vec3 vNormal;
+varying vec4 endAmbient;
+varying vec3 eyeVector;
 
-            vec3 vPos;
- 
-            void main()
-            {
-                vUV = fuUV;
+vec3 vPos;
+void main()
+{
+vUV = fuUV;
 
-                vNormal = normalize(mat3(FUSEE_ITMV[0].xyz, FUSEE_ITMV[1].xyz, FUSEE_ITMV[2].xyz) * fuNormal);
-                eyeVector = mat3(FUSEE_MVP[0].xyz, FUSEE_MVP[1].xyz, FUSEE_MVP[2].xyz) * -fuVertex;
-      
-                endAmbient = vec4(0,0,0,0);
+vNormal = normalize(mat3(FUSEE_ITMV[0].xyz, FUSEE_ITMV[1].xyz, FUSEE_ITMV[2].xyz) * fuNormal);
+eyeVector = mat3(FUSEE_MVP[0].xyz, FUSEE_MVP[1].xyz, FUSEE_MVP[2].xyz) * -fuVertex;
+endAmbient = vec4(0,0,0,0);
 
-                if(FUSEE_L0_ACTIVE == 1.0) {
-                    endAmbient += FUSEE_L0_AMBIENT;
-                }
+if(FUSEE_L0_ACTIVE == 1.0) {
+endAmbient += FUSEE_L0_AMBIENT;
+}
 
-                if(FUSEE_L1_ACTIVE == 1.0) {
-                    endAmbient += FUSEE_L1_AMBIENT;
-                }
+if(FUSEE_L1_ACTIVE == 1.0) {
+endAmbient += FUSEE_L1_AMBIENT;
+}
 
-                gl_Position = FUSEE_MVP * vec4(fuVertex, 1.0);
-            }";
+gl_Position = FUSEE_MVP * vec4(fuVertex, 1.0);
+}";
 
 
         private const string PsBump = @"
-            #ifdef GL_ES
-                precision highp float;
-            #endif
+#ifdef GL_ES
+precision highp float;
+#endif
 
-            uniform sampler2D texture1;
-            uniform sampler2D normalTex;
-            uniform float specularLevel;
+uniform sampler2D texture1;
+uniform sampler2D normalTex;
+uniform float specularLevel;
 
-            uniform vec4 FUSEE_L0_SPECULAR;
-            uniform vec4 FUSEE_L1_SPECULAR;
+uniform vec4 FUSEE_L0_SPECULAR;
+uniform vec4 FUSEE_L1_SPECULAR;
 
-            uniform float FUSEE_L0_ACTIVE;
-            uniform float FUSEE_L1_ACTIVE;
+uniform float FUSEE_L0_ACTIVE;
+uniform float FUSEE_L1_ACTIVE;
 
-            uniform vec4 FUSEE_L0_DIFFUSE;
-            uniform vec4 FUSEE_L1_DIFFUSE;
+uniform vec4 FUSEE_L0_DIFFUSE;
+uniform vec4 FUSEE_L1_DIFFUSE;
 
-            uniform vec3 FUSEE_L0_POSITION;
-            uniform vec3 FUSEE_L1_POSITION;
+uniform vec3 FUSEE_L0_POSITION;
+uniform vec3 FUSEE_L1_POSITION;
 
-            uniform vec3 FUSEE_L0_DIRECTION;
-            uniform vec3 FUSEE_L1_DIRECTION;
+uniform vec3 FUSEE_L0_DIRECTION;
+uniform vec3 FUSEE_L1_DIRECTION;
 
-            varying vec3 vNormal;
-            varying vec2 vUV;
-            varying vec4 endAmbient;
-            varying vec3 eyeVector;
- 
-            void main()
-            {       
-                float maxVariance = 2.0;
-                float minVariance = maxVariance/2.0;
+varying vec3 vNormal;
+varying vec2 vUV;
+varying vec4 endAmbient;
+varying vec3 eyeVector;
+void main()
+{
+float maxVariance = 2.0;
+float minVariance = maxVariance/2.0;
 
-                vec3 tempNormal = vNormal + normalize(texture2D(normalTex, vUV).rgb * maxVariance - minVariance);
- 
-                vec4 endSpecular = vec4(0,0,0,0);
-                vec4 tempTexSpecular = texture2D(texture1, vUV);
+vec3 tempNormal = vNormal + normalize(texture2D(normalTex, vUV).rgb * maxVariance - minVariance);
+vec4 endSpecular = vec4(0,0,0,0);
+vec4 tempTexSpecular = texture2D(texture1, vUV);
 
-                if(FUSEE_L0_ACTIVE == 1.0 ) {
-                    vec3 vHalfVector = normalize(normalize(eyeVector) - normalize(eyeVector - FUSEE_L0_POSITION));
-                    float L3NdotHV = max(min(dot(normalize(tempNormal), vHalfVector),1.0), 0.0);
-                    float shine = pow(L3NdotHV, specularLevel) * 16.0 * tempTexSpecular.z;
-                    endSpecular += FUSEE_L0_SPECULAR * shine;
-                }
+if(FUSEE_L0_ACTIVE == 1.0 ) {
+vec3 vHalfVector = normalize(normalize(eyeVector) - normalize(eyeVector - FUSEE_L0_POSITION));
+float L3NdotHV = max(min(dot(normalize(tempNormal), vHalfVector),1.0), 0.0);
+float shine = pow(L3NdotHV, specularLevel) * 16.0 * tempTexSpecular.z;
+endSpecular += FUSEE_L0_SPECULAR * shine;
+}
 
-                if(FUSEE_L1_ACTIVE == 1.0) {
-                    vec3 vHalfVector = normalize(normalize(eyeVector) - normalize(eyeVector - FUSEE_L1_POSITION));
-                    float L3NdotHV = max(dot(normalize(tempNormal), vHalfVector), 0.0);
-                    float shine = pow(L3NdotHV, specularLevel) * 16.0 * tempTexSpecular.z;
-                    endSpecular += FUSEE_L1_SPECULAR * shine;
-                }
-    
-                vec4 endIntensity = vec4(0,0,0,0);
+if(FUSEE_L1_ACTIVE == 1.0) {
+vec3 vHalfVector = normalize(normalize(eyeVector) - normalize(eyeVector - FUSEE_L1_POSITION));
+float L3NdotHV = max(dot(normalize(tempNormal), vHalfVector), 0.0);
+float shine = pow(L3NdotHV, specularLevel) * 16.0 * tempTexSpecular.z;
+endSpecular += FUSEE_L1_SPECULAR * shine;
+}
+vec4 endIntensity = vec4(0,0,0,0);
 
-                if(FUSEE_L0_ACTIVE == 1.0) {
-                    float intensity = max(dot(-normalize(FUSEE_L0_DIRECTION),normalize(tempNormal)),0.0);
-                    endIntensity += intensity * FUSEE_L0_DIFFUSE;
-                }
+if(FUSEE_L0_ACTIVE == 1.0) {
+float intensity = max(dot(-normalize(FUSEE_L0_DIRECTION),normalize(tempNormal)),0.0);
+endIntensity += intensity * FUSEE_L0_DIFFUSE;
+}
 
-                if(FUSEE_L1_ACTIVE == 1.0) {
-                    float intensity = max(dot(-normalize(FUSEE_L1_DIRECTION),normalize(tempNormal)),0.0);
-                    endIntensity += intensity * FUSEE_L1_DIFFUSE;
-                }
+if(FUSEE_L1_ACTIVE == 1.0) {
+float intensity = max(dot(-normalize(FUSEE_L1_DIRECTION),normalize(tempNormal)),0.0);
+endIntensity += intensity * FUSEE_L1_DIFFUSE;
+}
 
-                endIntensity += endSpecular;
-                endIntensity += endAmbient; 
-                gl_FragColor = texture2D(texture1, vUV) * endIntensity; 
-            }";
+endIntensity += endSpecular;
+endIntensity += endAmbient;
+gl_FragColor = texture2D(texture1, vUV) * endIntensity;
+}";
 
         #endregion
 
@@ -149,37 +142,12 @@ namespace Examples.MageExample
         public override void Init()
         {
             RC.ClearColor = new float4(0.5f, 0.5f, 0.5f, 1);
-            MySerializer ser = new MySerializer();
+
             // load meshes
-            //Body = MeshReader.LoadMesh(@"Assets/mageBodyOBJ.obj.model");
-            //GloveL = MeshReader.LoadMesh(@"Assets/mageGloveLOBJ.obj.model");
-            //GloveR = MeshReader.LoadMesh(@"Assets/mageGloveROBJ.obj.model");
-            // Laden
-            using (var file = File.OpenRead(@"Assets/body.bin"))
-            {
-                Body = ser.Deserialize(file, null, typeof(Mesh)) as Mesh;
-            }
-            using (var file = File.OpenRead(@"Assets/gloveR.bin"))
-            {
-                GloveR = ser.Deserialize(file, null, typeof(Mesh)) as Mesh;
-            }
-            using (var file = File.OpenRead(@"Assets/gloveL.bin"))
-            {
-                GloveL = ser.Deserialize(file, null, typeof(Mesh)) as Mesh;
-            }
-            // Speichern
-            //using (var file = File.Create(@"Assets/body.bin"))
-            //{
-            //    ser.Serialize(file, Body);
-            //}
-            //using (var file = File.Create(@"Assets/gloveR.bin"))
-            //{
-            //    ser.Serialize(file, GloveR);
-            //}
-            //using (var file = File.Create(@"Assets/gloveL.bin"))
-            //{
-            //    ser.Serialize(file, GloveL);
-            //}
+            Body = MeshReader.LoadMesh(@"Assets/mageBodyOBJ.obj.model");
+            GloveL = MeshReader.LoadMesh(@"Assets/mageGloveLOBJ.obj.model");
+            GloveR = MeshReader.LoadMesh(@"Assets/mageGloveROBJ.obj.model");
+
             // set up shader, lights and textures
             var spBody = RC.CreateShader(VsBump, PsBump);
             RC.SetShader(spBody);
@@ -226,35 +194,35 @@ namespace Examples.MageExample
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
             // mouse and input control
-            _angleVelHorz = -1.0f*(float) Time.Instance.DeltaTime;
+            _angleVelHorz = -1.0f * (float)Time.Instance.DeltaTime;
             _angleVelVert = 0.0f;
 
             if (Input.Instance.IsButton(MouseButtons.Left))
             {
-                _angleVelHorz = _rotationSpeed*Input.Instance.GetAxis(InputAxis.MouseX);
-                _angleVelVert = _rotationSpeed*Input.Instance.GetAxis(InputAxis.MouseY);
+                _angleVelHorz = _rotationSpeed * Input.Instance.GetAxis(InputAxis.MouseX);
+                _angleVelVert = _rotationSpeed * Input.Instance.GetAxis(InputAxis.MouseY);
             }
 
             _angleHorz += _angleVelHorz;
             _angleVert += _angleVelVert;
 
             if (Input.Instance.IsKey(KeyCodes.Left))
-                _angleHorz -= _rotationSpeed*(float) Time.Instance.DeltaTime;
+                _angleHorz -= _rotationSpeed * (float)Time.Instance.DeltaTime;
 
             if (Input.Instance.IsKey(KeyCodes.Right))
-                _angleHorz += _rotationSpeed*(float) Time.Instance.DeltaTime;
+                _angleHorz += _rotationSpeed * (float)Time.Instance.DeltaTime;
 
             if (Input.Instance.IsKey(KeyCodes.Up))
-                _angleVert -= _rotationSpeed*(float) Time.Instance.DeltaTime;
+                _angleVert -= _rotationSpeed * (float)Time.Instance.DeltaTime;
 
             if (Input.Instance.IsKey(KeyCodes.Down))
-                _angleVert += _rotationSpeed*(float) Time.Instance.DeltaTime;
+                _angleVert += _rotationSpeed * (float)Time.Instance.DeltaTime;
 
-            var mtxRot = float4x4.CreateRotationY(_angleHorz)*float4x4.CreateRotationX(_angleVert);
+            var mtxRot = float4x4.CreateRotationY(_angleHorz) * float4x4.CreateRotationX(_angleVert);
             var mtxCam = float4x4.LookAt(0, 400, 600, 0, 0, 0, 0, 1, 0);
 
             // body
-            RC.ModelView = float4x4.CreateTranslation(0, -200f, 0)*mtxRot*mtxCam;
+            RC.ModelView = float4x4.CreateTranslation(0, -200f, 0) * mtxRot * mtxCam;
 
             RC.SetShaderParamTexture(_texture1ParamBody, _iTex);
             RC.SetShaderParamTexture(_texture2ParamBody, _iTex2);
@@ -263,7 +231,7 @@ namespace Examples.MageExample
             RC.Render(Body);
 
             // left and right glove
-            RC.ModelView = float4x4.CreateTranslation(0, -200f, 0)*mtxRot*mtxCam;
+            RC.ModelView = float4x4.CreateTranslation(0, -200f, 0) * mtxRot * mtxCam;
 
             RC.SetShaderParamTexture(_texture1ParamBody, _iTexGlove);
             RC.SetShaderParamTexture(_texture2ParamBody, _iTex2Glove);
@@ -280,7 +248,7 @@ namespace Examples.MageExample
         {
             RC.Viewport(0, 0, Width, Height);
 
-            var aspectRatio = Width/(float) Height;
+            var aspectRatio = Width / (float)Height;
             RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 10000);
         }
 
--- src/Engine/Examples/Simple/Simple.cs
+++ src/Engine/Examples/Simple/Simple.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Fusee.Engine;
 using Fusee.Math;
-using System.IO;
 
 namespace Examples.Simple
 {
@@ -15,7 +14,8 @@ namespace Examples.Simple
         private const float Damping = 0.92f;
 
         // model variables
-        private Mesh _meshTea, _meshFace;
+        private Mesh _meshFace;
+        private Mesh _meshTea;
 
         // variables for shader
         private ShaderProgram _spColor;
@@ -29,34 +29,14 @@ namespace Examples.Simple
         // is called on startup
         public override void Init()
         {
-            
             RC.ClearColor = new float4(1, 1, 1, 1);
+
             // initialize the variables
-            //_meshTea = MeshReader.LoadMesh(@"Assets/Teapot.obj.model");
-            //_meshFace = MeshReader.LoadMesh(@"Assets/Face.obj.model");
-
-            MySerializer ser = new MySerializer();
-            
-            // Speichern
-            /*using (var file = File.Create(@"Assets/face.bin"))
-            {
-                ser.Serialize(file, _meshFace);
-            }*/
-
-
-            // Laden
-            using (var file = File.OpenRead(@"Assets/face.bin"))
-            {
-                _meshFace = ser.Deserialize(file, null, typeof(Mesh)) as Mesh;
-            }
-
-            using (var file = File.OpenRead(@"Assets/teekanne.bin"))
-            {
-               _meshTea = ser.Deserialize(file,null,typeof(Mesh)) as Mesh;
-            }
+            _meshTea = MeshReader.LoadMesh(@"Assets/Teapot.obj.model");
+            _meshFace = MeshReader.LoadMesh(@"Assets/Face.obj.model");
 
             _spColor = MoreShaders.GetDiffuseColorShader(RC);
-            _spTexture = MoreShaders.GetDiffuseTextureShader(RC);
+            _spTexture = MoreShaders.GetTextureShader(RC);
 
             _colorParam = _spColor.GetShaderParam("color");
             _textureParam = _spTexture.GetShaderParam("texture1");
@@ -69,17 +49,17 @@ namespace Examples.Simple
         // is called once a frame
         public override void RenderAFrame()
         {
-            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
             // move per mouse
             if (Input.Instance.IsButton(MouseButtons.Left))
             {
-                _angleVelHorz = RotationSpeed*Input.Instance.GetAxis(InputAxis.MouseX);
-                _angleVelVert = RotationSpeed*Input.Instance.GetAxis(InputAxis.MouseY);
+                _angleVelHorz = RotationSpeed * Input.Instance.GetAxis(InputAxis.MouseX);
+                _angleVelVert = RotationSpeed * Input.Instance.GetAxis(InputAxis.MouseY);
             }
             else
             {
-                var curDamp = (float) Math.Exp(-Damping*Time.Instance.DeltaTime);
+                var curDamp = (float)Math.Exp(-Damping * Time.Instance.DeltaTime);
 
                 _angleVelHorz *= curDamp;
                 _angleVelVert *= curDamp;
@@ -90,22 +70,22 @@ namespace Examples.Simple
 
             // move per keyboard
             if (Input.Instance.IsKey(KeyCodes.Left))
-                _angleHorz -= RotationSpeed*(float) Time.Instance.DeltaTime;
+                _angleHorz -= RotationSpeed * (float)Time.Instance.DeltaTime;
 
             if (Input.Instance.IsKey(KeyCodes.Right))
-                _angleHorz += RotationSpeed*(float) Time.Instance.DeltaTime;
+                _angleHorz += RotationSpeed * (float)Time.Instance.DeltaTime;
 
             if (Input.Instance.IsKey(KeyCodes.Up))
-                _angleVert -= RotationSpeed*(float) Time.Instance.DeltaTime;
+                _angleVert -= RotationSpeed * (float)Time.Instance.DeltaTime;
 
             if (Input.Instance.IsKey(KeyCodes.Down))
-                _angleVert += RotationSpeed*(float) Time.Instance.DeltaTime;
+                _angleVert += RotationSpeed * (float)Time.Instance.DeltaTime;
 
-            var mtxRot = float4x4.CreateRotationY(_angleHorz)*float4x4.CreateRotationX(_angleVert);
+            var mtxRot = float4x4.CreateRotationY(_angleHorz) * float4x4.CreateRotationX(_angleVert);
             var mtxCam = float4x4.LookAt(0, 200, 500, 0, 0, 0, 0, 1, 0);
 
             // first mesh
-            RC.ModelView = float4x4.CreateTranslation(0, -50, 0)*mtxRot*float4x4.CreateTranslation(-150, 0, 0)*mtxCam;
+            RC.ModelView = mtxRot * float4x4.CreateTranslation(-150, 0, 0) * mtxCam;
 
             RC.SetShader(_spColor);
             RC.SetShaderParam(_colorParam, new float4(0.5f, 0.8f, 0, 1));
@@ -113,12 +93,13 @@ namespace Examples.Simple
             RC.Render(_meshTea);
 
             // second mesh
-            RC.ModelView = mtxRot*float4x4.CreateTranslation(150, 0, 0)*mtxCam;
+            RC.ModelView = mtxRot * float4x4.CreateTranslation(150, 0, 0) * mtxCam;
 
             RC.SetShader(_spTexture);
             RC.SetShaderParamTexture(_textureParam, _iTex);
 
             RC.Render(_meshFace);
+
 
             // swap buffers
             Present();
@@ -129,7 +110,7 @@ namespace Examples.Simple
         {
             RC.Viewport(0, 0, Width, Height);
 
-            var aspectRatio = Width/(float) Height;
+            var aspectRatio = Width / (float)Height;
             RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 5000);
         }
 
