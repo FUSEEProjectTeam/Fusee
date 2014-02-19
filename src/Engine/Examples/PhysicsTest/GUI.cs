7c1892adabffd5a92b95d3899db415f9594c7dd2
Linda Schey <lindaschey@t-online.de>
2014-02-19 13:22:46 +0100

    Final fixes for Thesis Final Example

--- src/Engine/Examples/PhysicsTest/GUI.cs
+++ src/Engine/Examples/PhysicsTest/GUI.cs
@@ -14,7 +14,7 @@ namespace Examples.PhysicsTest
 
         private readonly GUIPanel _guiPanel;
 
-        private GUIText _fps, _numberRb, _shapeType, _shapes;
+        private GUIText _fps, _numberRb, _shapeType, _shapes, _info1, _info2, _info3;
 
         private readonly float4 _color1 = new float4(1f, 1f, 1f, 1);
         private readonly float4 _color2 = new float4(0, 0, 0, 1);
@@ -31,15 +31,20 @@ namespace Examples.PhysicsTest
             _guiHandler.AttachToContext(rc);
 
             //Start Pannel Init
-            _guiPanel = new GUIPanel("PhysicsTest", _fontMedium, 10, 10, 300, 150);
+            _guiPanel = new GUIPanel("PhysicsTest", _fontMedium, 10, 10, 330, 250);
 
 
-
+            _info1 = new GUIText("Über die Tasten des Nummernblocks  " , _fontMedium, 30, 170, _color2);
+            _info2 = new GUIText("(1-4) kann zwischen 4 verschiedenen ", _fontMedium, 30, 190, _color2);
+            _info3 = new GUIText("Scenen gewechselt werden", _fontMedium, 30, 210, _color2);
             _fps = new GUIText("FPS", _fontMedium, 30, 55, _color2);
             _numberRb = new GUIText("Rigidbodies: ", _fontMedium, 30, 85, _color2);
             _shapeType = new GUIText("Collision Shapes: ", _fontMedium, 30, 115, _color2);
             _shapes = new GUIText("", _fontMedium, 50, 135, _color2);
 
+            _guiPanel.ChildElements.Add(_info1);
+            _guiPanel.ChildElements.Add(_info2);
+            _guiPanel.ChildElements.Add(_info3);
             _guiPanel.ChildElements.Add(_fps);
             _guiPanel.ChildElements.Add(_numberRb);
             _guiPanel.ChildElements.Add(_shapeType);
