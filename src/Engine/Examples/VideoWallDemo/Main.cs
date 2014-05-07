6c3952f9bfb7e16428081ec5c00cf86288bc858a
Dominik <dominik.steffen@googlemail.com>
2014-05-07 11:27:27 +0200

    Some more commentary, some more testing. Some little fixes.

--- src/Engine/Examples/VideoWallDemo/Main.cs
+++ src/Engine/Examples/VideoWallDemo/Main.cs
@@ -12,20 +12,21 @@ namespace Examples.WindowSizesDemo
         {
             RC.ClearColor = new float4(0.1f, 0.1f, 0.5f, 1);
 
-            var windowPosX = Screen.PrimaryScreen.Bounds.Width/2;
-            var windowPosY = Screen.PrimaryScreen.Bounds.Height/2;
-            
             // IMPORTANT: You are supposed to use either one SetWindowSize() or VideoWall(). You can't use both.
-            // !!!
-
-            // IMPORTANT: It's possible to resize a window to fit on a video wall or display.
-            // Here are some different variants how to use the method. Uncomment to test.
-            //SetWindowSize(800, 600, false);
-            SetWindowSize(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height/9 * 2, false, 0, 0);
-            //SetWindowSize(Screen.PrimaryScreen.Bounds.Width/9 * 2, Screen.PrimaryScreen.Bounds.Height, true, 0, 0);
-            //SetWindowSize(640, 480, false, windowPosX - 640 / 2, windowPosY - 480 / 2);
-            // IMPORTANT: But it's also possible to let the system do the magic by giving it the number of monitors per axis and specify some border information.
+            // It's possible to resize a window to fit on a video wall or display.
+            // Here are some different variants how to use this functionality. Uncomment to test.
+            //SetWindowSize(800, 600);
+            //SetWindowSize(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height/9 * 2, false, 0, 0);
+            SetWindowSize(Screen.PrimaryScreen.Bounds.Width/9 * 2, Screen.PrimaryScreen.Bounds.Height, true, 0, 0);
+            //SetWindowSize(640, 480, false, Screen.PrimaryScreen.Bounds.Width/2 - 640/2, Screen.PrimaryScreen.Bounds.Height/2 - 480/2);
+            // IMPORTANT: For video Walls or projector matrices it's possible to let the system do the magic by
+            // giving it the number of monitors/projectors per axis and specify some border information.
+            // This only works in pseudo-fullscreen mode a.k.a borderless fullscreen-window.
             //VideoWall(0, 0, true, false);
+
+            // This is the old method. More than this could not be done directly in a project code(?)
+            //Width = 640;
+            //Height = 480;
         }
 
         // is called once a frame
