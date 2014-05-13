db17783606d6f3a8713adb4aa0fc4c2536201769
Dominik <dominik.steffen@googlemail.com>
2014-05-13 16:11:32 +0200

    Improved the example and did some ui testing.

--- src/Engine/Examples/VideoWallDemo/Main.cs
+++ src/Engine/Examples/VideoWallDemo/Main.cs
@@ -7,6 +7,12 @@ namespace Examples.WindowSizesDemo
 {
     public class WindowSizesDemo : RenderCanvas
     {
+        // Vars.
+        private GUIHandler _guiHandler;
+        private Mesh _cube;
+        private ShaderProgram _spcolor;
+        private IShaderParam _colorParam;
+
         // is called on startup
         public override void Init()
         {
@@ -16,8 +22,8 @@ namespace Examples.WindowSizesDemo
             // It's possible to resize a window to fit on a video wall or display.
             // Here are some different variants how to use this functionality. Uncomment to test.
             //SetWindowSize(800, 600);
-            //SetWindowSize(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height/9 * 2, false, 0, 0);
-            SetWindowSize(Screen.PrimaryScreen.Bounds.Width/9 * 2, Screen.PrimaryScreen.Bounds.Height, true, 0, 0);
+            SetWindowSize(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height/9 * 2, true, 0, 0);
+            //SetWindowSize(Screen.PrimaryScreen.Bounds.Width/9 * 2, Screen.PrimaryScreen.Bounds.Height, true, 0, 0);
             //SetWindowSize(640, 480, false, Screen.PrimaryScreen.Bounds.Width/2 - 640/2, Screen.PrimaryScreen.Bounds.Height/2 - 480/2);
             // IMPORTANT: For video Walls or projector matrices it's possible to let the system do the magic by
             // giving it the number of monitors/projectors per axis and specify some border information.
@@ -27,12 +33,39 @@ namespace Examples.WindowSizesDemo
             // This is the old method. More than this could not be done directly in a project code(?)
             //Width = 640;
             //Height = 480;
+
+            // Initialize an ui.
+            _guiHandler = new GUIHandler();
+            _guiHandler.AttachToContext(RC);
+
+            // Create a new element.
+            GUIElement element = new GUIImage("Assets/image.png", 0, 0, -10, 320, 130);
+
+            // Add the element to the gui.
+            _guiHandler.Add(element);
+
+            // Add something to render.
+            _cube = new Cube();
+            _spcolor = MoreShaders.GetDiffuseColorShader(RC);
+            _colorParam = _spcolor.GetShaderParam("color");
         }
 
         // is called once a frame
         public override void RenderAFrame()
         {
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+            
+            // First render 3d elements
+            RC.SetShader(MoreShaders.GetDiffuseColorShader(RC));
+            RC.SetShaderParam(_colorParam, new float4(0.8f, 0.5f, 0, 1));
+            RC.Render(_cube);
+
+            // Projection stuff.
+            var mtxCam = float4x4.LookAt(6, 3, 3, 0, 0, 0, 0, 1, 0);
+            RC.ModelView = mtxCam;
+
+            // Second render gui
+            _guiHandler.RenderGUI();
 
             Present();
         }
@@ -44,6 +77,8 @@ namespace Examples.WindowSizesDemo
 
             var aspectRatio = Width / (float)Height;
             RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 10000);
+
+            _guiHandler.Refresh();
         }
 
         public static void Main()
