4ba48d2ee0e4ef46ebce0679133c23f35296a78d
Alexander Scheurer <mail@aspepex.net>
2015-03-25 15:45:37 +0100

    Forced rebuild for c4d_build

--- src/Uniplug/Cinema4D/C4d/C4d_build.cmd
+++ src/Uniplug/Cinema4D/C4d/C4d_build.cmd
@@ -2,7 +2,7 @@
 for /f "tokens=2*" %%a in ('reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0" /v MSBuildToolsPath') do set "msbpath=%%b"
 if "%msbpath%"=="0x1" goto :errorMsbuildPath
 
-%msbpath%\msbuild.exe %3\C4d.csproj /p:Configuration=%1 /p:Platform=%2
+%msbpath%\msbuild.exe %3\C4d.csproj /t:Clean,Build /p:Configuration=%1 /p:Platform=%2
 
 goto :exit
 
