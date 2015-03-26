d9e58950c3fddb259ab42c3e08237c4a20d40b60
Alexander Scheurer <mail@aspepex.net>
2015-03-26 10:10:28 +0100

    C4d_BUILD Cleanup, Comments and Readme

--- src/Uniplug/Cinema4D/C4d_BUILD/C4d_build.cmd
+++ src/Uniplug/Cinema4D/C4d_BUILD/C4d_build.cmd
@@ -1,12 +1,13 @@
 @echo off &setlocal
+
+rem Checking the registry for the location of msbuild v4.
 for /f "tokens=2*" %%a in ('reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0" /v MSBuildToolsPath') do set "msbpath=%%b"
 if "%msbpath%"=="0x1" goto :errorMsbuildPath
 
+rem Building the C4d project
 %msbpath%\msbuild.exe %3\C4d.csproj /t:Clean,Build /p:Configuration=%1 /p:Platform=%2
 
 goto :exit
-
-rem C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe D:\DataX\GitHub\Fusee\src\Uniplug\Cinema4D\C4d\C4d.csproj /p:Configuration=Debug /p:Platform=x64
 
 :errorMsbuildPath
 echo Error: Could not find MSBuild.exe
