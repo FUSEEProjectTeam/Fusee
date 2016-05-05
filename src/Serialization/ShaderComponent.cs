a6f994e83f436436df9146ba2e1170f7126c4ecc
Moritz <moritz.roetner@hs-furtwangen.de>
2016-05-05 18:31:37 +0200

    added ShaderComponent

---
+++ src/Serialization/ShaderComponent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using ProtoBuf;
+
+namespace Fusee.Serialization
+{
+    /// <summary>
+    /// This class holds the shader definition. If contained within a node, the node's (and potentially child node's)
+    /// geometry is rendered with the specified shader.
+    /// </summary>
+    [ProtoContract]
+    public class ShaderComponent : SceneComponentContainer
+    {
+        /// <summary>
+        /// VS
+        /// </summary>
+        [ProtoMember(1)]
+        public string VS;
+
+        /// <summary>
+        /// PS
+        /// </summary>
+        [ProtoMember(2)]
+        public string PS;
+
+        /// <summary>
+        /// The RenderStates
+        /// </summary>
+        [ProtoMember(3)]
+        public Dictionary<uint, uint> RenderState;
+
+        /// <summary>
+        /// The EffectParameters
+        /// </summary>
+        [ProtoMember(3)]
+        public List<TypeContainer> EffectPrameter;
+
+
+    }
+}
---
+++ src/Serialization/TypeContainer.cs
@@ -0,0 +1,102 @@
+﻿using Fusee.Math.Core;
+using ProtoBuf;
+
+namespace Fusee.Serialization
+{
+    /// <summary>
+    /// Base class to hold all sorts of data type. Derived types specify the type of the value.
+    /// </summary>
+    [ProtoContract]
+    [ProtoInclude(100, typeof(TypeContainerDouble))]
+    [ProtoInclude(101, typeof(TypeContainerInt))]
+    [ProtoInclude(102, typeof(TypeContainerFloat))]
+    [ProtoInclude(103, typeof(TypeContainerFloat2))]
+    [ProtoInclude(104, typeof(TypeContainerFloat3))]
+    [ProtoInclude(105, typeof(TypeContainerFloat4))]
+    public class TypeContainer
+    {
+        /// <summary>
+        /// The Name
+        /// </summary>
+        [ProtoMember(1)]
+        public string Name;
+    }
+
+    /// <summary>
+    /// TypeContainer storing double values.
+    /// </summary>
+    [ProtoContract]
+    public class TypeContainerDouble : TypeContainer
+    {
+        /// <summary>
+        /// The key value combined with a <see cref="TypeContainer.Name"/>.
+        /// </summary>
+        [ProtoMember(1)]
+        public double Value;
+    }
+
+    /// <summary>
+    /// TypeContainer storing int values.
+    /// </summary>
+    [ProtoContract]
+    public class TypeContainerInt : TypeContainer
+    {
+        /// <summary>
+        /// The key value combined with a <see cref="TypeContainer.Name"/>.
+        /// </summary>
+        [ProtoMember(1)]
+        public int Value;
+    }
+
+    /// <summary>
+    /// TypeContainer storing float values.
+    /// </summary>
+    [ProtoContract]
+    public class TypeContainerFloat : TypeContainer
+    {
+        /// <summary>
+        /// The key value combined with a <see cref="TypeContainer.Name"/>.
+        /// </summary>
+        [ProtoMember(1)]
+        public float Value;
+    }
+
+    /// <summary>
+    /// TypeContainer storing float2 values.
+    /// </summary>
+    [ProtoContract]
+    public class TypeContainerFloat2 : TypeContainer
+    {
+        /// <summary>
+        /// The key value combined with a <see cref="TypeContainer.Name"/>.
+        /// </summary>
+        [ProtoMember(1)]
+        public float2 Value;
+    }
+
+    /// <summary>
+    /// TypeContainer storing float3 values.
+    /// </summary>
+    [ProtoContract]
+    public class TypeContainerFloat3 : TypeContainer
+    {
+        /// <summary>
+        /// The key value combined with a <see cref="TypeContainer.Name"/>.
+        /// </summary>
+        [ProtoMember(1)]
+        public float3 Value;
+    }
+
+    /// <summary>
+    /// TypeContainer storing float4 values.
+    /// </summary>
+    [ProtoContract]
+    public class TypeContainerFloat4 : TypeContainer
+    {
+        /// <summary>
+        /// The key value combined with a <see cref="TypeContainer.Name"/>.
+        /// </summary>
+        [ProtoMember(1)]
+        public float4 Value;
+    }
+}
