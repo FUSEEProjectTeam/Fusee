a7e2890dee243fd5fb8116cfcd4ab4fa95e1015d
Sarah Busert <sarah.busert@hs-furtwangen.de>
2017-08-23 15:51:44 +0200

    Replaced custom ext method SkipItem and LastItem with LINQ equivalents

--- src/Base/Common/ExtensionMethods.cs
+++ src/Base/Common/ExtensionMethods.cs
@@ -10,31 +10,6 @@ namespace Fusee.Base.Common
         #region IEnumerable extension methods
 
         /// <summary>
-        /// Bypasses a given number of elements in a sequence and returns the remaining. Alternative to LINQ's Skip().
-        /// </summary>
-        /// <param name="data">Source data</param>
-        /// <param name="count">Number of elements to bypass.</param>
-        /// <typeparam name="T">Type</typeparam>
-        /// <returns></returns>
-        public static IEnumerable<T> SkipItems<T>(this IEnumerable<T> data, int count)
-        {
-            var zwerg = new List<T>();
-            zwerg.AddRange((List<T>)data);
-            var i = 0;
-            
-            foreach (var t in data)
-            {
-                if (i < count)
-                {
-                    zwerg.RemoveAt(0);
-                }
-                else break;
-                i++;
-            }
-            return zwerg;
-        }
-
-        /// <summary>
         /// Returns a specified number of contiguous elements from the start of a sequence. Alternative to LINQ's Take().
         /// </summary>
         /// <param name="data">Source data.</param>
@@ -56,24 +31,6 @@ namespace Fusee.Base.Common
                 i++;
             }
             return items;
-        }
-
-        /// <summary>
-        /// Returns the last item of a sequence. Alternative to LINQ's Last()
-        /// </summary>
-        /// <param name="data">Source data</param>
-        /// <typeparam name="T">Type</typeparam>
-        /// <returns></returns>
-        public static T LastItem<T>(this IEnumerable<T> data)
-        {
-            var temp = default(T);
-            var iEnum = data.GetEnumerator();
-            while (iEnum.MoveNext())
-            {
-                temp = iEnum.Current;
-            }
-
-            return temp;
         }
 
         /// <summary>
