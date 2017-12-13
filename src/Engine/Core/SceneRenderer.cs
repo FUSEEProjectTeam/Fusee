11f69db84ea8e33949bc3f010e2fefbc8f35ff2e
Christoph Müller <chrism at g-graphix.de>
2017-12-13 16:17:44 +0100

    ShaderEffectComponent

--- src/Engine/Core/SceneRenderer.cs
+++ src/Engine/Core/SceneRenderer.cs
@@ -768,6 +768,13 @@ namespace Fusee.Engine.Core
             _state.Effect = effect;
         }
 
+
+        [VisitMethod]
+        public void RenderShaderEffect(ShaderEffectComponent shaderComponent)
+        {
+            _state.Effect = shaderComponent.Effect;
+        }
+
         [VisitMethod]
         public void RenderMesh(MeshComponent meshComponent)
         {
@@ -1115,6 +1122,7 @@ namespace Fusee.Engine.Core
         }
 
 
+ 
         private ShaderEffect BuildMaterialFromShaderComponent(ShaderComponent shaderComponent)
         {
             ShaderEffect shaderEffect;
