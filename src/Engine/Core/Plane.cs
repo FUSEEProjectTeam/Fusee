2c1aac9c9026599fd1aaa338a031df242337e901
Sarah Busert <sarah.busert@hs-furtwangen.de>
2017-12-19 09:18:22 +0100

    Added plane mesh

---
+++ src/Engine/Core/Plane.cs
@@ -0,0 +1,44 @@
+﻿using Fusee.Math.Core;
+
+namespace Fusee.Engine.Core
+{
+    public class Plane : Mesh
+    {
+        public Plane()
+        {
+            #region Fields
+
+            // TODO: Remove redundant vertices
+            Vertices = new[]
+            {
+                new float3 {x = -0.5f, y = -0.5f, z = 0},
+                new float3 {x = -0.5f, y = +0.5f, z = 0},
+                new float3 {x = +0.5f, y = +0.5f, z = 0},
+                new float3 {x = +0.5f, y = -0.5f, z = 0}
+            };
+
+            Triangles = new ushort[]
+            {
+                0, 2, 1, 0, 3, 2
+            };
+
+            Normals = new[]
+            {
+                new float3(0, 0, -1),
+                new float3(0, 0, -1),
+                new float3(0, 0, -1),
+                new float3(0, 0, -1)
+            };
+
+            UVs = new[]
+            {
+                new float2(1, 0),
+                new float2(1, 1),
+                new float2(0, 1),
+                new float2(0, 0)
+            };
+        }
+        #endregion
+    }
+}
+
