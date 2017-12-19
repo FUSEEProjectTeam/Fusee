0713e115ba2bb6bc6f4be1e63c6dba584b3cf72d
Sarah <sarah.busert@hs-furtwangen.de>
2017-12-19 21:48:54 +0100

    Fixed normals calculation for Icosphere

--- src/Engine/Core/Icosphere.cs
+++ src/Engine/Core/Icosphere.cs
@@ -1,14 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Fusee.Math.Core;
 
 namespace Fusee.Engine.Core
 {
+    /// <summary>
+    /// Unit Icosahedron primitive.
+    /// </summary>
     public class Icosahedron : Mesh
     {
+        /// <summary>
+        /// Creates an unit Icosahedron. Used to create a Icosphere.
+        /// </summary>
         public Icosahedron()
         {
-            // create 12 vertices of a icosahedron
+            //Create the 12 vertices of a icosahedron.
             var t = (float)(1.0 + System.Math.Sqrt(5.0)) / 2.0f;
 
             var verts = new List<float3>
@@ -28,15 +34,16 @@ namespace Fusee.Engine.Core
                 new float3(-t, 0, -1),
                 new float3(-t, 0, 1)
             };
-                
-            //Normalize to get unit icosahedron
-            foreach (var vert in verts)
+
+            //Normalize to get the unit icosahedron.
+            for (var i = 0; i < verts.Count; i++)
             {
+                var vert = verts[i];
                 vert.Normalize();
+                verts[i] = vert;
             }
 
             Vertices = verts.ToArray();
-
 
             Triangles = new ushort[]
             {
@@ -63,22 +70,35 @@ namespace Fusee.Engine.Core
             };
 
             Normals = new float3[] { };
-            Normals = CalcNormalHelper.CreateNormals(this).ToArray();
+            CalcNormalHelper.CreateVertexNormals(this);
         }
     }
 
 
-    public class Sphere: Mesh
+    /// <summary>
+    /// Unit Icosphere primitive.
+    /// </summary>
+    public class Icosphere : Mesh
     {
-        private ushort newVertIndex;
+        private ushort _newVertIndex;
         private readonly Dictionary<long, int> _middlePointIndexCache;
         private readonly List<float3> _sphereVertices;
-        
-        public Sphere(int recursionLevel)
+
+        /// <summary>
+        /// Creates an unit Icosphere from an Icosahedron.
+        /// </summary>
+        /// <param name="subdivLevel">Defines the number subdivisions and therefor the number of triangles and the smoothness of the sphere.</param>
+        public Icosphere(int subdivLevel)
         {
+            if (subdivLevel > 6)
+            {
+                //Restrict subdivision level to 6 at max - normals aren't calculated correctly above this value (possibly because of too small values).
+                subdivLevel = 6;
+            }
+
             _sphereVertices = new List<float3>();
             _middlePointIndexCache = new Dictionary<Int64, int>();
-            var sphere = Create(recursionLevel);
+            var sphere = Create(subdivLevel);
 
             Vertices = sphere.Vertices;
             Triangles = sphere.Triangles;
@@ -95,28 +115,28 @@ namespace Fusee.Engine.Core
                 AddNormalizedVertex(vert);
             }
 
-            // refine triangles
+            //Subdevide triangles
             for (var i = 0; i < recursionLevel; i++)
             {
                 var faces2 = new List<ushort>();
-                var tri =  new List<ushort>();
+                var tri = new List<ushort>();
 
                 for (var k = 0; k < mesh.Triangles.Length; k++)
                 {
                     tri.Add(mesh.Triangles[k]);
-                    if(tri.Count != 3) continue;
+                    if (tri.Count != 3) continue;
 
-                    // replace triangle by 4 triangles
+                    //Replace triangle by four triangles.
                     var a = GetMiddlePoint(tri[0], tri[1]);
                     var b = GetMiddlePoint(tri[1], tri[2]);
                     var c = GetMiddlePoint(tri[2], tri[0]);
 
                     var temp = new List<ushort>
                     {
-                        c, a, tri[0],
-                        a, b, tri[1],
-                        b, c, tri[2],
-                        c, b, a
+                        tri[0], a, c,
+                        tri[1], b, a,
+                        tri[2], c, b,
+                        a, b, c
                     };
 
                     faces2.AddRange(temp);
@@ -127,26 +147,25 @@ namespace Fusee.Engine.Core
             }
 
             mesh.Vertices = _sphereVertices.ToArray();
-
-            mesh.Normals = CalcNormalHelper.CreateNormals(mesh).ToArray();
-
+            CalcNormalHelper.CreateVertexNormals(mesh);
+            
             return mesh;
         }
 
-        // Add vertex to mesh, normalize position to be on unit sphere
+        //Add vertex to mesh, normalize position to be on unit sphere.
         private ushort AddNormalizedVertex(float3 p)
         {
             p.Normalize();
             _sphereVertices.Add(p);
 
-            return newVertIndex++;
+            return _newVertIndex++;
         }
 
-        // return index of point in the middle of p1 and p2
+        //Return the index of the point in the middle between p1 and p2.
         private ushort GetMiddlePoint(int p1, int p2)
         {
 
-            // first check if we have it already
+            //Check the cache...
             var firstIsSmaller = p1 < p2;
             long smallerIndex = firstIsSmaller ? p1 : p2;
             long greaterIndex = firstIsSmaller ? p2 : p1;
@@ -164,35 +183,57 @@ namespace Fusee.Engine.Core
                 (point1.y + point2.y) / 2.0f,
                 (point1.z + point2.z) / 2.0f);
 
-           // add vertex makes sure point is on unit sphere
-           var i = AddNormalizedVertex(middle);
+            //Add the Vertex and make sure the point is on the unit sphere.
+            var i = AddNormalizedVertex(middle);
 
             _middlePointIndexCache.Add(key, i);
             return i;
         }
-        
+
     }
 
+    /// <summary>
+    /// Contains static methods to calculate the normals for a mesh.
+    /// </summary>
     public static class CalcNormalHelper
     {
-
-        public static  List<float3> CreateNormals(Mesh mesh)
+        /// <summary>
+        /// Calculates the vertex normals for a given mesh by calculating the avarage of all normals of faces, adjacent to a vertex.
+        /// </summary>
+        /// <param name="mesh">The mesh for which to calculate the normals.</param>
+        public static void CreateVertexNormals(Mesh mesh)
         {
-            var normals = new List<float3>();
-
+            mesh.Normals = new float3[mesh.Vertices.Length];
+            
             var triVerts = new List<float3>();
+            var triIndeices =  new List<ushort>();
             foreach (var tri in mesh.Triangles)
             {
                 triVerts.Add(mesh.Vertices[tri]);
+                triIndeices.Add(tri);
 
                 if (triVerts.Count != 3) continue;
-                normals.Add(CalculateTriNormal(triVerts));
-                normals.Add(CalculateTriNormal(triVerts));
-                normals.Add(CalculateTriNormal(triVerts));
-                triVerts = new List<float3>();
-            }
 
-            return normals;
+                var triNormal = CalculateTriNormal(triVerts);
+
+                CalcAverageNormal(mesh.Normals, triNormal, triIndeices[0]);
+                CalcAverageNormal(mesh.Normals, triNormal, triIndeices[1]);
+                CalcAverageNormal(mesh.Normals, triNormal, triIndeices[2]);
+
+                triVerts = new List<float3>();
+                triIndeices = new List<ushort>();
+            }
+        }
+
+        private static void CalcAverageNormal(IList<float3> normals, float3 triNormal, ushort triIndex)
+        {
+            if (normals[triIndex] == float3.Zero)
+                normals[triIndex] = triNormal;
+            else
+            {
+                var averageNormal = (triNormal + normals[triIndex]) / 2;
+                normals[triIndex] = averageNormal;
+            }
         }
 
         private static float3 CalculateTriNormal(IList<float3> triVerts)
@@ -200,10 +241,9 @@ namespace Fusee.Engine.Core
             var v = triVerts[1] - triVerts[0];
             var w = triVerts[2] - triVerts[1];
 
-            var normal = float3.Cross(w,v);
-            
-            normal.Normalize();
+            var normal = float3.Cross(w, v);
 
+            normal.Normalize();
             return normal;
         }
     }
