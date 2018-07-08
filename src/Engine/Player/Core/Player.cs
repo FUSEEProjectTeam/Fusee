c22a182c215d2d8afe1640d69f24ac1f9ac6879c
Moritz <moritz.roetner@hs-furtwangen.de>
2018-07-08 15:36:11 +0200

    Revert "Revert "Converter can now handle BoneAnimation, however still BROKEN""
    This reverts commit c04d796f44975924b23e5711ab3c51e347d6d0ae.

--- src/Engine/Player/Core/Player.cs
+++ src/Engine/Player/Core/Player.cs
@@ -1,22 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Fusee.Base.Common;
 using Fusee.Base.Core;
 using Fusee.Engine.Common;
 using Fusee.Engine.Core;
+using Fusee.Engine.Core.GUI;
 using Fusee.Math.Core;
 using Fusee.Serialization;
-using static Fusee.Engine.Core.Input;
-using static Fusee.Engine.Core.Time;
-using Fusee.Engine.Core.GUI;
+using Fusee.Xene;
 
 namespace Fusee.Engine.Player.Core
 {
 
-    [FuseeApplication(Name = "FUSEE Player", Description = "Watch any FUSEE scene.")]
+    [FuseeApplication(Name = "FUSEE Bone Animation Example", Description = "Quick bump example")]
     public class Player : RenderCanvas
     {
-        public string ModelFile = "FUSEERocket.fus";
-
         // angle variables
         private static float _angleHorz = M.PiOver3, _angleVert = -M.PiOver6 * 0.5f,
                              _angleVelHorz, _angleVelVert, _angleRoll, _angleRollInit, _zoomVel, _zoom;
@@ -83,7 +82,113 @@ namespace Fusee.Engine.Player.Core
             RC.ClearColor = new float4(1, 1, 1, 1);
 
             // Load the standard model
-            _scene = AssetStorage.Get<SceneContainer>(ModelFile);
+            _scene = new SceneContainer
+            {
+                Children = new List<SceneNodeContainer>
+                {
+                    new SceneNodeContainer
+                    {
+                        Components = new List<SceneComponentContainer>
+                        {
+                            new TransformComponent
+                            {
+                                Rotation = float3.Zero,
+                                Translation = new float3(0, 0, 0),
+                                Scale = float3.One
+                            },
+                            new BoneComponent()
+                        },
+                        Children = new List<SceneNodeContainer>
+                        {
+                            new SceneNodeContainer
+                            {
+                                Components = new List<SceneComponentContainer>
+                                {
+                                    new TransformComponent
+                                    {
+                                        Rotation = float3.Zero,
+                                        Translation = new float3(0, 0, 0),
+                                        Scale = float3.One
+                                    },
+                                    new BoneComponent(),
+                                    new WeightComponent(),
+                                    new MaterialComponent
+                                    {
+                                        Diffuse = new MatChannelContainer
+                                        {
+                                            Color = new float3(1.0f, 0.4f, 0.2f)
+                                        }
+                                    },
+                                    CreateCuboid(float3.One)
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            // convert scene graph is not called in this project, so we can add a bone animation
+
+            // then add a weightcomponent with weight matrices etc:
+            // binding matrices is the start point of every transformation
+            // as many entries as vertices are present in current model
+            var allMeshes = _scene.Children.FindComponents(x => x.GetType() == typeof(Mesh)).Select(x => (Mesh)x).ToList();
+            var vertexCount = 0;
+            foreach (var mesh in allMeshes)
+            {
+                vertexCount += mesh.Vertices.Length;
+            }
+
+            var bindingMatrices = new List<float4x4>();
+            for (var i = 0; i < vertexCount; i++)
+            {
+                bindingMatrices.Add(float4x4.Identity);
+            }
+
+            var WeightMap = new List<VertexWeightList>();
+            for (var i = 0; i < vertexCount; i++)
+            {
+                WeightMap.Add(new VertexWeightList
+                {
+                    VertexWeights = new List<VertexWeight>
+                    {
+                        new VertexWeight
+                        {
+                            JointIndex = 0,
+                            Weight = i * 0.4f
+                        },
+                        new VertexWeight()
+                        {
+                            JointIndex = 1,
+                            Weight = i * 0.23f
+                        }
+                    }
+                });
+            }
+
+            _scene.Children[0].Children[0].Components[2] = new WeightComponent
+            {
+                BindingMatrices = bindingMatrices,
+                WeightMap = WeightMap,
+                Joints = new List<SceneNodeContainer> // here we need the number 2 for the converter 
+                {
+                    new SceneNodeContainer(),
+                    new SceneNodeContainer()
+                }
+            };
+
+         
+            // now we can convert the scene
+            _scene = new ConvertSceneGraph().Convert(_scene);
+
+
+            // after CONVERSION we need to add all bones, because he safes them in a dictonary and scenenode
+            // before conversion != after conversion !!
+            _scene.Children[0].Children[0].GetComponent<WeightComponent>().Joints[0] = _scene.Children[0];
+            _scene.Children[0].Children[0].GetComponent<WeightComponent>().Joints[1] = _scene.Children[0].Children[0];
+
+
+
             AABBCalculator aabbc = new AABBCalculator(_scene);
             var bbox = aabbc.GetBox();
             if (bbox != null)
@@ -127,6 +232,8 @@ namespace Fusee.Engine.Player.Core
             // _guiSubText.Text = "dT: xxx ms, W: xxxx, H: xxxx, PS: xxxxxxxx";
             _subtextWidth = GUIText.GetTextWidth(_guiSubText.Text, _guiLatoBlack);
             _subtextHeight = GUIText.GetTextHeight(_guiSubText.Text, _guiLatoBlack);
+
+
         }
 
         // RenderAFrame is called once a frame
@@ -137,49 +244,49 @@ namespace Fusee.Engine.Player.Core
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
             // Mouse and keyboard movement
-            if (Keyboard.LeftRightAxis != 0 || Keyboard.UpDownAxis != 0)
+            if (Input.Keyboard.LeftRightAxis != 0 || Input.Keyboard.UpDownAxis != 0)
             {
                 _keys = true;
             }
 
-            var curDamp = (float)System.Math.Exp(-Damping * DeltaTime);
+            var curDamp = (float)System.Math.Exp(-Damping * Time.DeltaTime);
 
             // Zoom & Roll
-            if (Touch.TwoPoint)
+            if (Input.Touch.TwoPoint)
             {
                 if (!_twoTouchRepeated)
                 {
                     _twoTouchRepeated = true;
-                    _angleRollInit = Touch.TwoPointAngle - _angleRoll;
-                    _offsetInit = Touch.TwoPointMidPoint - _offset;
+                    _angleRollInit = Input.Touch.TwoPointAngle - _angleRoll;
+                    _offsetInit = Input.Touch.TwoPointMidPoint - _offset;
                     _maxPinchSpeed = 0;
                 }
-                _zoomVel = Touch.TwoPointDistanceVel * -0.01f;
-                _angleRoll = Touch.TwoPointAngle - _angleRollInit;
-                _offset = Touch.TwoPointMidPoint - _offsetInit;
-                float pinchSpeed = Touch.TwoPointDistanceVel;
+                _zoomVel = Input.Touch.TwoPointDistanceVel * -0.01f;
+                _angleRoll = Input.Touch.TwoPointAngle - _angleRollInit;
+                _offset = Input.Touch.TwoPointMidPoint - _offsetInit;
+                float pinchSpeed = Input.Touch.TwoPointDistanceVel;
                 if (pinchSpeed > _maxPinchSpeed) _maxPinchSpeed = pinchSpeed; // _maxPinchSpeed is used for debugging only.
             }
             else
             {
                 _twoTouchRepeated = false;
-                _zoomVel = Mouse.WheelVel * -0.5f;
+                _zoomVel = Input.Mouse.WheelVel * -0.5f;
                 _angleRoll *= curDamp * 0.8f;
                 _offset *= curDamp * 0.8f;
             }
 
             // UpDown / LeftRight rotation
-            if (Mouse.LeftButton)
+            if (Input.Mouse.LeftButton)
             {
                 _keys = false;
-                _angleVelHorz = -RotationSpeed * Mouse.XVel * 0.000002f;
-                _angleVelVert = -RotationSpeed * Mouse.YVel * 0.000002f;
+                _angleVelHorz = -RotationSpeed * Input.Mouse.XVel * 0.000002f;
+                _angleVelVert = -RotationSpeed * Input.Mouse.YVel * 0.000002f;
             }
-            else if (Touch.GetTouchActive(TouchPoints.Touchpoint_0) && !Touch.TwoPoint)
+            else if (Input.Touch.GetTouchActive(TouchPoints.Touchpoint_0) && !Input.Touch.TwoPoint)
             {
                 _keys = false;
                 float2 touchVel;
-                touchVel = Touch.GetVelocity(TouchPoints.Touchpoint_0);
+                touchVel = Input.Touch.GetVelocity(TouchPoints.Touchpoint_0);
                 _angleVelHorz = -RotationSpeed * touchVel.x * 0.000002f;
                 _angleVelVert = -RotationSpeed * touchVel.y * 0.000002f;
             }
@@ -187,8 +294,8 @@ namespace Fusee.Engine.Player.Core
             {
                 if (_keys)
                 {
-                    _angleVelHorz = -RotationSpeed * Keyboard.LeftRightAxis * 0.002f;
-                    _angleVelVert = -RotationSpeed * Keyboard.UpDownAxis * 0.002f;
+                    _angleVelHorz = -RotationSpeed * Input.Keyboard.LeftRightAxis * 0.002f;
+                    _angleVelVert = -RotationSpeed * Input.Keyboard.UpDownAxis * 0.002f;
                 }
                 else
                 {
@@ -222,6 +329,14 @@ namespace Fusee.Engine.Player.Core
             RC.ModelView = mtxCam * mtxRot * _sceneScale * _sceneCenter;
             var mtxOffset = float4x4.CreateTranslation(2 * _offset.x / Width, -2 * _offset.y / Height, 0);
             RC.Projection = mtxOffset * _projection;
+
+            // move one bone
+            var translation = _scene.Children[0].Children[0].GetComponent<TransformComponent>();
+            var translation2 = _scene.Children[0].GetComponent<TransformComponent>();
+            translation.Rotation.y += Input.Keyboard.ADAxis * 0.03f;
+            //translation2.Translation.x -= Input.Keyboard.ADAxis * 0.4f;
+            Diagnostics.Log(_scene.Children[0].GetComponent<TransformComponent>().Translation);
+
 
             // Tick any animations and Render the scene loaded in Init()
             _sceneRenderer.Animate();
@@ -276,5 +391,120 @@ namespace Fusee.Engine.Player.Core
         {
             OpenLink("http://fusee3d.org");
         }
+
+        public static Mesh CreateCuboid(float3 size)
+        {
+            return new Mesh
+            {
+                Vertices = new[]
+                {
+                    new float3 {x = +0.5f * size.x, y = -0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = +0.5f * size.x, y = +0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = +0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = -0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = +0.5f * size.x, y = -0.5f * size.y, z = -0.5f * size.z},
+                    new float3 {x = +0.5f * size.x, y = +0.5f * size.y, z = -0.5f * size.z},
+                    new float3 {x = +0.5f * size.x, y = +0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = +0.5f * size.x, y = -0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = -0.5f * size.y, z = -0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = +0.5f * size.y, z = -0.5f * size.z},
+                    new float3 {x = +0.5f * size.x, y = +0.5f * size.y, z = -0.5f * size.z},
+                    new float3 {x = +0.5f * size.x, y = -0.5f * size.y, z = -0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = -0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = +0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = +0.5f * size.y, z = -0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = -0.5f * size.y, z = -0.5f * size.z},
+                    new float3 {x = +0.5f * size.x, y = +0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = +0.5f * size.x, y = +0.5f * size.y, z = -0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = +0.5f * size.y, z = -0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = +0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = +0.5f * size.x, y = -0.5f * size.y, z = -0.5f * size.z},
+                    new float3 {x = +0.5f * size.x, y = -0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = -0.5f * size.y, z = +0.5f * size.z},
+                    new float3 {x = -0.5f * size.x, y = -0.5f * size.y, z = -0.5f * size.z}
+                },
+
+                Triangles = new ushort[]
+                {
+                    // front face
+                    0, 2, 1, 0, 3, 2,
+
+                    // right face
+                    4, 6, 5, 4, 7, 6,
+
+                    // back face
+                    8, 10, 9, 8, 11, 10,
+
+                    // left face
+                    12, 14, 13, 12, 15, 14,
+
+                    // top face
+                    16, 18, 17, 16, 19, 18,
+
+                    // bottom face
+                    20, 22, 21, 20, 23, 22
+
+                },
+
+                Normals = new[]
+                {
+                    new float3(0, 0, 1),
+                    new float3(0, 0, 1),
+                    new float3(0, 0, 1),
+                    new float3(0, 0, 1),
+                    new float3(1, 0, 0),
+                    new float3(1, 0, 0),
+                    new float3(1, 0, 0),
+                    new float3(1, 0, 0),
+                    new float3(0, 0, -1),
+                    new float3(0, 0, -1),
+                    new float3(0, 0, -1),
+                    new float3(0, 0, -1),
+                    new float3(-1, 0, 0),
+                    new float3(-1, 0, 0),
+                    new float3(-1, 0, 0),
+                    new float3(-1, 0, 0),
+                    new float3(0, 1, 0),
+                    new float3(0, 1, 0),
+                    new float3(0, 1, 0),
+                    new float3(0, 1, 0),
+                    new float3(0, -1, 0),
+                    new float3(0, -1, 0),
+                    new float3(0, -1, 0),
+                    new float3(0, -1, 0)
+                },
+
+                UVs = new[]
+                {
+                    new float2(1, 0),
+                    new float2(1, 1),
+                    new float2(0, 1),
+                    new float2(0, 0),
+                    new float2(1, 0),
+                    new float2(1, 1),
+                    new float2(0, 1),
+                    new float2(0, 0),
+                    new float2(1, 0),
+                    new float2(1, 1),
+                    new float2(0, 1),
+                    new float2(0, 0),
+                    new float2(1, 0),
+                    new float2(1, 1),
+                    new float2(0, 1),
+                    new float2(0, 0),
+                    new float2(1, 0),
+                    new float2(1, 1),
+                    new float2(0, 1),
+                    new float2(0, 0),
+                    new float2(1, 0),
+                    new float2(1, 1),
+                    new float2(0, 1),
+                    new float2(0, 0)
+                },
+                BoundingBox = new AABBf(-0.5f * size, 0.5f * size)
+            };
+        }
     }
 }
+
+
