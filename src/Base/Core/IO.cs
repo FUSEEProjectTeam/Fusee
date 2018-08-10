555af6b0eb1e74244994603912a6a9dd96a6f17b
Alexander Scheurer <mail@aspepex.net>
2018-08-10 15:26:43 +0200

    Revert IO.cs in Fusee.Base.Core

---
+++ src/Base/Core/IO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Fusee.Base.Common;
+
+namespace Fusee.Base.Core
+{
+    /// <summary>
+    /// Contains static methods not supported by portable libraries
+    /// dealing with input/output.
+    /// </summary>
+    public static class IO
+    {
+        /// <summary>
+        /// The platform dependent implementation of all IO functionality.
+        /// </summary>
+        /// <value>
+        /// The io implementation.
+        /// </value>
+        [InjectMe]
+        public static IIOImp IOImp { set; get; }
+
+        /// <summary>
+        /// Opens a file and returns its contents as a <see cref="Stream" />.
+        /// </summary>
+        /// <param name="path">The path to the file to open.</param>
+        /// <param name="mode">The mode.</param>
+        /// <returns>
+        /// A stream
+        /// </returns>
+        /// <exception>
+        /// Exceptions thrown by this method depend on the underlying 
+        /// platform dependent implementation.
+        /// </exception>
+        public static Stream StreamFromFile(string path, Common.FileMode mode)
+        {
+            return IOImp.StreamFromFile(path, mode);
+        }
+    }
+}
