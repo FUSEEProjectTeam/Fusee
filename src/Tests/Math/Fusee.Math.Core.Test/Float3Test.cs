59b3fdbb69f76d4db1d73de49fbdc20274e175a0
qogoist <jonas.haller.jh@gmail.com>
2019-03-26 18:14:39 +0100

    Tests for M and float3

---
+++ src/Tests/Math/Fusee.Math.Core.Test/Float3Test.cs
@@ -0,0 +1,215 @@
+﻿using Xunit;
+using System.Collections.Generic;
+
+namespace Fusee.Math.Core
+{
+    public class Float3Test
+    {
+        #region UtilityMethods
+        public static IEnumerable<object[]> GetFloat3()
+        {
+            var zero = new float3(0, 0, 0);
+            var one = new float3(1, 1, 1);
+
+            yield return new object[] {zero, one};
+            yield return new object[] {one, zero};
+        }
+        #endregion
+
+        #region Fields
+        [Fact]
+        public void One_IsOne()
+        {
+            Assert.Equal(new float3(1, 1, 1), float3.One);
+        }
+
+        [Fact]
+        public void UnitX_EqualsX()
+        {
+            Assert.Equal(new float3(1, 0, 0), float3.UnitX);
+        }
+
+        [Fact]
+        public void UnitY_EqualsY()
+        {
+            Assert.Equal(new float3(0, 1, 0), float3.UnitY);
+        }
+
+        [Fact]
+        public void UnitZ_EqualZ()
+        {
+            Assert.Equal(new float3(0, 0, 1), float3.UnitZ);
+        }
+
+        [Fact]
+        public void Zero_IsZero()
+        {
+            Assert.Equal(new float3(0, 0, 0), float3.Zero);
+        }
+        #endregion
+
+        #region Operators
+        [Theory]
+        [MemberData(nameof(GetFloat3))]
+        public void Operator_Add_TwoVectors_ReturnOneVector(float3 x, float3 y)
+        {
+            var actual = x + y;
+
+            Assert.Equal(new float3(1, 1, 1), actual);
+
+        }
+
+        [Fact]
+        public void Operator_Division_VectorAndScalar_ReturnsVector()
+        {
+            var vec = new float3(2, 2, 2);
+            var scalar = 2.0f;
+
+            var actual = vec / scalar;
+
+            Assert.Equal(new float3(1, 1, 1), actual);
+        }
+
+        [Fact]
+        public void Operator_Equality_IsEqual()
+        {
+            var actual = new float3(1, 1, 1);
+
+            Assert.True(new float3(1, 1, 1) == actual);
+        }
+
+        [Fact]
+        public void Operator_Equality_IsNotEqual()
+        {
+            var actual = new float3(1, 1, 1);
+
+            Assert.False(new float3(0, 0, 0) == actual);
+        }
+
+        [Fact]
+        public void Operator_Inequality_IsEqual()
+        {
+            var actual = new float3(1, 1, 1);
+
+            Assert.False(new float3(1, 1, 1) != actual);
+        }
+
+        [Fact]
+        public void Operator_Inequality_IsNotEqual()
+        {
+            var actual = new float3(1, 1, 1);
+
+            Assert.True(new float3(0, 0, 0) != actual);
+        }
+
+        [Fact]
+        public void Operator_Multiply_VectorAndScalar_ReturnVector()
+        {
+            var vec = new float3(1, 1, 1);
+            var scalar = 2.0f;
+
+            var actual = vec * scalar;
+
+            Assert.Equal(new float3(2, 2, 2), actual);
+        }
+
+        [Fact]
+        public void Operator_Multiply_ScalarAndVector_ReturnVector()
+        {
+            var vec = new float3(1, 1, 1);
+            var scalar = 2.0f;
+
+            var actual = scalar * vec;
+
+            Assert.Equal(new float3(2, 2, 2), actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetFloat3))]
+        public void Operator_Multiply_TwoVectors_ReturnVector(float3 x, float3 y)
+        {
+            var actual = x * y;
+
+            Assert.Equal(new float3(0, 0, 0), actual);
+        }
+
+        [Fact]
+        public void Operator_Subtract_TwoVectors_ReturnVector()
+        {
+            var x = new float3(1, 1, 1);
+            var y = new float3(1, 1, 1);
+
+            var actual = x - y;
+
+            Assert.Equal(new float3(0, 0, 0), actual);
+        }
+
+        [Fact]
+        public void Operator_UnaryNegation_NegatePositiveVector()
+        {
+            var x = new float3(1, 1, 1);
+
+            var actual = -x;
+
+            Assert.Equal(new float3(-1, -1, -1), actual);
+        }
+
+        [Fact]
+        public void Operator_UnaryNegation_NegateNegativeVector()
+        {
+            var x = new float3(-1, -1, -1);
+
+            var actual = -x;
+
+            Assert.Equal(new float3(1, 1, 1), actual);
+        }
+        #endregion
+
+        #region Methods
+        [Theory]
+        [MemberData(nameof(GetFloat3))]
+        public void Add_TwoVectors_ReturnsVector(float3 x, float3 y)
+        {
+            var actual = float3.Add(x,y);
+
+            Assert.Equal(new float3(1, 1, 1), actual);
+        }
+
+
+        #endregion
+
+        #region Constructors
+        [Fact]
+        public void Float3_fromDouble3()
+        {
+            var actual = new double3(1, 1, 1);
+
+            Assert.Equal(new float3(1, 1, 1), new float3(actual));
+        }
+
+        [Fact]
+        public void Float3_fromFloat2()
+        {
+            var actual = new float2(1, 1);
+
+            Assert.Equal(new float3(1, 1, 0), new float3(actual));
+        }
+
+        [Fact]
+        public void Float3_fromFloat3()
+        {
+            var actual = new float3(1, 1, 1);
+
+            Assert.Equal(new float3(1, 1, 1), new float3(actual));
+        }
+
+        [Fact]
+        public void Float3_fromFloat4()
+        {
+            var actual = new float4(1, 1, 1, 1);
+
+            Assert.Equal(new float3(1, 1, 1), new float3(actual));
+        }
+        #endregion
+    }
+}
---
+++ src/Tests/Math/Fusee.Math.Core.Test/MTest.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Fusee.Math.Core
+{
+    public class MTest
+    {
+        [Fact]
+        public void BinominalCoefficient_IsOne()
+        {
+            var k = 0;
+            var n = 1;
+
+            Assert.Equal(1, M.BinomialCoefficient(n, k));
+        }
+
+        [Theory]
+        [InlineData(2, 0, 1)]
+        [InlineData(0, 1, 2)]
+        public void Clamp_IsMinIsMax_float(float x, float min, float max)
+        {
+            Assert.Equal(1, M.Clamp(x, min, max));
+        }
+
+        [Theory]
+        [InlineData(2, 0, 1)]
+        [InlineData(0, 1, 2)]
+        public void Clamp_IsMinIsMax_double(double x, double min, double max)
+        {
+            Assert.Equal(1, M.Clamp(x, min, max));
+        }
+    }
+}
