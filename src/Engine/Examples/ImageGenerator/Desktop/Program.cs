e3021f8fa2f5e472bd17f6325a744659f9fe4992
Christoph <Mller>
2019-03-27 11:24:21 +0100

    Image Generator first Occurrence
    Image generation works. Transparency in UI elements is black.

---
+++ src/Engine/Examples/ImageGenerator/Desktop/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Fusee.Base.Common;
+using Fusee.Base.Core;
+using Fusee.Base.Imp.Desktop;
+using Fusee.Engine.Core;
+using Fusee.Serialization;
+using FileMode = Fusee.Base.Common.FileMode;
+using Path = Fusee.Base.Common.Path;
+using System.Reflection;
+
+namespace Fusee.Engine.Examples.ImageGenerator.Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inject Fusee.Engine.Base InjectMe dependencies
+            IO.IOImp = new Fusee.Base.Imp.Desktop.IOImp();
+
+            var fap = new Fusee.Base.Imp.Desktop.FileAssetProvider("Assets");
+            fap.RegisterTypeHandler(
+                new AssetHandler
+                {
+                    ReturnedType = typeof(Font),
+                    Decoder = delegate (string id, object storage)
+                    {
+                        if (!Path.GetExtension(id).ToLower().Contains("ttf")) return null;
+                        return new Font{ _fontImp = new FontImp((Stream)storage) };
+                    },
+                    Checker = id => Path.GetExtension(id).ToLower().Contains("ttf")
+                });
+            fap.RegisterTypeHandler(
+                new AssetHandler
+                {
+                    ReturnedType = typeof(SceneContainer),
+                    Decoder = delegate (string id, object storage)
+                    {
+                        if (!Path.GetExtension(id).ToLower().Contains("fus")) return null;
+                        var ser = new Serializer();
+                        return new ConvertSceneGraph().Convert(ser.Deserialize((Stream)storage, null, typeof(SceneContainer)) as SceneContainer);
+                    },
+                    Checker = id => Path.GetExtension(id).ToLower().Contains("fus")
+                });
+
+            AssetStorage.RegisterProvider(fap);
+
+            var app = new Generator();
+
+            // Inject Fusee.Engine InjectMe dependencies (hard coded)
+            var cimp = new RenderCanvasImpIG();
+            app.CanvasImplementor = cimp;
+            app.ContextImplementor = new Fusee.Engine.Imp.Graphics.Desktop.RenderContextImp(cimp);
+            // Input.AddDriverImp(new Fusee.Engine.Imp.Graphics.Desktop.RenderCanvasInputDriverImp(app.CanvasImplementor));
+            // Input.AddDriverImp(new Fusee.Engine.Imp.Graphics.Desktop.WindowsTouchInputDriverImp(app.CanvasImplementor));
+            // app.InputImplementor = new Fusee.Engine.Imp.Graphics.Desktop.InputImp(app.CanvasImplementor);
+            // app.AudioImplementor = new Fusee.Engine.Imp.Sound.Desktop.AudioImp();
+            // app.NetworkImplementor = new Fusee.Engine.Imp.Network.Desktop.NetworkImp();
+            // app.InputDriverImplementor = new Fusee.Engine.Imp.Input.Desktop.InputDriverImp();
+            // app.VideoManagerImplementor = ImpFactory.CreateIVideoManagerImp();
+
+            /*
+            var mode = new GraphicsMode(new ColorFormat(8, 8, 8, 8), 24, 0, 0, ColorFormat.Empty, 1);
+            var win = new OpenTK.GameWindow(640, 480, mode, "", OpenTK.GameWindowFlags.Default, OpenTK.DisplayDevice.Default, 3, 0, GraphicsContextFlags.Default);
+            */
+
+            // Initialize canvas/app and canvas implementor
+            app.DoInit();
+            cimp.DoInit();
+
+            // Render a single frame and save it
+            cimp.DoRender();
+            var bmp = cimp.ShootCurrentFrame();
+            bmp.Save(@"test.png", System.Drawing.Imaging.ImageFormat.Png);
+            Console.WriteLine("Test image written");
+
+            // Do not Start the app
+            // Don't call 
+            //app.Run();
+        }
+    }
+}
