1a2400ad51630136bb6fb97e0ced5698fad4b849
qogoist <jonas.haller.jh@gmail.com>
2019-03-27 15:16:42 +0100

    More tests for float3

--- src/Tests/Math/Fusee.Math.Core.Test/Float3Test.cs
+++ src/Tests/Math/Fusee.Math.Core.Test/Float3Test.cs
@@ -5,7 +5,7 @@ namespace Fusee.Math.Core
 {
     public class Float3Test
     {
-        #region UtilityMethods
+        #region Utility
         public static IEnumerable<object[]> GetFloat3()
         {
             var zero = new float3(0, 0, 0);
@@ -13,6 +13,45 @@ namespace Fusee.Math.Core
 
             yield return new object[] {zero, one};
             yield return new object[] {one, zero};
+        }
+
+        public static IEnumerable<object[]> GetUVExpected()
+        {
+            yield return new object[] { 0, 0, new float3(1, 0, 0) };
+            yield return new object[] { 1, 0, new float3(0, 1, 0) };
+            yield return new object[] { 0, 1, new float3(0, 0, 1) };
+        }
+
+        public static IEnumerable<object[]> GetFloat3ExpectedAngle()
+        {
+            var a = new float3(1, 0, 0);
+            var b = new float3(0, 1, 0);
+            yield return new object[] { a, b, 1.5708f};
+            yield return new object[] { a, a, 0};
+        }
+
+        public static IEnumerable<object[]> GetVecMinMaxExpected()
+        {
+            var a = new float3(0, 0, 0);
+            var b = new float3(1, 1, 1);
+            var c = new float3(2, 2, 2);
+
+            yield return new object[] { a, b, c, b };
+            yield return new object[] { c, a, b, b };
+            yield return new object[] { b, a, c, b };
+        }
+
+        public static IEnumerable<object[]> GetVectorCross()
+        {
+            var a = new float3(1, 0, 0);
+            var b = new float3(0, 1, 0);
+            var c = new float3(0, 0, 1);
+
+            yield return new object[] { a, b, c };
+            yield return new object[] { c, a, b };
+            yield return new object[] { b, c, a };
+            yield return new object[] { a, a, new float3(0, 0, 0) };
+            yield return new object[] { b, a, -c };
         }
         #endregion
 
@@ -175,7 +214,205 @@ namespace Fusee.Math.Core
             Assert.Equal(new float3(1, 1, 1), actual);
         }
 
+        [Theory]
+        [MemberData(nameof(GetFloat3))]
+        public void Add_TwoVectors_SaveToThird(float3 x, float3 y)
+        {
+            float3 actual;
 
+            float3.Add(ref x, ref y, out actual);
+
+            Assert.Equal(new float3(1, 1, 1), actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetUVExpected))]
+        public void Barycentric_VaryUV_ReturnResult(float u, float v, float3 expected)
+        {
+            var a = new float3(1, 0, 0);
+            var b = new float3(0, 1, 0);
+            var c = new float3(0, 0, 1);
+
+            var actual = float3.Barycentric(a, b, c, u, v);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetFloat3ExpectedAngle))]
+        public void CalculateAngle_TwoVectors_ReturnAngle(float3 a, float3 b, float expected)
+        {
+            float actual;
+
+            actual = float3.CalculateAngle(a, b);
+
+            Assert.Equal(expected, actual, 5);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetFloat3ExpectedAngle))]
+        public void CalculateAngle_TwoVectors_SaveToAngle(float3 a, float3 b, float expected)
+        {
+            float actual;
+
+            float3.CalculateAngle(ref a, ref b, out actual);
+
+            Assert.Equal(expected, actual, 5);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetVecMinMaxExpected))]
+        public void Clamp_VectorToMinToMax(float3 vec, float3 min, float3 max, float3 expected)
+        {
+            var actual = float3.Clamp(vec, min, max);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetVecMinMaxExpected))]
+        public void Clamp_VectorToMinToMax_ToVector(float3 vec, float3 min, float3 max, float3 expected)
+        {
+            float3 actual;
+
+            float3.Clamp(ref vec, ref min, ref max, out actual);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetFloat3))]
+        public void ComponentMax_TwoVectors_ReturnOne(float3 a, float3 b)
+        {
+            var actual = float3.ComponentMax(a, b);
+
+            Assert.Equal(new float3(1, 1, 1), actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetFloat3))]
+        public void ComponentMax_TwoVectors_ToVector(float3 a, float3 b)
+        {
+            float3 actual;
+
+            float3.ComponentMax(ref a, ref b, out actual);
+
+            Assert.Equal(new float3(1, 1, 1), actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetFloat3))]
+        public void ComponentMin_TwoVectors_ReturnOne(float3 a, float3 b)
+        {
+            var actual = float3.ComponentMin(a, b);
+
+            Assert.Equal(new float3(0, 0, 0), actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetFloat3))]
+        public void ComponentMin_TwoVectors_ToVector(float3 a, float3 b)
+        {
+            float3 actual;
+
+            float3.ComponentMin(ref a, ref b, out actual);
+
+            Assert.Equal(new float3(0, 0, 0), actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetVectorCross))]
+        public void Cross_TwoVectors_ReturnOne(float3 a, float3 b, float3 expected)
+        {
+            float3 actual;
+
+            actual = float3.Cross(a, b);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetVectorCross))]
+        public void Cross_TwoVectors_ToVector(float3 a, float3 b, float3 expected)
+        {
+            float3 actual;
+
+            float3.Cross(ref a, ref b, out actual);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Divide_TwoVectors_ReturnOne()
+        {
+            float3 a = new float3(2, 2, 2);
+            float3 b = new float3(2, 2, 2);
+
+            var actual = float3.Divide(a, b);
+
+            Assert.Equal(new float3(1, 1, 1), actual);
+        }
+
+        [Fact]
+        public void Divide_TwoVectors_ToVector()
+        {
+            float3 a = new float3(2, 2, 2);
+            float3 b = new float3(2, 2, 2);
+            float3 actual;
+
+            float3.Divide(ref a, ref b, out actual);
+
+            Assert.Equal(new float3(1, 1, 1), actual);
+        }
+
+        [Fact]
+        public void Divide_VectorScalar_ReturnVector()
+        {
+            float3 vec = new float3(2, 2, 2);
+            float scalar = 2;
+
+            var actual = float3.Divide(vec, scalar);
+
+            Assert.Equal(new float3(1, 1, 1), actual);
+        }
+
+        [Fact]
+        public void Divide_VectorScalar_ToVector()
+        {
+            float3 vec = new float3(2, 2, 2);
+            float scalar = 2;
+            float3 actual;
+
+            float3.Divide(ref vec, scalar, out actual);
+
+            Assert.Equal(new float3(1, 1, 1), actual);
+        }
+
+        [Fact]
+        public void Dot_TwoVectos_ReturnScalar()
+        {
+            float3 a = new float3(1, 1, 1);
+            float3 b = new float3(1, 2, 3);
+
+            var actual = float3.Dot(a, b);
+
+            Assert.Equal(6, actual);
+        }
+
+        [Fact]
+        public void Dot_TwoVectos_ToScalar()
+        {
+            float3 a = new float3(1, 1, 1);
+            float3 b = new float3(1, 2, 3);
+            float actual;
+
+            float3.Dot(ref a, ref b, out actual);
+
+            Assert.Equal(6, actual);
+        }
+
+        [Fact]
+        public void Equal
         #endregion
 
         #region Constructors
