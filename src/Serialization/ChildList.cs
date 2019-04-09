01b0680752547e6dc4296059dcd35922b63d3ad1
Sarah Busert <sarah.busert@hs-furtwangen.de>
2019-04-09 17:43:20 +0200

    ChildList: added SetItem and RemoveItem

--- src/Serialization/ChildList.cs
+++ src/Serialization/ChildList.cs
@@ -17,20 +17,42 @@ namespace Fusee.Serialization
 
         protected override void InsertItem(int index, SceneNodeContainer snc)
         {
+            AddSnc(snc);
+            base.InsertItem(index, snc);
+        }
 
+        protected override void SetItem(int index, SceneNodeContainer snc)
+        {
+            AddSnc(snc);
+            base.SetItem(index, snc);
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            var snc = this[index];
+            
+            //Replace the parent of this snc's children with the parent of this scn
+            if (snc.Children.Count != 0)
+            {
+                foreach (var child in snc.Children)
+                {
+                    child.Parent = snc.Parent;
+                }
+            }
+            base.RemoveItem(index);
+        }
+
+        private void AddSnc(SceneNodeContainer snc)
+        {
             if (snc.Parent == null)
             {
                 OnAdd?.Invoke(this, new AddChildEventArgs(snc));
-                base.InsertItem(index, snc);
             }
             else
             {
                 //remove from old parent's child list
                 snc.Parent.Children.Remove(snc);
-
                 OnAdd?.Invoke(this, new AddChildEventArgs(snc));
-                base.InsertItem(index, snc);
-
             }
         }
     }
