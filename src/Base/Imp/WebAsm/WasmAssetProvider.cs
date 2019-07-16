b0a908900de4e27640912f9e6a3602704888181f
wrestledBearOnce <hallo@moritzroetner.de>
2019-07-16 11:54:51 +0200

    Moved the rest to Base.Imp

--- src/Base/Imp/WebAsm/WasmAssetProvider.cs
+++ src/Base/Imp/WebAsm/WasmAssetProvider.cs
@@ -1,14 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Fusee.Base.Common;
 using Fusee.Base.Core;
-using Samples;
+using WebAssembly;
 using FileMode = System.IO.FileMode;
 using Path = Fusee.Base.Common.Path;
 
 namespace Fusee.Base.Imp.WebAsm
 {
+    public static class WasmResourceLoader
+    {
+        public static string GetLocalAddress()
+        {
+            using (var window = (JSObject)Runtime.GetGlobalObject("window"))
+            using (var location = (JSObject)window.GetObjectProperty("location"))
+            {
+                var address = (string)location.GetObjectProperty("href");
+
+                if (address.Contains("/"))
+                {
+                    address = address.Substring(0, address.LastIndexOf('/') + 1);
+                }
+
+                return address;
+            }
+        }
+
+        public static async Task<Stream> LoadAsync(string relativePath, string baseAddress)
+        {
+            Stream content;
+
+            var httpClient = new HttpClient { BaseAddress = new Uri(baseAddress) };
+
+#if DEBUG
+            Console.WriteLine($"Requesting '{relativePath}' at '{baseAddress}'...");
+#endif
+
+            try
+            {
+                var response = await httpClient.GetAsync(relativePath);
+                response.EnsureSuccessStatusCode();
+                content = await response.Content.ReadAsStreamAsync();
+            }
+            catch (Exception exception)
+            {
+                content = null;
+
+                Console.WriteLine($"[Error] {nameof(WasmResourceLoader)}.{nameof(LoadAsync)}(): {exception}");
+            }
+
+#if DEBUG
+            if (content != null)
+            {
+                Console.WriteLine($"Content length: {content.Length} B");
+            }
+#endif
+
+            return content;
+        }
+    }
+
     /// <summary>
     /// Asset provider for direct file access. Typically used in desktop builds where assets are simply contained within
     /// a subdirectory of the application.
