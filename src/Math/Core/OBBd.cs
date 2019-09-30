f01687f3b11a71297a2f4963ec4ee620ad1bb6da
Alexander Scheurer <mail@aspepex.net>
2019-09-30 10:39:02 +0200

    Cleaned up F.Math.Core
    - enabled LangVersion 8 and nullable reference types
    - removed warning disable 1591
    - removed ReSharper notations
    - removed unnecessary usings

--- src/Math/Core/OBBd.cs
+++ src/Math/Core/OBBd.cs
@@ -1,10 +1,5 @@
 ﻿using ProtoBuf;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 
 namespace Fusee.Math.Core
 {
