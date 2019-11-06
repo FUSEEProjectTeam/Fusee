cae397f145b7f936fb025c526141ba76fe47ef4c
Alexander Scheurer <mail@aspepex.net>
2019-11-06 15:33:48 +0100

    'fusee install' now installs file association for .fus and fuz, as well as a custom uri handler for fusee://

--- src/Tools/fuseeCmdLine/Program.cs
+++ src/Tools/fuseeCmdLine/Program.cs
@@ -17,6 +17,7 @@ using Path = System.IO.Path;
 using Fusee.Base.Imp.Desktop;
 using System.IO.Compression;
 using System.Net;
+using Microsoft.Win32;
 
 namespace Fusee.Tools.fuseeCmdLine
 {
@@ -30,6 +31,8 @@ namespace Fusee.Tools.fuseeCmdLine
         PlatformNotHandled = -5,
         InsufficentPrivileges = -6,
         CouldNotDownloadInputFile = -7,
+        CouldNotWriteRegistry = -8,
+        CouldNotFindFusee = -9,
 
         InternalError = -42,
     }
@@ -113,6 +116,9 @@ namespace Fusee.Tools.fuseeCmdLine
 
             [Option('i', "blenderdir", HelpText = "Manually set the directory where to (un/)install the Blender FUSEE Add-on to/from. If not set, fusee.exe tries to find an appropriate Add-on directory based on the installation type (option '--type' per-user or machine-wide).")]
             public string BlenderDir { get; set; }
+
+            [Option('f', "fileass", Default = false, HelpText = "Sets up file association for .fus and .fuz files as well as a custom uri handler for 'fusee://'.")]
+            public bool fileAss { get; set; }
 
         }
 
@@ -863,9 +869,12 @@ namespace Fusee.Tools.fuseeCmdLine
 
                     // Set the individual installation steps (currently four). If NONE of them is set, select ALL OF THEM.
                     bool instBlender = opts.Blender;
-                    if (!(instBlender))
+                    bool fileAss = opts.fileAss;
+
+                    if (!(instBlender || fileAss))
                     {
                         instBlender = true;
+                        fileAss = true;
                     }
 
                     ErrorCode exitCode = ErrorCode.Success;
@@ -929,6 +938,71 @@ namespace Fusee.Tools.fuseeCmdLine
                             {
                                 Console.Error.WriteLine($"ERROR: Unable to install the FUSEE Blender Add-on.\n{ex}");
                                 exitCode = ErrorCode.InternalError;
+                            }
+                        }
+
+                        if (fileAss)
+                        {
+                            var pathtofuseedll = Assembly.GetEntryAssembly().Location;
+                            var pathtofuseeexe = "";
+                            var userprofilepath = Environment.GetEnvironmentVariable("USERPROFILE");
+
+                            if (pathtofuseedll.Contains(userprofilepath + @"\.dotnet\tools\") && File.Exists(userprofilepath + @"\.dotnet\tools\fusee.exe"))
+                            {
+                                pathtofuseeexe = userprofilepath + @"\.dotnet\tools\fusee.exe";
+                            }
+                            else if (File.Exists(Path.ChangeExtension(pathtofuseedll, "exe")))
+                            {
+                                pathtofuseeexe = Path.ChangeExtension(pathtofuseedll, "exe");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error: Could not find fusee.exe.");
+                                exitCode = ErrorCode.CouldNotFindFusee;   
+                            }
+
+                            if (!string.IsNullOrEmpty(pathtofuseeexe))
+                            {
+                                try
+                                {
+                                    RegistryKey schemeKey1 = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Classes\fusee");
+                                    schemeKey1.SetValue("", "Fusee");
+                                    schemeKey1.SetValue("URL Protocol", "");
+
+                                    RegistryKey schemeKey2 = Registry.CurrentUser.CreateSubKey(@"Software\Classes\fusee\DefaultIcon");
+                                    schemeKey2.SetValue("", pathtofuseeexe + ",0");
+
+                                    RegistryKey schemeKey3 = Registry.CurrentUser.CreateSubKey(@"Software\Classes\fusee\Shell");
+
+                                    RegistryKey schemeKey4 = Registry.CurrentUser.CreateSubKey(@"Software\Classes\fusee\Shell\Open");
+
+                                    RegistryKey schemeKey5 = Registry.CurrentUser.CreateSubKey(@"Software\Classes\fusee\Shell\Open\Command");
+                                    schemeKey5.SetValue("", pathtofuseeexe + " player %1");
+
+                                    Console.WriteLine("Set 'fusee://' Uri Scheme handler to use " + pathtofuseeexe);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("Error: Could not set registry keys for custom uri scheme handler: " + e.Message);
+                                    exitCode = ErrorCode.CouldNotWriteRegistry;
+                                }
+
+                                try
+                                {
+                                    RegistryKey fileKey1 = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Classes\.fus");
+                                    fileKey1.SetValue("", "fusee"); //this associates with the fusee custom uri handler
+
+                                    RegistryKey fileKey2 = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Classes\.fuz");
+                                    fileKey2.SetValue("", "fusee"); //this associates with the fusee custom uri handler
+                                    fileKey2.SetValue("Content Type", "application/zip");
+
+                                    Console.WriteLine("Set file association for .fus and .fuz files.");
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("Could not set registry keys for file association: " + e.Message);
+                                    exitCode = ErrorCode.CouldNotWriteRegistry;
+                                }
                             }
                         }
                     }
@@ -998,6 +1072,13 @@ namespace Fusee.Tools.fuseeCmdLine
                                 Console.Error.WriteLine($"ERROR: Unable to uninstall the FUSEE Blender Add-on.\n{ex}");
                                 exitCode = ErrorCode.InternalError;
                             }
+                        }
+
+                        if (fileAss)
+                        {
+                            Registry.CurrentUser.DeleteSubKeyTree(@"SOFTWARE\Classes\.fus", true);
+                            Registry.CurrentUser.DeleteSubKeyTree(@"SOFTWARE\Classes\.fuz", true);
+                            Registry.CurrentUser.DeleteSubKeyTree(@"SOFTWARE\Classes\fusee", true);
                         }
                     }
 
