26aa42ff8bc4c138a319cec5938b9dc52ffd46ad
Alexander Scheurer <mail@aspepex.net>
2019-11-07 22:00:49 +0100

    Moved NuGet meta packages to dis

--- BuildNuget.cmd
+++ BuildNuget.cmd
@@ -8,8 +8,8 @@ echo Cleaning up build directory.
 git clean -xdf
 
 echo Building distribution.
-dotnet pack Fusee.Engine.sln -c Release-NuGet -o bin\Release\nuget --include-source
-dotnet pack src\Meta\nuget\Core\Core.csproj -c Release -o bin\Release\nuget --include-source
-dotnet pack src\Meta\nuget\Desktop\Desktop.csproj -c Release -o bin\Release\nuget --include-source
+dotnet pack Fusee.Engine.sln -c Release-NuGet -o bin\Release\nuget --include-symbols --include-source
+dotnet pack dis\NuGet\Core\Core.csproj -c Release -o bin\Release\nuget
+dotnet pack dis\NuGet\Desktop\Desktop.csproj -c Release -o bin\Release\nuget
 dotnet pack dis\DnTemplate\DnTemplate.csproj -c Release -o bin\Release\nuget
 endlocal
