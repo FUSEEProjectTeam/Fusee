03d00c0bf5995b4f2b0246e873f0cea9d24fe384
Jonas Haller <jonas.haller.jh@gmail.com>
2019-11-09 14:56:44 +0100

    added missing XML comments in Engine.Common

--- src/Engine/Common/ResizeEventArgs.cs
+++ src/Engine/Common/ResizeEventArgs.cs
@@ -7,9 +7,20 @@ namespace Fusee.Engine.Common
     /// </summary>
     public class ResizeEventArgs : EventArgs
     {
+        /// <summary>
+        /// The width held for the Resize event.
+        /// </summary>
         public int Width { get; private set; }
+        /// <summary>
+        /// The height held for the Resize event.
+        /// </summary>
         public int Height { get; private set; }
 
+        /// <summary>
+        /// Constructs a new ResizeEventArgs.
+        /// </summary>
+        /// <param name="width">The width held for the Resize event.</param>
+        /// <param name="height">The height held for the Resize event.</param>
         public ResizeEventArgs(int width, int height)
         {
             Width = width;
