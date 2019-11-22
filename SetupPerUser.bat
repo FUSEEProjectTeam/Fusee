3df1aed3818d734def4af1450445ae3bf5cac6d4
Alexander Scheurer <mail@aspepex.net>
2019-11-22 14:39:56 +0100

    Removed legacy setup scripts

--- SetupPerUser.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-CLS
-ECHO.
-ECHO =============================
-ECHO Fusee Setup per User
-ECHO =============================
-"%~dp0bin/Debug/Tools/fusee.exe" install
-REM keep cmd open after execution to show installation messages
-cmd /k
