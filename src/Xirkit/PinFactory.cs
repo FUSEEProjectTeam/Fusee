98e86ee3c7bc553ca48e963bc41ec8ab8d93b331
Alexander Scheurer <mail@aspepex.net>
2020-01-28 10:06:47 +0100

    Updated double3.Parse and double3.ToString implementation

--- src/Xirkit/PinFactory.cs
+++ src/Xirkit/PinFactory.cs
@@ -264,7 +264,7 @@ namespace Fusee.Xirkit
             AddConverter<string, bool>(x => bool.Parse(x));
             AddConverter<string, string>(x => x);
             AddConverter<string, double2>(double2.Parse);  // Pass the Parse method directly (it already has the Converter signature)
-            AddConverter<string, double3>(double3.Parse);
+            AddConverter<string, double3>(double3.ParseConverter);
             AddConverter<string, double4>(double4.ParseConverter);
             AddConverter<string, double4x4>(double4x4.Parse);
             AddConverter<string, float2>(float2.ParseConverter);
