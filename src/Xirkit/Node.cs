f4f1fe002d0efe871010da9103a19e26ccc93d1f
Jonas Haller <jonas.haller.jh@gmail.com>
2020-03-16 20:44:41 +0100

    Added exception when trying to create a Node from a Struct

--- src/Xirkit/Node.cs
+++ src/Xirkit/Node.cs
@@ -59,6 +59,8 @@ namespace Fusee.Xirkit
         /// <param name="o">The object to be hoste</param>
         public Node(object o)
         {
+            if (!o.GetType().IsByRef)
+                throw new ArgumentException("Not a reference type.", nameof(o));
             _o = o;
             _cp = o as ICalculationPerformer;
             _outPinList = new List<IOutPin>();
