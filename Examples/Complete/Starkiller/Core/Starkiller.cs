b309f2833f6196783b7e2bbc09a72d4ffcd714cb
Niclas-Cravaack <60705949+Niclas-Cravaack@users.noreply.github.com>
2020-04-23 13:33:57 +0200

    summary

--- Examples/Complete/Starkiller/Core/Starkiller.cs
+++ Examples/Complete/Starkiller/Core/Starkiller.cs
@@ -2,223 +2,222 @@ using Fusee.Base.Common;
 using Fusee.Base.Core;
 using Fusee.Engine.Common;
 using Fusee.Engine.Core;
-using Fusee.Engine.Core.Scene;
-using Fusee.Engine.Core.ShaderShards;
-using Fusee.Engine.GUI;
 using Fusee.Math.Core;
+using Fusee.Serialization;
 using Fusee.Xene;
+using static Fusee.Engine.Core.Input;
+using static Fusee.Engine.Core.Time;
+using Fusee.Engine.GUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using static Fusee.Engine.Core.Input;
-using static Fusee.Engine.Core.Time;
+using Fusee.Engine.Core.Scene;
 
 namespace Fusee.Examples.Starkiller.Core
 {
-    [FuseeApplication(Name = "FUSEE Starkiller Example", Description = "A very killer example.")]
+    [FuseeApplication(Name = "Starkiller", Description = "Yet another FUSEE App.")]
     public class Starkiller : RenderCanvas
     {
-        // angle variables
-        private static float _angleHorz = M.PiOver3, _angleVert = -M.PiOver6 * 0.5f, _angleVelHorz, _angleVelVert;
-
-        private const float RotationSpeed = 7;
-        private const float Damping = 0.8f;
-
-        private SceneContainer _rocketScene;
+        private SceneContainer _starkillerScene;
         private SceneRendererForward _sceneRenderer;
+        private Transform _Rumpf;
+        private Transform _TwLinks;
+        private Transform _TwRechts;
+        private Transform _starkillerTransform = new Transform();
+        private SceneContainer _scene;
 
-        private const float ZNear = 1f;
-        private const float ZFar = 1000;
-        private readonly float _fovy = M.PiOver4;
 
-        private SceneRendererForward _guiRenderer;
-        private SceneContainer _gui;
-        private SceneInteractionHandler _sih;
-        private readonly CanvasRenderMode _canvasRenderMode = CanvasRenderMode.SCREEN;
-
-        private bool _keys;
-
-        // Init is called on startup.
-        public override async Task<bool> Init()
+        private SceneContainer CreateScene()
         {
-            _gui = CreateGui();
+            //load the Nodes in
+            SceneContainer _starkillerScene = AssetStorage.Get<SceneContainer>("StarkillerAssets.fus");
+            SceneNode meteorit1 = _starkillerScene.Children.FindNodes(n => n.Name == "Meteorit1").First();
+            SceneNode visier = _starkillerScene.Children.FindNodes(n => n.Name == "Visier").First();
+            SceneNode rumpf = _starkillerScene.Children.FindNodes(n => n.Name == "Rumpf").First();
+            SceneNode tw_links = _starkillerScene.Children.FindNodes(n => n.Name == "TW_Links").First();
+            SceneNode tw_rechts = _starkillerScene.Children.FindNodes(n => n.Name == "TW_Links").First();
 
-            // Create the interaction handler
-            _sih = new SceneInteractionHandler(_gui);
-
-            // Set the clear color for the backbuffer to white (100% intensity in all color channels R, G, B, A).
-            RC.ClearColor = new float4(1, 1, 1, 1);
-
-            // Load the rocket model
-            _rocketScene = AssetStorage.Get<SceneContainer>("FUSEERocket.fus");
-
-            // Wrap a SceneRenderer around the model.
-            _sceneRenderer = new SceneRendererForward(_rocketScene);
-            _guiRenderer = new SceneRendererForward(_gui);
-
-            return true;
-        }
-
-        // RenderAFrame is called once a frame
-        public override void RenderAFrame()
-        {
-            // Clear the backbuffer
-            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
-
-            RC.Viewport(0, 0, Width, Height);
-
-            // Mouse and keyboard movement
-            if (Keyboard.LeftRightAxis != 0 || Keyboard.UpDownAxis != 0)
+            SceneNode starkiller = new SceneNode
             {
-                _keys = true;
-            }
-
-            if (Mouse.LeftButton)
-            {
-                _keys = false;
-                _angleVelHorz = -RotationSpeed * Mouse.XVel * DeltaTime * 0.0005f;
-                _angleVelVert = -RotationSpeed * Mouse.YVel * DeltaTime * 0.0005f;
-            }
-            else if (Touch.GetTouchActive(TouchPoints.Touchpoint_0))
-            {
-                _keys = false;
-                var touchVel = Touch.GetVelocity(TouchPoints.Touchpoint_0);
-                _angleVelHorz = -RotationSpeed * touchVel.x * DeltaTime * 0.0005f;
-                _angleVelVert = -RotationSpeed * touchVel.y * DeltaTime * 0.0005f;
-            }
-            else
-            {
-                if (_keys)
+                Components = new List<SceneComponent>
                 {
-                    _angleVelHorz = -RotationSpeed * Keyboard.LeftRightAxis * DeltaTime;
-                    _angleVelVert = -RotationSpeed * Keyboard.UpDownAxis * DeltaTime;
-                }
-                else
-                {
-                    var curDamp = (float)System.Math.Exp(-Damping * DeltaTime);
-                    _angleVelHorz *= curDamp;
-                    _angleVelVert *= curDamp;
-                }
-            }
-
-            _angleHorz += _angleVelHorz;
-            _angleVert += _angleVelVert;
-
-            // Create the camera matrix and set it as the current ModelView transformation
-            var mtxRot = float4x4.CreateRotationX(_angleVert) * float4x4.CreateRotationY(_angleHorz);
-            var mtxCam = float4x4.LookAt(0, +2, -10, 0, +2, 0, 0, 1, 0);
-
-            var view = mtxCam * mtxRot;
-            var perspective = float4x4.CreatePerspectiveFieldOfView(_fovy, (float)Width / Height, ZNear, ZFar);
-            var orthographic = float4x4.CreateOrthographic(Width, Height, ZNear, ZFar);
-
-            // Render the scene loaded in Init()
-            RC.View = view;
-            RC.Projection = perspective;
-            _sceneRenderer.Render(RC);
-
-            //Constantly check for interactive objects.
-
-            RC.Projection = orthographic;
-            if (!Mouse.Desc.Contains("Android"))
-                _sih.CheckForInteractiveObjects(RC, Mouse.Position, Width, Height);
-            if (Touch.GetTouchActive(TouchPoints.Touchpoint_0) && !Touch.TwoPoint)
-            {
-                _sih.CheckForInteractiveObjects(RC, Touch.GetPosition(TouchPoints.Touchpoint_0), Width, Height);
-            }
-
-            _guiRenderer.Render(RC);
-
-            // Swap buffers: Show the contents of the backbuffer (containing the currently rendered frame) on the front buffer.
-            Present();
-        }
-
-        private SceneContainer CreateGui()
-        {
-            var vsTex = AssetStorage.Get<string>("texture.vert");
-            var psTex = AssetStorage.Get<string>("texture.frag");
-
-            var canvasWidth = Width / 100f;
-            var canvasHeight = Height / 100f;
-
-            var btnFuseeLogo = new GUIButton
-            {
-                Name = "Canvas_Button"
-            };
-            btnFuseeLogo.OnMouseEnter += BtnLogoEnter;
-            btnFuseeLogo.OnMouseExit += BtnLogoExit;
-            btnFuseeLogo.OnMouseDown += BtnLogoDown;
-
-            var guiFuseeLogo = new Texture(AssetStorage.Get<ImageData>("FuseeText.png"));
-            var fuseeLogo = new TextureNode(
-                "fuseeLogo",
-                vsTex,
-                psTex,
-                //Set the albedo texture you want to use.
-                guiFuseeLogo,
-                //Define anchor points. They are given in percent, seen from the lower left corner, respectively to the width/height of the parent.
-                //In this setup the element will stretch horizontally but stay the same vertically if the parent element is scaled.
-                UIElementPosition.GetAnchors(AnchorPos.TOP_TOP_LEFT),
-                //Define Offset and therefor the size of the element.
-                UIElementPosition.CalcOffsets(AnchorPos.TOP_TOP_LEFT, new float2(0, canvasHeight - 0.5f), canvasHeight, canvasWidth, new float2(1.75f, 0.5f))
-                );
-            fuseeLogo.AddComponent(btnFuseeLogo);
-
-            var fontLato = AssetStorage.Get<Font>("Lato-Black.ttf");
-            var guiLatoBlack = new FontMap(fontLato, 24);
-
-            var text = new TextNode(
-                "FUSEE Simple Example",
-                "ButtonText",
-                vsTex,
-                psTex,
-                UIElementPosition.GetAnchors(AnchorPos.STRETCH_HORIZONTAL),
-                UIElementPosition.CalcOffsets(AnchorPos.STRETCH_HORIZONTAL, new float2(canvasWidth / 2 - 4, 0), canvasHeight, canvasWidth, new float2(8, 1)),
-                guiLatoBlack,
-                ColorUint.Tofloat4(ColorUint.Greenery),
-                HorizontalTextAlignment.CENTER,
-                VerticalTextAlignment.CENTER);
-
-            var canvas = new CanvasNode(
-                "Canvas",
-                _canvasRenderMode,
-                new MinMaxRect
-                {
-                    Min = new float2(-canvasWidth / 2, -canvasHeight / 2f),
-                    Max = new float2(canvasWidth / 2, canvasHeight / 2f)
-                })
-            {
+                    // TRANSFROM COMPONENT
+                    _starkillerTransform,
+                    // SHADER EFFECT COMPONENT
+                    rumpf.GetComponent<ShaderEffect>(),
+                },
                 Children = new ChildList()
-                {
-                    //Simple Texture Node, contains the fusee logo.
-                    fuseeLogo,
-                    text
-                }
             };
+
+            starkiller.Children.Add(new SceneNode
+            {
+                Components = new List<SceneComponent>
+                                {
+                                    new Transform
+                                    {
+                                        Translation = new float3(0,0,40),
+                                    },
+                                    visier.GetComponent<ShaderEffect>(),
+                                    visier.GetComponent<Mesh>()
+                                },
+                Name = "Schiff",
+
+                Children = new ChildList
+                            {
+                                new SceneNode
+                                {
+                                    Components = new List<SceneComponent>
+                                    {
+                                    new Transform
+                                        {
+                                            Translation = new float3(0,0,0)
+                                        },
+                                           rumpf.GetComponent<ShaderEffect>(),
+                                           rumpf.GetComponent<Mesh>()
+                                    },
+                                Name = "Rumpf",
+
+                                Children = new ChildList
+                                {
+                                    new SceneNode
+                                    {
+                                        Components = new List<SceneComponent>
+                                        {
+                                        new Transform
+                                            {
+                                                Translation = new float3(0,0,0)
+                                            },
+                                            tw_links.GetComponent<ShaderEffect>(),
+                                            tw_links.GetComponent<Mesh>()
+                                        },
+                                    Name = "TwLinks",
+                                    },
+
+                                    new SceneNode
+                                    {
+                                        Components = new List<SceneComponent>
+                                        {
+                                        new Transform
+                                            {
+                                                Translation = new float3(0,0,0)
+                                            },
+                                            tw_rechts.GetComponent<ShaderEffect>(),
+                                            tw_rechts.GetComponent<Mesh>()
+                                        },
+                                    Name = "TwRechts",
+                                    }
+                                }
+                            },
+                        }
+                    }
+                    );
 
             return new SceneContainer
             {
                 Children = new List<SceneNode>
                 {
-                    //Add canvas.
-                    canvas
+                    starkiller
                 }
             };
         }
 
-        public void BtnLogoEnter(CodeComponent sender)
+
+        public override async Task<bool> Init()
         {
-            _gui.Children.FindNodes(node => node.Name == "fuseeLogo").First().GetComponent<ShaderEffect>().SetEffectParam(UniformNameDeclarations.AlbedoColor, new float4(0.8f, 0.8f, 0.8f, 1f));
+            //Set the clear color for the backbuffer to white (100% intensity in all color channels R, G, B, A).
+            RC.ClearColor = new float4(0, 0, 0, 0);
+
+          
+
+
+
+
+
+            //Wrap a SceneRenderer around the model.
+            _scene = CreateScene();
+            _sceneRenderer = new SceneRendererForward(_scene);
+
+            _Rumpf = _scene.Children.FindNodes(n => n.Name == "Rumpf")?.FirstOrDefault()?.GetTransform();
+            _TwLinks = _scene.Children.FindNodes(n => n.Name == "TwLinks")?.FirstOrDefault()?.GetTransform();
+            _TwRechts = _scene.Children.FindNodes(n => n.Name == "TwRechts")?.FirstOrDefault()?.GetTransform();
+
+            return true;
         }
 
-        public void BtnLogoExit(CodeComponent sender)
+
+
+
+        //RenderAFrame is called once a frame
+        public override void RenderAFrame()
         {
-            _gui.Children.FindNodes(node => node.Name == "fuseeLogo").First().GetComponent<ShaderEffect>().SetEffectParam(UniformNameDeclarations.AlbedoColor, float4.One);
+            //Clear the backbuffer
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+            RC.Viewport(0, 0, Width, Height);
+
+
+            RC.View = float4x4.CreateTranslation(0, -20, 50) * float4x4.CreateRotationX(-5 * M.Pi / 180);
+
+            //Bewegung des Rumpfs auf x und y
+            float rumpfBewegungHorizontal = _Rumpf.Translation.x;
+            rumpfBewegungHorizontal += 0.5f * Keyboard.ADAxis;
+
+            float rumpfBewegungVertikal = _Rumpf.Translation.y;
+            rumpfBewegungVertikal += 0.5f * Keyboard.WSAxis;
+
+            _Rumpf.Translation = new float3(rumpfBewegungHorizontal, rumpfBewegungVertikal, 0);
+
+            //Barrelroll
+
+            if (Keyboard.IsKeyDown(KeyCodes.Q))
+            {
+                float barrelroll = _Rumpf.Rotation.z;
+                barrelroll += 5 * M.Pi / 180;
+                _Rumpf.Rotation = new float3(0, 0, barrelroll);
+            }
+            else if (Keyboard.IsKeyDown(KeyCodes.E))
+            {
+                float barrelroll = _Rumpf.Rotation.z;
+                barrelroll -= 5 * M.Pi / 180;
+                _Rumpf.Rotation = new float3(0, 0, barrelroll);
+            }
+
+
+            //Tick any animations and Render the scene loaded in Init()
+            _sceneRenderer.Render(RC);
+
+            //Swap buffers: Show the contents of the backbuffer (containing the currently rendered frame) on the front buffer.
+            Present();
+        }
+        public void SetProjectionAndViewport()
+
+        {
+
+            //Set the rendering area to the entire window size
+
+            RC.Viewport(0, 0, Width, Height);
+
+
+
+            //Create a new projection matrix generating undistorted images on the new aspect ratio.
+
+            var aspectRatio = Width / (float)Height;
+
+
+
+            //0.25*PI Rad -> 45° Opening angle along the vertical direction. Horizontal opening angle is calculated based on the aspect ratio
+
+            //Front clipping happens at 1 (Objects nearer than 1 world unit get clipped)
+
+            //Back clipping happens at 2000 (Anything further away from the camera than 2000 world units gets clipped, polygons will be cut)
+
+            var projection = float4x4.CreatePerspectiveFieldOfView(M.PiOver4, aspectRatio, 1, 20000);
+
+            RC.Projection = projection;
+
         }
 
-        public void BtnLogoDown(CodeComponent sender)
-        {
-            OpenLink("http://fusee3d.org");
-        }
+
     }
 }
