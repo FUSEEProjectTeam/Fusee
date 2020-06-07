74753ce23164d257b7c96260b86f616083071c43
Alexander Scheurer <mail@aspepex.net>
2020-06-07 11:56:33 +0200

    Replaced formally renamed enums with the correct string.

--- src/Engine/Core/ShaderCodeBuilder.cs
+++ src/Engine/Core/ShaderCodeBuilder.cs
@@ -49,7 +49,7 @@ namespace Fusee.Engine.Core
             },
             new[]
             {
-                new EffectParameterDeclaration { Name = RenderTargetTextureTypes.Albedo.ToString(), Value = srcTex},
+                new EffectParameterDeclaration { Name = "G_ALBEDO", Value = srcTex},
                 new EffectParameterDeclaration { Name = UniformNameDeclarations.ScreenParams, Value = screenParams},
             });
 
