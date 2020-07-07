fe17fb462cceddb25a0d9214c698e1a3b66c6f34
Moritz Roetner <moritz.roetner@ipm.fraunhofer.de>
2020-07-07 14:13:24 +0200

    Added Wasm impl Base and Engine

---
+++ src/Engine/Imp/Graphics/WebAsm/ArrayExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fusee.Engine.Imp.Graphics.WebAsm
+{
+    /// <summary>
+    /// A static helper class to provide functions for arrays.
+    /// </summary>
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Casts an array to an array of the given type T.
+        /// </summary>
+        /// <typeparam name="T">The given type to cast to.</typeparam>
+        /// <param name="array">The array to parse.</param>
+        /// <param name="cast">The cast to make.</param>
+        /// <returns></returns>
+        public static T[] ToArray<T>(this WebAssembly.Core.Array array, Func<object, T> cast)
+        {
+            if (array == null)
+            {
+                return null;
+            }
+
+            var length = array.Length;
+            var result = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = cast(array[i]);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/Engine/Imp/Graphics/WebAsm/WebGL.Backend.cs
@@ -0,0 +1,415 @@
+﻿using Fusee.Base.Core;
+using System;
+using System.Text;
+using WebAssembly;
+using WebAssembly.Core;
+
+namespace Fusee.Engine.Imp.Graphics.WebAsm
+{
+#pragma warning disable 1591
+    public abstract class JSHandler : IDisposable
+    {
+        internal JSObject Handle { get; set; }
+
+        public bool IsDisposed { get; private set; }
+
+        ~JSHandler()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (IsDisposed)
+            {
+                return;
+            }
+
+            IsDisposed = true;
+
+            Handle.Dispose();
+        }
+    }
+
+    public partial class WebGLActiveInfo : JSHandler
+    {
+    }
+
+    public partial class WebGLContextAttributes : JSHandler
+    {
+        public WebGLContextAttributes()
+        {
+            Handle = new JSObject();
+        }
+    }
+
+    public partial class WebGLObject : JSHandler
+    {
+    }
+
+    public partial class WebGLRenderingContext : WebGLRenderingContextBase
+    {
+        public WebGLRenderingContext(JSObject canvas)
+            : base(canvas, "webgl")
+        {
+        }
+
+        public WebGLRenderingContext(JSObject canvas, WebGLContextAttributes contextAttributes)
+            : base(canvas, "webgl", contextAttributes)
+        {
+        }
+    }
+
+    public abstract partial class WebGLRenderingContextBase : JSHandler
+    {
+        private const string WindowPropertyName = "WebGLRenderingContext";
+
+        protected readonly JSObject gl;
+
+        protected WebGLRenderingContextBase(
+            JSObject canvas,
+            string contextType,
+            string windowPropertyName = WindowPropertyName)
+            : this(canvas, contextType, null, windowPropertyName)
+        {
+        }
+
+        protected WebGLRenderingContextBase(
+            JSObject canvas,
+            string contextType,
+            WebGLContextAttributes contextAttributes,
+            string windowPropertyName = WindowPropertyName)
+        {
+            if (!CheckWindowPropertyExists(windowPropertyName))
+            {
+                throw new PlatformNotSupportedException(
+                    $"The context '{contextType}' is not supported in this browser");
+            }
+
+            gl = (JSObject)canvas.Invoke("getContext", contextType, contextAttributes?.Handle);
+        }
+
+        public static bool IsSupported => CheckWindowPropertyExists(WindowPropertyName);
+
+        public static bool IsVerbosityEnabled { get; set; } = false;
+
+        public static ITypedArray CastNativeArray(object managedArray)
+        {
+            var arrayType = managedArray.GetType();
+
+            // Here are listed some JavaScript array types:
+            // https://github.com/mono/mono/blob/a7f5952c69ae76015ccaefd4dfa8be2274498a21/sdks/wasm/bindings-test.cs
+            if (arrayType == typeof(byte[]))
+            {
+                return Uint8Array.From((byte[])managedArray);
+            }
+            else if (arrayType == typeof(float[]))
+            {
+                return Float32Array.From((float[])managedArray);
+            }
+            else if (arrayType == typeof(ushort[]))
+            {
+                return Uint16Array.From((ushort[])managedArray);
+            }
+            else if (arrayType == typeof(uint[]))
+            {
+                return Uint32Array.From((uint[])managedArray);
+            }
+
+            var ex = new ArgumentException("Type {managedArray} not convertible!");
+            Diagnostics.Error("Error converting managed array to javascript array!", ex);
+            throw ex;
+        }
+
+        protected static bool CheckWindowPropertyExists(string property)
+        {
+            var window = (JSObject)Runtime.GetGlobalObject();
+            var exists = window.GetObjectProperty(property) != null;
+
+            return exists;
+        }
+
+        private void DisposeArrayTypes(object[] args)
+        {
+            for (var i = 0; i < args.Length; i++)
+            {
+                var arg = args[i];
+
+                if (arg is ITypedArray typedArray && typedArray != null)
+                {
+                    var disposable = (IDisposable)typedArray;
+                    disposable.Dispose();
+                }
+                if (arg is WebAssembly.Core.Array jsArray && jsArray != null)
+                {
+                    var disposable = (IDisposable)jsArray;
+                    disposable.Dispose();
+
+                }
+            }
+        }
+
+        protected object Invoke(string method, params object[] args)
+        {
+            var actualArgs = Translate(args);
+            var result = gl.Invoke(method, actualArgs);
+            DisposeArrayTypes(actualArgs);
+
+            if (IsVerbosityEnabled)
+            {
+                var dump = new StringBuilder();
+                dump.Append(method).Append('(');
+
+                for (var i = 0; i < args.Length; i++)
+                {
+                    var item = args[i];
+                    dump.Append(Dump(item));
+
+                    if (i < (args.Length - 1))
+                    {
+                        dump.Append(", ");
+                    }
+                }
+
+                dump.Append(") = ").Append(Dump(result));
+            }
+
+            return result;
+        }
+
+        protected T Invoke<T>(string method, params object[] args)
+            where T : JSHandler, new()
+        {
+            var rawResult = Invoke(method, args);
+
+            return new T
+            {
+                Handle = (JSObject)rawResult
+            };
+        }
+
+        protected uint[] InvokeForIntToUintArray(string method, params object[] args)
+        {
+            var temp = InvokeForArray<int>(method, args);
+
+            if (temp == null)
+            {
+                return null;
+            }
+
+            var result = new uint[temp.Length];
+
+            for (var i = 0; i < temp.Length; i++)
+            {
+                result[i] = (uint)temp[i];
+            }
+
+            return result;
+        }
+
+        protected T[] InvokeForArray<T>(string method, params object[] args)
+        {
+            using (var rawResult = (WebAssembly.Core.Array)Invoke(method, args))
+            {
+                return rawResult.ToArray(item => (T)item);
+            }
+        }
+
+        protected T[] InvokeForJavaScriptArray<T>(string method, params object[] args)
+            where T : JSHandler, new()
+        {
+            using var rawResult = (WebAssembly.Core.Array)Invoke(method, args);
+            return rawResult.ToArray(item => new T { Handle = (JSObject)item });
+        }
+
+        protected T InvokeForBasicType<T>(string method, params object[] args)
+            where T : IConvertible
+        {
+            var result = Invoke(method, args);
+
+            return (T)result;
+        }
+
+        private string Dump(object @object)
+        {
+            return $"{@object ?? "null"} ({@object?.GetType()})";
+        }
+
+        private object[] Translate(object[] args)
+        {
+            var actualArgs = new object[args.Length];
+
+            for (var i = 0; i < actualArgs.Length; i++)
+            {
+                var arg = args[i];
+
+                if (arg == null)
+                {
+                    actualArgs[i] = null;
+                    continue;
+                }
+
+                if (arg is JSHandler jsHandler)
+                {
+                    arg = jsHandler.Handle;
+                }
+                else if (arg is System.Array array)
+                {
+                    if (((System.Array)arg).GetType().GetElementType().IsPrimitive)
+                    {
+                        arg = CastNativeArray(array);
+                    }
+                    else
+                    {
+                        var argArray = new WebAssembly.Core.Array();
+                        foreach (var item in (System.Array)arg)
+                        {
+                            argArray.Push(item);
+                        }
+                        arg = argArray;
+                    }
+                }
+
+                actualArgs[i] = arg;
+            }
+
+            return actualArgs;
+        }
+    }
+
+    public partial class WebGLShaderPrecisionFormat : JSHandler
+    {
+    }
+
+    public partial class WebGLUniformLocation : JSHandler, IDisposable
+    {
+    }
+
+    public partial class WebGL2RenderingContext : WebGL2RenderingContextBase
+    {
+        public WebGL2RenderingContext(JSObject canvas)
+            : base(canvas, "webgl2")
+        {
+        }
+
+        public WebGL2RenderingContext(JSObject canvas, WebGLContextAttributes contextAttributes)
+            : base(canvas, "webgl2", contextAttributes)
+        {
+        }
+    }
+
+    public abstract partial class WebGL2RenderingContextBase : WebGLRenderingContextBase
+    {
+        private const string WindowPropertyName = "WebGL2RenderingContext";
+
+        protected WebGL2RenderingContextBase(
+            JSObject canvas,
+            string contextType,
+            string windowPropertyName = WindowPropertyName)
+            : this(canvas, contextType, null, windowPropertyName)
+        {
+        }
+
+        protected WebGL2RenderingContextBase(
+            JSObject canvas,
+            string contextType,
+            WebGLContextAttributes contextAttributes,
+            string windowPropertyName = WindowPropertyName)
+            : base(canvas, contextType, contextAttributes, windowPropertyName)
+        {
+        }
+
+        public static new bool IsSupported => CheckWindowPropertyExists(WindowPropertyName);
+
+        public void TexImage2D(
+            uint target,
+            int level,
+            int internalformat,
+            int width,
+            int height,
+            int border,
+            uint format,
+            uint type,
+            ReadOnlySpan<byte> source)
+        {
+            using var nativeArray = Uint8Array.From(source);
+            TexImage2D(target, level, internalformat, width, height, border, format, type, nativeArray);
+        }
+
+        public void TexImage3D(
+            uint target,
+            int level,
+            int internalformat,
+            int width,
+            int height,
+            int depth,
+            int border,
+            uint format,
+            uint type,
+            ReadOnlySpan<byte> source)
+        {
+            using var nativeArray = Uint8Array.From(source);
+            TexImage3D(target, level, internalformat, width, height, depth, border, format, type, nativeArray);
+        }
+
+        public void TexSubImage3D(
+            uint target,
+            int level,
+            int xoffset,
+            int yoffset,
+            int zoffset,
+            int width,
+            int height,
+            int depth,
+            uint format,
+            uint type,
+            ReadOnlySpan<byte> source)
+        {
+            using var nativeArray = Uint8Array.From(source);
+            TexSubImage3D(
+                target,
+                level,
+                xoffset,
+                yoffset,
+                zoffset,
+                width,
+                height,
+                depth,
+                format,
+                type,
+                nativeArray);
+        }
+
+        public void TexSubImage2D(
+            uint target,
+            int level,
+            int xoffset,
+            int yoffset,
+            int width,
+            int height,
+            uint format,
+            uint type,
+            ReadOnlySpan<byte> source)
+        {
+            using var nativeArray = Uint8Array.From(source);
+            TexSubImage2D(
+                target,
+                level,
+                xoffset,
+                yoffset,
+                width,
+                height,
+                format,
+                type,
+                nativeArray);
+        }
+    }
+
+#pragma warning restore 1591
+}
