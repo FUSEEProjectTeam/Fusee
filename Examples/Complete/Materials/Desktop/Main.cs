9952c7dd5783086c9f4e6deb12ab217031fa8433
Alexander Scheurer <mail@aspepex.net>
2020-10-26 13:34:04 +0100

    .fus files with textures now load from subfolders. Fixes #202

--- Examples/Complete/Materials/Desktop/Main.cs
+++ Examples/Complete/Materials/Desktop/Main.cs
@@ -36,7 +36,7 @@ namespace Fusee.Examples.Materials.Desktop
                     Decoder = (string id, object storage) =>
                     {
                         if (!Path.GetExtension(id).Contains("fus", System.StringComparison.OrdinalIgnoreCase)) return null;
-                        return FusSceneConverter.ConvertFrom(ProtoBuf.Serializer.Deserialize<FusFile>((Stream)storage));
+                        return FusSceneConverter.ConvertFrom(ProtoBuf.Serializer.Deserialize<FusFile>((Stream)storage), id);
                     },
                     Checker = id => Path.GetExtension(id).Contains("fus", System.StringComparison.OrdinalIgnoreCase)
                 });
