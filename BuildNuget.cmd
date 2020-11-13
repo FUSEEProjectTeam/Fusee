a3351df9e2765e4515f1732a814eb435c3021794
Alexander Scheurer <mail@aspepex.net>
2020-11-13 13:11:24 +0100

    More error handling for NuGet build script

--- BuildNuget.cmd
+++ BuildNuget.cmd
@@ -1,6 +1,15 @@
 @echo off
 setlocal
 
+dotnet >NUL 2>NUL
+IF %ERRORLEVEL% EQU 9009 GOTO ERRORDOTNET
+
+msbuild -ver >NUL 2>NUL
+IF %ERRORLEVEL% EQU 9009 GOTO ERRORMSBUILD
+
+git >NUL 2>NUL
+IF %ERRORLEVEL% EQU 9009 GOTO ERRORGIT
+
 echo This will be cleaning up the repository (git clean -xdf), this will DELETE all untracked files. (Ctrl-C to abort)
 pause
 
@@ -18,7 +27,19 @@ dotnet pack dis\NuGet\Core\Core.csproj -c Release -o bin\Release\nuget
 dotnet pack dis\NuGet\Desktop\Desktop.csproj -c Release -o bin\Release\nuget
 msbuild dis\NuGet\Android\Android.csproj -t:restore,pack -p:Configuration=Release
 dotnet pack dis\DnTemplate\DnTemplate.csproj -c Release -o bin\Release\nuget
+goto END
 
-rem nuget pack src\Base\Imp\Android\Fusee.Base.Imp.Android.csproj -OutputDirectory bin\Release\nuget -Properties Configuration=Release -Build -Symbols -SymbolPackageFormat snupkg
-rem nuget pack src\Engine\Imp\Graphics\Android\Fusee.Engine.Imp.Graphics.Android.csproj -OutputDirectory bin\Release\nuget -Properties Configuration=Release -Build -Symbols -SymbolPackageFormat snupkg
+:ERRORDOTNET
+echo Error: dotnet not found in path
+goto END
+
+:ERRORMSBUILD
+echo Error: msbuild not found in path
+goto END
+
+:ERRORGIT
+echo Error: git not found in path
+goto END
+
+:END
 endlocal
