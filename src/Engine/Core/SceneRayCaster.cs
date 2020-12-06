fa92c2415ea600db68ec0a7f3bc9775cb4278167
Jonas Haller <jonas.haller.jh@gmail.com>
2020-12-06 17:03:17 +0100

    Add UVs and missing XML comments to SceneRayCaster

--- src/Engine/Core/SceneRayCaster.cs
+++ src/Engine/Core/SceneRayCaster.cs
@@ -7,18 +7,57 @@ using Fusee.Xene;
 
 namespace Fusee.Engine.Core
 {
+    /// <summary>
+    /// This class contains information about the scene of the picked point.
+    /// </summary>
     public class RayCastResult
     {
+        /// <summary>
+        /// The scene node containter of the result.
+        /// </summary>
         public SceneNode Node;
 
+        /// <summary>
+        /// The picked mesh.
+        /// </summary>
         public Mesh Mesh;
 
+        /// <summary>
+        /// The index of the triangle in which the intersection of ray and mesh happened.
+        /// </summary>
         public int Triangle;
 
+        /// <summary>
+        /// The barycentric u, v coordinates within the picked triangle.
+        /// </summary>
         public float U, V;
 
+        /// <summary>
+        /// The (texture-) UV coordinates of the picked point.
+        /// </summary>
+        public float2 UV
+        {
+            get
+            {
+                float2 uva = Mesh.UVs[Mesh.Triangles[Triangle]];
+                float2 uvb = Mesh.UVs[Mesh.Triangles[Triangle + 1]];
+                float2 uvc = Mesh.UVs[Mesh.Triangles[Triangle + 2]];
+
+                return float2.Barycentric(uva, uvb, uvc, U, V);
+            }
+        }
+
+        /// <summary>
+        /// The model matrix.
+        /// </summary>
         public float4x4 Model;
 
+        /// <summary>
+        /// Gets the triangles of the picked mesh.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
         public void GetTriangle(out float3 a, out float3 b, out float3 c)
         {
             a = Mesh.Vertices[Mesh.Triangles[Triangle + 0]];
@@ -26,6 +65,9 @@ namespace Fusee.Engine.Core
             c = Mesh.Vertices[Mesh.Triangles[Triangle + 2]];
         }
 
+        /// <summary>
+        /// Returns the barycentric triangle coordinates.
+        /// </summary>
         public float3 TriangleBarycentric
         {
             get
@@ -35,18 +77,41 @@ namespace Fusee.Engine.Core
             }
         }
 
+        /// <summary>
+        /// Returns the model position.
+        /// </summary>
         public float3 ModelPos => TriangleBarycentric;
 
+        /// <summary>
+        /// Returns the world position of the intersection.
+        /// </summary>
         public float3 WorldPos => float4x4.TransformPerspective(Model, ModelPos);
 
+        /// <summary>
+        /// Returns the (absolute) distance between ray origin and the intersection.
+        /// </summary>
+        public float DistanceFromOrigin;
     }
 
+    /// <summary>
+    /// Implements the scene raycaster.
+    /// </summary>
     public class SceneRayCaster : Viserator<RayCastResult, SceneRayCaster.RayCasterState, SceneNode, SceneComponent>
     {
+        /// <summary>
+        /// The origin of the ray (in world space).
+        /// </summary>
         public float3 Origin { get; private set; }
+
+        /// <summary>
+        /// The direction of the ray (in world space).
+        /// </summary>
         public float3 Direction { get; private set; }
-        
+
         #region State
+        /// <summary>
+        /// The raycaster state upon scene traversal.
+        /// </summary>
         public class RayCasterState : VisitorState
         {
             private readonly CollapsingStateStack<float4x4> _model = new CollapsingStateStack<float4x4>();
@@ -60,6 +125,9 @@ namespace Fusee.Engine.Core
                 get => _model.Tos;
             }
 
+            /// <summary>
+            /// The default constructor for the <see cref="RayCasterState" class, which registers state stacks for the model./>
+            /// </summary>
             public RayCasterState()
             {
                 RegisterState(_model);
@@ -67,22 +135,33 @@ namespace Fusee.Engine.Core
         }
         #endregion
 
-        public SceneRayCaster (SceneContainer scene) 
+        /// <summary>
+        /// The constructor to initialize a new SceneRayCaster.
+        /// </summary>
+        /// <param name="scene">The <see cref="SceneContainer"/> to use.</param>
+        public SceneRayCaster(SceneContainer scene)
             : base(scene.Children)
         {
-            // TODO: Implement constructor
+
         }
 
+        /// <summary>
+        /// This method is called when traversal starts to initialize the traversal state.
+        /// </summary>
         protected override void InitState()
         {
             base.InitState();
             State.Model = float4x4.Identity;
         }
 
+        /// <summary>
+        /// Returns a collection of objects that are hit by the ray and that can be iterated over.
+        /// </summary>
+        /// <param name="origin">The origin of the ray (in world space).</param>
+        /// <param name="direction">The direction of the ray (in world space).</param>
+        /// <returns>A collection of <see cref="RayCastRestult"/> that can be iterated over.</returns>
         public IEnumerable<RayCastResult> RayCast(float3 origin, float3 direction)
         {
-            Console.WriteLine("Starting Traversal!");
-
             Origin = origin;
             Direction = direction;
             return Viserate();
@@ -91,7 +170,7 @@ namespace Fusee.Engine.Core
         #region Visitors
 
         /// <summary>
-        /// If a TransformComponent is visited the model matrix of the <see cref="RenderContext"/> and <see cref="RendererState"/> is updated.
+        /// If a TransformComponent is visited the model matrix of the <see cref="RenderContext"/> and <see cref="RayCasterState"/> is updated.
         /// </summary> 
         /// <param name="transform">The TransformComponent.</param>
         [VisitMethod]
@@ -100,8 +179,12 @@ namespace Fusee.Engine.Core
             State.Model *= transform.Matrix();
         }
 
+        /// <summary>
+        /// Creates a raycast result from a given mesh if it is hit by the ray.
+        /// </summary>
+        /// <param name="mesh">The given mesh.</param>
         [VisitMethod]
-        public void IntersectMesh(Mesh mesh)
+        public void HitMesh(Mesh mesh)
         {
             if (!mesh.Active) return;
 
@@ -137,43 +220,13 @@ namespace Fusee.Engine.Core
                         Triangle = i,
                         Model = State.Model,
                         U = u,
-                        V = v
-                    }) ;
+                        V = v,
+                        DistanceFromOrigin = System.Math.Abs(distance)
+                    });
                 }
-
             }
         }
 
         #endregion
     }
 }
-
-/*
-// Camera and therefore ray origin in world space
-var origin = _mainCamTransform.Translation;
-
-// Direction of the ray in world space
-var ray_clip = new float4(pickPosClip.x, pickPosClip.y, 1, 1);
-var ray_cam = float4x4.Transform(RC.InvProjection, ray_clip);
-ray_cam = new float4(ray_cam.x, ray_cam.y, 1, 0);
-var ray_world = float4x4.Transform(RC.InvView, ray_cam).xyz;
-ray_world = float3.Normalize(ray_world);
-
-// Vertices of the picked triangle in world space
-var a = new float3(newPick.Mesh.Vertices[newPick.Triangle + 0]);
-a = float4x4.Transform(_triangleTransform.Matrix(), a);
-
-var b = new float3(newPick.Mesh.Vertices[newPick.Triangle + 1]);
-b = float4x4.Transform(_triangleTransform.Matrix(), b);
-
-var c = new float3(newPick.Mesh.Vertices[newPick.Triangle + 2]);
-c = float4x4.Transform(_triangleTransform.Matrix(), c);
-
-
-var n = float3.Normalize(float3.Cross(a - c, b - c));
-
-var distance = -float3.Dot(origin - a, n) / float3.Dot(ray_world, n);
-
-
-var worldPos = origin + ray_world * distance;
-*/
