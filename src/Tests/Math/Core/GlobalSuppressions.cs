9f351349c1ac1ea8a8a8fff0070c67963debc72d
Alexander Scheurer <mail@aspepex.net>
2021-08-20 12:23:24 +0200

    Suppression for expected warnings

---
+++ src/Tests/Math/Core/GlobalSuppressions.cs
@@ -0,0 +1,16 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Batch testing", Scope = "member", Target = "~M:Fusee.Tests.Math.Core.Float2Test.PointIntriangle(Fusee.Math.Core.float2,Fusee.Math.Core.float2,Fusee.Math.Core.float2,System.Single,System.Single,Fusee.Math.Core.float2)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Batch testing", Scope = "member", Target = "~M:Fusee.Tests.Math.Core.Float3Test.PointInTriangle_CornersInTriangle(Fusee.Math.Core.float3,Fusee.Math.Core.float3,Fusee.Math.Core.float3,System.Single,System.Single,Fusee.Math.Core.float3)")]
+[assembly: SuppressMessage("Usage", "xUnit1026:Theory methods should use all of their parameters", Justification = "Batch testing", Scope = "member", Target = "~M:Fusee.Tests.Math.Core.Float3Test.PointInTriangle_CornersInTriangle(Fusee.Math.Core.float3,Fusee.Math.Core.float3,Fusee.Math.Core.float3,System.Single,System.Single,Fusee.Math.Core.float3)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Batch testing", Scope = "member", Target = "~M:Fusee.Tests.Math.Core.RotationConversionTest.EulerToMatrix(System.String,Fusee.Math.Core.float3,Fusee.Math.Core.Quaternion,Fusee.Math.Core.float4x4,Fusee.Math.Core.float3)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Batch testing", Scope = "member", Target = "~M:Fusee.Tests.Math.Core.RotationConversionTest.EulerToQuaternion(System.String,Fusee.Math.Core.float3,Fusee.Math.Core.Quaternion,Fusee.Math.Core.float4x4,Fusee.Math.Core.float3)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Batch testing", Scope = "member", Target = "~M:Fusee.Tests.Math.Core.RotationConversionTest.MatrixToEuler(System.String,Fusee.Math.Core.float3,Fusee.Math.Core.Quaternion,Fusee.Math.Core.float4x4,Fusee.Math.Core.float3)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Batch testing", Scope = "member", Target = "~M:Fusee.Tests.Math.Core.RotationConversionTest.MatrixToQuaternion(System.String,Fusee.Math.Core.float3,Fusee.Math.Core.Quaternion,Fusee.Math.Core.float4x4,Fusee.Math.Core.float3)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Batch testing", Scope = "member", Target = "~M:Fusee.Tests.Math.Core.RotationConversionTest.QuaternionToEuler(System.String,Fusee.Math.Core.float3,Fusee.Math.Core.Quaternion,Fusee.Math.Core.float4x4,Fusee.Math.Core.float3)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Batch testing", Scope = "member", Target = "~M:Fusee.Tests.Math.Core.RotationConversionTest.QuaternionToMatrix(System.String,Fusee.Math.Core.float3,Fusee.Math.Core.Quaternion,Fusee.Math.Core.float4x4,Fusee.Math.Core.float3)")]
