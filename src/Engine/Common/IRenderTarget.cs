c51e0d63b5f6789333b393a54085b0428410d20c
Moritz Roetner <moritz.roetner@ipm.fraunhofer.de>
2021-08-20 11:21:43 +0200

    Revert "Fixed comments"
    This reverts commit 299a4f6b007b8cd985ec01392910d924a7121940.

--- src/Engine/Common/IRenderTarget.cs
+++ src/Engine/Common/IRenderTarget.cs
@@ -71,7 +71,7 @@ namespace Fusee.Engine.Common
         event EventHandler<EventArgs> DeleteBuffers;
 
         /// <summary>
-        /// Sets a RenderTexture into the correct position in the RenderTexture array.
+        /// Sets a RenderTexture into the correct position in the RederTexture array.
         /// </summary>
         /// <param name="src">The source RenderTexture.</param>
         /// <param name="tex">The type of the texture.</param>
