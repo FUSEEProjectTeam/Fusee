9c75d390d3f94ec00b0973a147cf005e84e27681
ASPePeX <ASPePeX@users.noreply.github.com>
2021-08-21 10:01:25 +0000

    Linting

--- src/Tests/Xirkit/Core/GlobalSuppressions.cs
+++ src/Tests/Xirkit/Core/GlobalSuppressions.cs
@@ -5,4 +5,3 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Usage", "xUnit1026:Theory methods should use all of their parameters", Justification = "<Pending>", Scope = "member", Target = "~M:Fusee.Tests.Xirkit.ConversionTest.ConvertFromString(System.Int32,System.Single,System.Double,System.Boolean,System.String,Fusee.Math.Core.double2,Fusee.Math.Core.double3,Fusee.Math.Core.double4,Fusee.Math.Core.double4x4,Fusee.Math.Core.float2,Fusee.Math.Core.float3,Fusee.Math.Core.float4,Fusee.Math.Core.float4x4)")]
