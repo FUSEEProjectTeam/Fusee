368d0966dfbbf9c36122b2eb305206c3e3eb16a6
Alexander Scheurer <mail@aspepex.net>
2021-11-23 13:35:19 +0100

    Project structure for build tools

---
+++ src/Tools/Build/Blazorpatch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fusee.Tools.Build.Blazorpatch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Tools/Build/Versionupdate/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fusee.Tools.Build.Versionupdate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
