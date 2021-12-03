a4fc3e7ac771c168bf2870d8389702f91d9f00e5
Alexander Scheurer <mail@aspepex.net>
2021-12-03 10:59:21 +0100

    F.T.B.Blazorpatch now removes the <base> tag

--- src/Tools/Build/Blazorpatch/Program.cs
+++ src/Tools/Build/Blazorpatch/Program.cs
@@ -13,8 +13,9 @@ namespace Fusee.Tools.Build.Blazorpatch
             [Option('p', "path", Required = true, HelpText = "Path to Blazor folder")]
             public string BlazorPath { get; set; }
 
-            [Option('t', "type", Default = ReplaceType.All, HelpText = "Type of replace operation. Valid options: RemoveHashingCheck, DecodePatch, All")]
+            [Option('t', "type", Default = ReplaceType.All, HelpText = "Type of replace operation. Valid options: All, RemoveHashingCheck, DecodePatch, RemoveBaseUrl, CopyPlayerCore, CopyIcos")]
             public ReplaceType ReplaceType { get; set; }
+
             [Option('f', "fuseeroot", HelpText = "Points to the Fusee root directory if not run from there.")]
             public string FuseeRoot { get; set; }
         }
@@ -24,9 +25,10 @@ namespace Fusee.Tools.Build.Blazorpatch
             All,
             RemoveHashingCheck,
             DecodePatch,
+            RemoveBaseUrl,
             CopyPlayerCore,
             CopyIcos,
-            NoJekyll
+            //NoJekyll
         }
 
         public enum ErrorCode : int
@@ -41,7 +43,11 @@ namespace Fusee.Tools.Build.Blazorpatch
         public const string hashPattern = @"(\(asset.url.*?integrity: asset.hash.*?\))";
         public const string hashReplace = @"(asset.url)";
 
-        public const string decodeFile = @"index.html";
+        public const string baseUrlPattern = @"\s*?<base.*?\/>";
+        public const string baseUrlReplace = @"";
+
+        public const string indexFile = @"index.html";
+
         public const string decodeUrl = @"https://raw.githubusercontent.com/google/brotli/master/js/decode.min.js";
         public const string decodePattern = @"(<script.*?blazor\.webassembly\.js""><\/script>)";
         public const string decodeReplace = @"<script src=""_framework/blazor.webassembly.js"" autostart=""false""></script>
@@ -82,6 +88,9 @@ namespace Fusee.Tools.Build.Blazorpatch
                           case ReplaceType.RemoveHashingCheck:
                               RemoveHashingCheck(o.BlazorPath);
                               break;
+                          case ReplaceType.RemoveBaseUrl:
+
+                              break;
                           case ReplaceType.DecodePatch:
                               DecodePatch(o.BlazorPath);
                               break;
@@ -91,9 +100,9 @@ namespace Fusee.Tools.Build.Blazorpatch
                           case ReplaceType.CopyIcos:
                               CopyIcos(o.BlazorPath, o.FuseeRoot);
                               break;
-                          case ReplaceType.NoJekyll:
-                              CreateNojekyll(o.BlazorPath);
-                              break;
+                          //case ReplaceType.NoJekyll:
+                          //    CreateNojekyll(o.BlazorPath);
+                          //    break;
                           case ReplaceType.All:
                               RemoveHashingCheck(o.BlazorPath);
                               DecodePatch(o.BlazorPath);
@@ -116,6 +125,15 @@ namespace Fusee.Tools.Build.Blazorpatch
             File.WriteAllText(Path.Combine(filePath, hashFile), newFileContent);
         }
 
+        public static void RemoveBaseUrl(string filePath)
+        {
+            var fileContent = File.ReadAllText(Path.Combine(filePath, indexFile));
+
+            var newFileContent = Regex.Replace(fileContent, baseUrlPattern, baseUrlReplace);
+
+            File.WriteAllText(Path.Combine(filePath, indexFile), newFileContent);
+        }
+
         public static void DecodePatch(string filePath)
         {
             using (var client = new HttpClient())
@@ -133,11 +151,11 @@ namespace Fusee.Tools.Build.Blazorpatch
                 }
             }
 
-            var fileContent = File.ReadAllText(Path.Combine(filePath, decodeFile));
+            var fileContent = File.ReadAllText(Path.Combine(filePath, indexFile));
 
             var newFileContent = Regex.Replace(fileContent, decodePattern, decodeReplace);
 
-            File.WriteAllText(Path.Combine(filePath, decodeFile), newFileContent);
+            File.WriteAllText(Path.Combine(filePath, indexFile), newFileContent);
         }
 
         public static void CopyPlayerCore(string filePath)
