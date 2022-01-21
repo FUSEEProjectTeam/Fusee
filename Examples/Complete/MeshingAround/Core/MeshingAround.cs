0ea726ad6d4ba075522183db3dd2f14ebacb79bf
Sarah Busert <sarah.busert@ipm.fraunhofer.de>
2022-01-21 15:31:04 +0100

    Removed RC method calls from MeshingAround

--- Examples/Complete/MeshingAround/Core/MeshingAround.cs
+++ Examples/Complete/MeshingAround/Core/MeshingAround.cs
@@ -16,6 +16,7 @@ namespace Fusee.Examples.MeshingAround.Core
         private float _beta;
 
         private SceneRendererForward _renderer;
+        private Transform _camPivotTransform;
 
         // Init is called on startup.
         public override void Init()
@@ -90,94 +91,122 @@ namespace Fusee.Examples.MeshingAround.Core
             ////////////////// Fill SceneNode ////////////////////////////////
             SceneNode parentNode = new()
             {
-                Components = new List<SceneComponent>(),
+                Components = new List<SceneComponent>()
+                {
+                    new Transform()
+                    {
+                        Rotation = float3.Zero,
+                        Scale = float3.One,
+                        Translation = new float3(0, 0, 0)
+                    }
+                },
                 Children = new ChildList()
             };
 
-            Transform parentTrans = new()
+            SceneNode sceneNodeOne = new()
             {
-                Rotation = float3.Zero,
-                Scale = float3.One,
-                Translation = new float3(0, 0, 0)
+                Components = new List<SceneComponent>()
+                {
+                    new Transform()
+                    {
+                        Rotation = float3.Zero,
+                        Scale = float3.One,
+                        Translation = new float3(0, 0, 0)
+                    },
+                    new Mesh()
+                    {
+                        Vertices = meshOne.Vertices,
+                        Triangles = meshOne.Triangles,
+                        Normals = meshOne.Normals,
+                    }
+                }
             };
-
-            parentNode.Components.Add(parentTrans);
-
-            SceneNode sceneNodeCOne = new() { Components = new List<SceneComponent>() };
-
-            Mesh meshCOne = new()
-            {
-                Vertices = meshOne.Vertices,
-                Triangles = meshOne.Triangles,
-                Normals = meshOne.Normals,
-            };
-
-            Transform tranC = new()
-            {
-                Rotation = float3.Zero,
-                Scale = float3.One,
-                Translation = new float3(0, 0, 0)
-            };
-
-            sceneNodeCOne.Components.Add(tranC);
-            sceneNodeCOne.Components.Add(meshCOne);
             ///////////////////////////////////////////////////////////
-            SceneNode sceneNodeCCube = new() { Components = new List<SceneComponent>() };
-
-            Mesh meshCCube = new()
-            {
-                Vertices = cube.Vertices,
-                Triangles = cube.Triangles,
-                Normals = cube.Normals,
+            SceneNode sceneNodeCube = new() 
+            { 
+                Components = new List<SceneComponent>() 
+                {
+                    new Transform()
+                    {
+                        Rotation = float3.Zero,
+                        Scale = float3.One,
+                        Translation = new float3(-2, -1, 0)
+                    },
+                     new Mesh()
+                    {
+                        Vertices = cube.Vertices,
+                        Triangles = cube.Triangles,
+                        Normals = cube.Normals,
+                    }
+                    
+                }
             };
-            Transform tranCube = new()
-            {
-                Rotation = float3.Zero,
-                Scale = float3.One,
-                Translation = new float3(-2, -1, 0)
-            };
-
-            sceneNodeCCube.Components.Add(tranCube);
-            sceneNodeCCube.Components.Add(meshCCube);
             //////////////////////////////////////////////////////////////////
-            SceneNode sceneNodeCTri = new() { Components = new List<SceneComponent>() };
-
-            Mesh meshCTri = new()
-            {
-                Vertices = triangle.Vertices,
-                Triangles = triangle.Triangles,
-                Normals = triangle.Normals,
+            SceneNode sceneNodeCTri = new() 
+            { 
+                Components = new List<SceneComponent>()
+                {
+                    new Transform()
+                    {
+                        Rotation = float3.Zero,
+                        Scale = float3.One,
+                        Translation = new float3(1.5f, -1, 0)
+                    },
+                    new Mesh()
+                    {
+                        Vertices = triangle.Vertices,
+                        Triangles = triangle.Triangles,
+                        Normals = triangle.Normals,
+                    }
+                }
             };
-            Transform tranTri = new()
-            {
-                Rotation = float3.Zero,
-                Scale = float3.One,
-                Translation = new float3(1.5f, -1, 0)
-            };
-
-            sceneNodeCTri.Components.Add(tranTri);
-            sceneNodeCTri.Components.Add(meshCTri);
             //////////////////////////////////////////////////////////////////
 
             parentNode.Children.Add(sceneNodeCTri);
-            parentNode.Children.Add(sceneNodeCOne);
-            parentNode.Children.Add(sceneNodeCCube);
-            var sc = new SceneContainer { Children = new List<SceneNode> { parentNode } };
+            parentNode.Children.Add(sceneNodeOne);
+            parentNode.Children.Add(sceneNodeCube);
+
+            _camPivotTransform = new Transform();
+
+            var sc = new SceneContainer 
+            { 
+                Children = new List<SceneNode> 
+                {
+                    new SceneNode()
+                    {
+                        Components =
+                        {
+                            _camPivotTransform,
+                        },
+                        Children =
+                        {
+                            new SceneNode()
+                            {
+                                Name = "MainCam",
+                                Components = new List<SceneComponent>
+                                {
+                                    new Transform()
+                                    {
+                                        Translation = new float3(0, 0, -3)
+                                    },
+                                    new Camera(ProjectionMethod.Perspective, 0.1f, 100, M.PiOver4)
+                                    {
+                                        BackgroundColor = float4.One
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    parentNode 
+                } 
+            };
 
             _renderer = new SceneRendererForward(sc);
-
-            // Set the clear color for the back buffer to white (100% intensity in all color channels R, G, B, A).
-            RC.ClearColor = new float4(0, 1, 1, 1);
         }
 
         // RenderAFrame is called once a frame
         public override void RenderAFrame()
         {
-            // Clear the back buffer
-            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
-
-            RC.Viewport(0, 0, Width, Height);
-
             float2 speed = Mouse.Velocity + Touch.GetVelocity(TouchPoints.Touchpoint_0);
             if (Mouse.LeftButton || Touch.GetTouchActive(TouchPoints.Touchpoint_0))
             {
@@ -185,10 +214,7 @@ namespace Fusee.Examples.MeshingAround.Core
                 _beta -= speed.y * 0.0001f;
             }
 
-            // Create the camera matrix and set it as the current ModelView transformation
-            float4x4 mtxRot = float4x4.CreateRotationX(_beta) * float4x4.CreateRotationY(_alpha);
-            float4x4 mtxCam = float4x4.LookAt(0, 0, -3, 0, 0, 0, 0, 1, 0);
-            RC.View = mtxCam * mtxRot;
+            _camPivotTransform.RotationQuaternion = Quaternion.FromEuler(_beta, _alpha, 0);
 
             _renderer.Render(RC);
 
