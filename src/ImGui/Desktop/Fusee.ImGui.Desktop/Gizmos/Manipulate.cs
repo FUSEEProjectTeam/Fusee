841dee36284f4d5ced38da60a54842fac0774534
Moritz Roetner <moritz.roetner@ipm.fraunhofer.de>
2022-03-01 13:12:48 +0100

    Added Gizmos

---
+++ src/ImGui/Desktop/Fusee.ImGui.Desktop/Gizmos/Manipulate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fusee.DImGui.Desktop.Gizmos
+{
+    internal class Manipulate
+    {
+    }
+}
