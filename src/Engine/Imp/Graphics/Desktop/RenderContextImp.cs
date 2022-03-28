7d7b6c7a4ba5de3e7e6f5c30148e6eb696079646
Sarah Busert <sarah.busert@ipm.fraunhofer.de>
2022-03-28 16:11:34 +0200

    RCI Desktop: DSA for CreateTexture(IWritableTexture)

--- src/Engine/Imp/Graphics/Desktop/RenderContextImp.cs
+++ src/Engine/Imp/Graphics/Desktop/RenderContextImp.cs
@@ -191,6 +191,10 @@ namespace Fusee.Engine.Imp.Graphics.Desktop
                 ColorFormat.fRGBA32 => SizedInternalFormat.Rgba32f,
                 ColorFormat.iRGBA32 => SizedInternalFormat.Rgba32i,
                 ColorFormat.fRGB16 => (SizedInternalFormat)All.Rgb16f,
+                ColorFormat.fRGB32 => (SizedInternalFormat)All.Rgb32f,
+                ColorFormat.Depth24 => (SizedInternalFormat)All.DepthComponent24,
+                ColorFormat.Depth16 => (SizedInternalFormat)All.DepthComponent16,
+                ColorFormat.RGB => (SizedInternalFormat)All.Rgb8,
                 _ => throw new ArgumentOutOfRangeException("SizedInternalFormat not supported. Try to use a format with r,g,b and a components."),
             };
         }
@@ -389,27 +393,29 @@ namespace Fusee.Engine.Imp.Graphics.Desktop
         /// <returns>An ITextureHandle that can be used for texturing in the shader. In this implementation, the handle is an integer-value which is necessary for OpenTK.</returns>
         public ITextureHandle CreateTexture(IWritableTexture img)
         {
-            int id = GL.GenTexture();
-            GL.BindTexture(TextureTarget.Texture2D, id);
+            GL.CreateTextures(TextureTarget.Texture2D, 1, out int id);
             _lastBoundTexId = id;
 
             var glMinMagFilter = GetMinMagFilter(img.FilterMode);
-            var minFilter = glMinMagFilter.Item1;
-            var magFilter = glMinMagFilter.Item2;
-            var glWrapMode = GetWrapMode(img.WrapMode);
+            var minFilter = (int)glMinMagFilter.Item1;
+            var magFilter = (int)glMinMagFilter.Item2;
+            var glWrapMode = (int)GetWrapMode(img.WrapMode);
             var pxInfo = GetTexturePixelInfo(img);
 
-            GL.TexImage2D(TextureTarget.Texture2D, 0, pxInfo.InternalFormat, img.Width, img.Height, 0, pxInfo.Format, pxInfo.PxType, IntPtr.Zero);
+            GL.TextureStorage2D(id, 1, GetSizedInteralFormat(img.PixelFormat), img.Width, img.Height);
+            //GL.TextureSubImage2D(id, 0, 0, 0, img.Width, img.Height, pxInfo.Format, pxInfo.PxType, new byte[img.Width * img.Height * img.PixelFormat.BytesPerPixel]);
 
             if (img.DoGenerateMipMaps)
-                GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+                GL.GenerateTextureMipmap(id);
 
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureCompareMode, (int)GetTexComapreMode(img.CompareMode));
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureCompareFunc, (int)GetDepthCompareFunc(img.CompareFunc));
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)minFilter);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)magFilter);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)glWrapMode);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)glWrapMode);
+            var compareMode = (int)GetTexComapreMode(img.CompareMode);
+            var compareFunc = (int)GetDepthCompareFunc(img.CompareFunc);
+            GL.TextureParameterI(id, TextureParameterName.TextureCompareMode, ref compareMode);
+            GL.TextureParameterI(id, TextureParameterName.TextureCompareFunc, ref compareFunc);
+            GL.TextureParameterI(id, TextureParameterName.TextureMinFilter, ref minFilter);
+            GL.TextureParameterI(id, TextureParameterName.TextureMagFilter, ref magFilter);
+            GL.TextureParameterI(id, TextureParameterName.TextureWrapS, ref glWrapMode);
+            GL.TextureParameterI(id, TextureParameterName.TextureWrapT, ref glWrapMode);
 
             ITextureHandle texID = new TextureHandle { TexHandle = id };
 
