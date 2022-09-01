6cf963337fc0a09c63e8fcd01d9c7a0d676ec471
Moritz Roetner <moritz.roetner@ipm.fraunhofer.de>
2022-09-01 14:24:56 +0200

    Reverted M.ScreenToWorld() changes

--- src/Math/Core/M.cs
+++ src/Math/Core/M.cs
@@ -775,24 +775,18 @@ namespace Fusee.Math.Core
         /// at any desired z coordinate specified by <paramref name="zPosition"/>
         /// </summary>
         /// <param name="windowCoordIn">Position in window space coordinates</param>
-        /// <param name="zPosition">Desired z coordinate in clip space</param>
-        /// <param name="Projection">Projection matrix</param>
-        /// <param name="View">View matrix</param>
-        /// <param name="InvProjection">Inverse Projection matrix</param>
-        /// <param name="InvView">Inverse View matrix</param>
+        /// <param name="zPosition">Desired z coordinate in world space</param>
+        /// <param name="invProjection">Inverse Projection matrix</param>
+        /// <param name="invView">Inverse View matrix</param>
         /// <param name="windowWidth">Width of window</param>
         /// <param name="windowHeight">Height of window</param>
         /// <returns></returns>
-        public static float3 ScreenPointToWorld(float2 windowCoordIn, float zPosition, float4x4 Projection, float4x4 View, float4x4 InvProjection, float4x4 InvView, int windowWidth, int windowHeight)
+        public static float3 ScreenPointToWorld(float2 windowCoordIn, float zPosition, float4x4 invProjection, float4x4 invView, int windowWidth, int windowHeight)
         {
-            var oneInClipSpace = float4x4.TransformPerspective(Projection * View, new float4(0, 0, zPosition, 1));
-
             var pickPosClip = (windowCoordIn * new float2(2.0f / windowWidth, -2.0f / windowHeight)) + new float2(-1, 1);
-
-            var vec = new float4(pickPosClip.x, pickPosClip.y, oneInClipSpace.z, 1f);
-
-            var pos = float4x4.TransformPerspective(InvProjection, vec);
-            pos = InvView * pos;
+            var vec = new float4(pickPosClip.x, pickPosClip.y, zPosition, 1f);
+            var pos = float4x4.TransformPerspective(invProjection, vec);
+            pos = invView * pos;
 
             return new float3(pos.x, pos.y, pos.z);
         }
