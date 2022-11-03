f2582f3fe8e232ba900353f16fdce7031c3d2f0b
Moritz Roetner <moritz.roetner@ipm.fraunhofer.de>
2022-11-03 16:04:25 +0100

    Added custom pick method to PickComponent

--- src/Engine/Core/Scene/PickComponent.cs
+++ src/Engine/Core/Scene/PickComponent.cs
@@ -9,6 +9,14 @@ namespace Fusee.Engine.Core.Scene
     /// </summary>
     public class PickComponent : SceneComponent
     {
+        /// <summary>
+        /// Pick layer, on picking the result with the higher layer will be prefered
+        /// </summary>
         public int PickLayer { get; set; }
+
+        /// <summary>
+        /// Posibility to deposit a custom method on how to pick the following mesh(es)
+        /// </summary>
+        public Func<PickResult>? CustomPickMethod;
     }
 }
