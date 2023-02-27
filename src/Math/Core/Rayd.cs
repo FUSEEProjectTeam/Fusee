30871ca42cd42088f3cf45875740a34dddaca7c7
Alexander Scheurer <mail@aspepex.net>
2023-02-27 16:26:10 +0100

    JSON Serialization and better handling for Ray classes

--- src/Math/Core/Rayd.cs
+++ src/Math/Core/Rayd.cs
@@ -1,24 +1,56 @@
-﻿namespace Fusee.Math.Core
+﻿using Newtonsoft.Json;
+
+namespace Fusee.Math.Core
 {
     /// <summary>
     /// Represents a ray with a given origin and direction.
     /// </summary>
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
     public struct RayD
     {
         /// <summary>
         /// The point in world coordinates from which the ray originates.
         /// </summary>
+        [JsonProperty(PropertyName = "Origin")] 
         public double3 Origin;
+
+        private double3 _direction;
 
         /// <summary>
         /// The direction of the ray.
         /// </summary>
-        public double3 Direction { get; private set; }
+        [JsonProperty(PropertyName = "Direction")] 
+        public double3 Direction
+        {
+            get { return _direction; }
+            set
+            {
+                _direction = double3.Normalize(value);
+
+                _inverseDirty = true;
+            }
+        }
+
+        private double3 _inverse;
+        private bool _inverseDirty;
 
         /// <summary>
         /// The inverse of the direction vector of the ray (1 / direction).
         /// </summary>
-        public double3 Inverse { get; private set; }
+        public double3 Inverse
+        {
+            get
+            {
+                if (_inverseDirty)
+                {
+                    _inverse = new double3(1 / Direction.x, 1 / Direction.y, 1 / Direction.z);
+
+                    _inverseDirty = false;
+                }
+
+                return _inverse;
+            }
+        }
 
         /// <summary>
         /// Create a new ray.
@@ -28,8 +60,11 @@
         public RayD(double3 origin_, double3 direction_)
         {
             Origin = origin_;
-            Direction = double3.Normalize(direction_);
-            Inverse = new double3(1 / Direction.x, 1 / Direction.y, 1 / Direction.z);
+
+            _direction = double3.Normalize(direction_);
+
+            _inverse = default;
+            _inverseDirty = true;
         }
 
         /// <summary>
@@ -42,14 +77,15 @@
         {
             Origin = double4x4.Invert(view).Translation();
 
+            _inverse = default;
+            _inverseDirty = true;
+
             double4x4 invViewProjection = double4x4.Invert(projection * view);
 
             var pickPosWorld4 = double4x4.Transform(invViewProjection, new double4(pickPosClip.x, pickPosClip.y, 1, 1));
             var pickPosWorld = (pickPosWorld4 / pickPosWorld4.w).xyz;
 
-            Direction = (pickPosWorld - Origin).Normalize();
-
-            Inverse = new double3(1 / Direction.x, 1 / Direction.y, 1 / Direction.z);
+            _direction = (pickPosWorld - Origin).Normalize();
         }
     }
 }
--- src/Math/Core/Rayf.cs
+++ src/Math/Core/Rayf.cs
@@ -1,24 +1,56 @@
-﻿namespace Fusee.Math.Core
+﻿using Newtonsoft.Json;
+
+namespace Fusee.Math.Core
 {
     /// <summary>
     /// Represents a ray with a given origin and direction.
     /// </summary>
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
     public struct RayF
     {
         /// <summary>
         /// The point in world coordinates from which the ray originates.
         /// </summary>
+        [JsonProperty(PropertyName = "Origin")] 
         public float3 Origin;
+
+        private float3 _direction;
 
         /// <summary>
         /// The direction of the ray.
         /// </summary>
-        public float3 Direction { get; private set; }
+        [JsonProperty(PropertyName = "Direction")] 
+        public float3 Direction
+        {
+            get { return _direction; }
+            set
+            {
+                _direction = float3.Normalize(value);
+
+                _inverseDirty = true;
+            }
+        }
+
+        private float3 _inverse;
+        private bool _inverseDirty;
 
         /// <summary>
         /// The inverse of the direction vector of the ray (1 / direction).
         /// </summary>
-        public float3 Inverse { get; private set; }
+        public float3 Inverse
+        {
+            get
+            {
+                if (_inverseDirty)
+                {
+                    _inverse = new float3(1 / Direction.x, 1 / Direction.y, 1 / Direction.z);
+
+                    _inverseDirty = false;
+                }
+
+                return _inverse;
+            }
+        }
 
         /// <summary>
         /// Create a new ray.
@@ -28,8 +60,11 @@
         public RayF(float3 origin_, float3 direction_)
         {
             Origin = origin_;
-            Direction = float3.Normalize(direction_);
-            Inverse = new float3(1 / Direction.x, 1 / Direction.y, 1 / Direction.z);
+
+            _direction = float3.Normalize(direction_);
+
+            _inverse = default;
+            _inverseDirty = true;
         }
 
         /// <summary>
@@ -42,14 +77,15 @@
         {
             Origin = float4x4.Invert(view).Translation();
 
+            _inverse = default;
+            _inverseDirty = true;
+
             float4x4 invViewProjection = float4x4.Invert(projection * view);
 
             var pickPosWorld4 = float4x4.Transform(invViewProjection, new float4(pickPosClip.x, pickPosClip.y, 1, 1));
             var pickPosWorld = (pickPosWorld4 / pickPosWorld4.w).xyz;
 
-            Direction = (pickPosWorld - Origin).Normalize();
-
-            Inverse = new float3(1 / Direction.x, 1 / Direction.y, 1 / Direction.z);
+            _direction = (pickPosWorld - Origin).Normalize();
         }
     }
 }
